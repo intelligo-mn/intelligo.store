347970f014ebfb9083883900aae9d3ad986f6345
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2015-12-01 12:31:40 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>My Business</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link href="/css/core.css" rel="stylesheet" type="text/css" />
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+<div id="pre-header">
+	<div id="pre-header-container">
+		<div id="logo">
+			<a href="/">My Business</a>
+		</div>
+		<!-- language menu -->
+	</div>
+</div>
+<div id="wrapper">
+<header>
+	<div id="header" style="background-image:url(/images/headers/)">
+		
+	</div>
+	<!-- navigation 1 -->
+</header>
+<div id="container">
+	<section>
+		<div id="left">
+			
+			<div class="column-item">
+				<!-- navigation 2 -->			
+			</div>
+			
+			<!-- column content -->
+			
+		</div>
+	</section>
+	<section>
+		<div id="right">
+		
+			<!-- page content -->
+		
+		</div>
+	</section>
+</div>
+<footer>
+	<div id="footer">
+		<div id="footer-left">
+			<p>&copy; <a href="/" title="Web Design Tutorials">SSDTutorials</a> <!-- date --></p>
+		</div>
+		<div id="footer-right">
+			<!-- navigation 3 -->
+		</div>
+	</div>
+</footer>
+</div>
+<script src="/js/jquery-1.6.4.min.js"></script>
+<script src="/js/core.js"></script>
+</body>
+</html>
