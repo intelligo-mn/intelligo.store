4ced1bcb48b07369a42fcdfaece29c6b5945dae0
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2015-12-03 01:40:23 +0800

    create template header footer

---
+++ template/front/footer.php
@@ -0,0 +1,19 @@
+
+		</div>
+	</section>
+</div>
+<footer>
+	<div id="footer">
+		<div id="footer-left">
+			<p>&copy; <a href="/" title="Web Design Tutorials">My PHP CMS</a> <!-- date --></p>
+		</div>
+		<div id="footer-right">
+			<!-- navigation 3 -->
+		</div>
+	</div>
+</footer>
+</div>
+<script src="/js/jquery-1.6.4.min.js"></script>
+<script src="/js/core.js"></script>
+</body>
+</html>
---
+++ template/front/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>My Business</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link href="/css/core.css" rel="stylesheet" type="text/css" />
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+<div id="pre-header">
+	<div id="pre-header-container">
+		<div id="logo">
+			<a href="/">Toroo CMS</a>
+		</div>
+		<!-- language menu -->
+	</div>
+</div>
+<div id="wrapper">
+<header>
+	<div id="header" style="background-image:url(/images/headers/)">
+		
+	</div>
+	<!-- navigation 1 -->
+</header>
+<div id="container">
+	<section>
+		<div id="left">
+			
+			<div class="column-item">
+				<!-- navigation 2 -->			
+			</div>
+			
+			<!-- column content -->
+			
+		</div>
+	</section>
+	<section>
+		<div id="right">
