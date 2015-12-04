6364f84e9b79a9ab3bfb4408b4b2b589e90fe909
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2015-12-04 23:47:06 +0800

    update URI CLASS

--- classes/Url.php
+++ classes/Url.php
@@ -42,7 +42,33 @@ class Url{
 				$uri = substr($uri, -1);
 			}
 			if (!empty($uri)) {
-				
+				$uri = explode('/', $uri);
+				$first = array_shift($uri);
+				if (in_array($first, $this->key_modules)) {
+					$this->module = $first;
+					$first = array_shift($uri);
+				}
+				$this->main = $first;
+				$this->cpage = $this->main;
+				if (count($uri) > 1) {
+					$paris = array();
+					foreach ($uri as $key => $value) {
+						$pairs[] = $value;
+						if (count($pairs) > 1) {
+							if (!Helper::isEmpty($pairs[1])) {
+								if ($pairs[0] == $this->key_page) {
+									$this->cpage = $pairs[1];		
+								} else if ($pairs[0] == 'c') {
+									$this->c = $pairs[1];
+								} else if ($pairs[0] == 'a'){
+									$this->a = $pairs[1];
+								}
+								$this->params[$pairs[0]] = $pairs[1];
+							}
+							$paris = array();
+						}
+					}
+				}
 			}
 		}
 	}
