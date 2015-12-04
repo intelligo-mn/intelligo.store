636cee40670fda68aa86a3fa5feff5619ae3abd8
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2015-12-04 22:08:48 +0800

    create new plugs folder language.php file

--- classes/Helper.php
+++ classes/Helper.php
@@ -4,4 +4,15 @@ class Helper
 	public static function makeArray($array = null){
 		return is_array($array) ? $array : array($array);
 	}
+
+	public static function getPlug($file = null, $data = null){
+		if (!empty($file)) {
+			$file = ROOT_PATH.DS.'plugs'.DS.$file.'.php';
+			if (is_file($file)) {
+				ob_start();
+				require_once($file);
+				return ob_get_clean();
+			}
+		}
+	}
 }
