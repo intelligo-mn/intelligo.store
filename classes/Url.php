a0d4c252b332e62adc69d02bcfae50c6bc5213e3
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2015-12-04 23:35:34 +0800

    URI CLASS clearString  isEmpty

--- classes/Url.php
+++ classes/Url.php
@@ -32,6 +32,47 @@ class Url{
 					$this->splitRaw($uriRaw[0]);
 				}
 			}
+			$uri = Helper::clearString($uri, PAGE_EXT)
+			$firstChar = substr($uri, 0, 1);
+			if ($firstChar == '/') {
+				$uri = substr($uri, 1);
+			}
+			$lastChar = substr($uri, 0, -1);
+			if ($lastChar == '/') {
+				$uri = substr($uri, -1);
+			}
+			if (!empty($uri)) {
+				
+			}
 		}
 	}
+
+	public static clearString($string = null, $array = null){
+		if (!empty($string) && !self::isEmpty($array)) {
+			$array = self::makeArray($array);
+			foreach ($array as $key => $value) {
+				$string = str_replace($value, '', $string)
+			}
+			return $string;
+		}
+	}
+
+	public static isEmpty($value = null){
+		return empty($value) && !is_numeric($value) ? true : false;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
