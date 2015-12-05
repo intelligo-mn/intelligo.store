d9ea357761e7db97ab067e75def38abd709d4fed
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2015-12-05 22:33:21 +0800

    create new NAVIGATION CLASS active method

--- classes/Navigation.php
+++ classes/Navigation.php
@@ -28,4 +28,25 @@ class Navigation {
 		}
 	}
 
+	public function active($main = null, $pairs = null, $single = true){
+		if (!empty($main)) {
+			if (empty($pairs)) {
+				if ($main == $this->objUrl->main {
+					return !$single ? ' '.$this->classActive : ' class="'.$this->classActive.'"';
+				}
+			}
+			else {
+				$exceptions = array();
+				foreach ($pairs as $key => $value) {
+					$paramUrl = $this->objUrl->get($key);
+					if ($paramUrl != $value) {
+						$exceptions[] = $key;
+					}
+				}
+				if ($main == $this->objUrl->$main && empty($exceptions)) {
+					return !$single ? ' '.this->classActive : ' class="'.$this->classActive.'"';
+				}
+			}
+		}
+	}
 }
