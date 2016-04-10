a8817b3988b644b5bfe6cffe647f14439d1256bc
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-04-10 20:34:10 +0900

    updated

--- app/User.php
+++ app/User.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Users Model
+ * Modu - User Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
