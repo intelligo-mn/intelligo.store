c01e28194dc84791e17520a2f210e12dfdb05735
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-04-14 20:38:14 +0900

    user preperense

--- app/UserPreference.php
+++ app/UserPreference.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Users Preferences Model
+ * Modu - UserPreference Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
