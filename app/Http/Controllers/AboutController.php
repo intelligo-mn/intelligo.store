6d1c7dc55ae6a487984bc66fd4dd30ec02c20a84
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-04-15 20:43:03 +0900

    about controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,9 +3,9 @@
 namespace app\Http\Controllers;
 
 /*
- * Antvel - Company CMS Controller
+ * Modu - About Controller
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Company;
