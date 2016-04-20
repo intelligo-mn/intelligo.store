9f9df284634683c12be964b2a03c474281399a14
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-04-20 20:39:12 +0900

    virtual product created

--- app/VirtualProduct.php
+++ app/VirtualProduct.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Virtual Products Model
+ * Modu - VirtualProduct Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
