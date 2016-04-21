27db1a472b49227c676b153e3dda8fdcd79a922b
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-04-21 20:39:51 +0900

    virtual product order

--- app/VirtualProductOrder.php
+++ app/VirtualProductOrder.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Virtual Products Order Model
+ * Modu - Virtual Product Order Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
