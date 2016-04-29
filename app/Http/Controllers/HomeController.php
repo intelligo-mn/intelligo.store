f43bcfdb0ab183a402d35edc91bb48ab62d05205
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-04-29 20:55:54 +0900

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,9 @@
 namespace app\Http\Controllers;
 
 /*
- * Antvel - Home Controller
+ * Modu - Home Controller
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\FreeProduct;
