8ce36477a5f4b8ba8d232090a25e43ce6542219f
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-05-03 20:53:17 +0900

    updatd

--- app/Http/Controllers/FreeProductsController.php
+++ app/Http/Controllers/FreeProductsController.php
@@ -3,9 +3,9 @@
 namespace app\Http\Controllers;
 
 /*
- * Antvel - Free Products Controller
+ * Modu - FreeProduct Controller
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Address;
