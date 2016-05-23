163cc75ab6c6fa9c84ccb66d565d55493f2f57ab
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-05-24 02:49:13 +0900

    product add form created #7

---
+++ admin/add.php
@@ -0,0 +1,49 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title> </title>
+</head>
+<body>
+
+	<form action="add.php" method="post" enctype="multipart/form-data">
+		
+		<table align="center" width="1000" border="1">
+			
+			<tr>
+				<td><h2>Бараа нэмэх</h2></td>
+			</tr>
+			<tr>
+				<td>Барааны нэр:</td>
+				<td><input type="text" name="product_title"/></td>
+			</tr>
+			<tr>
+				<td>Барааны категор:</td>
+				<td><input type="text" name="product_title"/></td>
+			</tr>
+			<tr>
+				<td>Барааны брэнд:</td>
+				<td><input type="text" name="product_title"/></td>
+			</tr>
+			<tr>
+				<td>Барааны зураг:</td>
+				<td><input type="text" name="product_title"/></td>
+			</tr>
+			<tr>
+				<td>Барааны үнэ:</td>
+				<td><input type="text" name="product_title"/></td>
+			</tr>
+			<tr>
+				<td>Барааны тайлбар:</td>
+				<td><input type="text" name="product_title"/></td>
+			</tr>
+			<tr>
+				<td>Барааны түлхүүр үг:</td>
+				<td><input type="text" name="product_title"/></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="insert_post"/></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
