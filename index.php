5689a13574ffd4d100255be636b1dfd004a53154
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-05-23 22:02:45 +0900

    style reset

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo 'Hello world';
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Modu</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+
+</body>
+</html>
