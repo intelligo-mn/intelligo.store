fbdfdf9ec6a0dd7185b0824cfe64fe1b41fa1d0a
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-05-24 13:35:08 +0900

    sql updated

--- database/modu.sql
+++ database/modu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2016 at 04:37 PM
+-- Generation Time: May 24, 2016 at 04:33 AM
 -- Server version: 5.7.9
 -- PHP Version: 5.6.16
 
@@ -31,7 +31,15 @@ CREATE TABLE IF NOT EXISTS `brands` (
   `brand_id` int(100) NOT NULL AUTO_INCREMENT,
   `brand_title` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`brand_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
+(1, 'Apple'),
+(2, 'Microsoft');
 
 -- --------------------------------------------------------
 
@@ -44,6 +52,27 @@ CREATE TABLE IF NOT EXISTS `category` (
   `cat_id` int(100) NOT NULL AUTO_INCREMENT,
   `cat_title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`cat_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_title`) VALUES
+(1, 'Book'),
+(2, 'Computer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE IF NOT EXISTS `menu` (
+  `menu_id` int(100) NOT NULL AUTO_INCREMENT,
+  `menu_title` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`menu_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -63,7 +92,14 @@ CREATE TABLE IF NOT EXISTS `product` (
   `product_image` text COLLATE utf8_unicode_ci NOT NULL,
   `product_keywords` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`product_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_desc`, `product_image`, `product_keywords`) VALUES
+(1, 1, 1, 'Macbook', 100, 'dec', 'des', 'test');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
