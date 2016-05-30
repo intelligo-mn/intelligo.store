f8e0b0b864d6c87fe42e35ace1a9992c871749f3
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-05-30 16:06:34 +0000

    header template completed

--- index.php
+++ index.php
@@ -5,54 +5,10 @@
 	include ("functions/product.php");
 ?>
 
-<html>
-<head>
-	<title>Modu</title>
-	<meta charset="UTF-8">
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
 
-	<div class="main">
+	<link rel="stylesheet" type="text/css" href="css/core.css">
 
-		<div class="home-header">header</div>
-	
-		<div class="home-menu">
-			<?php include ("templates/front/header.php");?>	
-			<form method="get" action="results.php" enctype="multipart/form-data">
-				<input type="text" name="search_query" placeholder="Хайх үгээ бич"/>
-				<input type="submit" name="search" value="Хайх"/>
-			</form>
-		</div>
-	
-		<div class="home-sidebar">
-
-			<div class='category'>
-				<ul>
-				<?php 
-					getCategory(); 
-				?>
-				<?php 
-					getBrand(); 
-				?>
-				</ul>
-				
-			</div>
-			
-		</div>
-	
-		<div class="home-content">
-			
-			<?php
-				getProduct();
-				getCatProduct();
-				getBrandProduct();
-			?>		
-		</div>
-	
-		<div class="home-footer">footer</div>
-	
-	</div>
+	<?php include ("templates/front/header.php");?>	
 </body>
 </html>
