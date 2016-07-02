90a3fb7bf167c50b0fcf36b3462bb05ae4d98b41
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-07-02 20:27:38 +0900

    product offer

--- app/ProductOffer.php
+++ app/ProductOffer.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Products Offers Model
+ * Modu - Product Offer Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
