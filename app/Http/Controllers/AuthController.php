f971aeabd8f8abe10900580b1672b6869abfef37
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-07-08 11:08:44 +0000

    Sign up controller view created

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modu\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+
+    public function getSignup (){
+        return view('auth.signup');    
+    }    
+    
+    public function postSignup(Request $request)
+    {
+        
+    }
+}   
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,3 +8,9 @@ Route::get('/', [
 Route::get('/alert', function(){
    return redirect()->route('home')->with('info', 'You have signed up!'); 
 });
+
+
+Route::get('/signup',[
+   'uses' => '\Modu\Http\Controllers\AuthController@getSignup',
+   'as' => 'auth.signup',
+]);
