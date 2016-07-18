d1d3c6abe291f82dd8e684771c8ecab393f424bc
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-07-18 12:20:34 +0000

    Friends table migration #28

---
+++ database/migrations/2016_07_18_121503_create_friends_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('friend_id');
+            $table->boolean('accepted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('friends');
+    }
+}
