35946e6f6d618a04e62f809180690583e143e9a9
b.tortuvshin <b.tortuvshin@chinggis.systems>
2016-08-01 10:00:16 +0900

    updated

--- resources/views/errors/swalerror.blade.php
+++
@@ -1,9 +0,0 @@
-@if (Session::has('success.message'))
-    <script>
-        swal({   title: "Амжилттай!", text: "{{ Session::get('success.message') }}", type: "success",  html: true });
-    </script>
-    @elseif (Session::has('error.message'))
-    <script>
-        swal({   title: "Алдаа!", text: "{{ Session::get('error.message') }}", type: "error",  html: true });
-    </script>
-@endif
