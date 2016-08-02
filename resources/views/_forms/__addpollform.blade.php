9b7f1aac892df45e99207cb32c301558b9c77e61
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-02 10:11:00 +0000

    demo app removed

--- resources/views/_forms/__addpollform.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="entry" data-type="poll" @if(isset($entry->id)) data-entry-id="{{ $entry->id }}" @endif>
-    <div class="entryactions"> <button class="button button-red get-button delete-entry"><i class="fa fa-trash"></i></button>  <button class="button button-white get-button up-down-trigger up-entry"><i class="fa fa-arrow-up"></i></button>  <button class="button button-white get-button up-down-trigger down-entry"><i class="fa fa-arrow-down"></i></button> </div>
-    <h3><i class="fa fa-check-circle-o"></i> {{ trans('addpost.option') }}</h3>
-    <div class="inpunting ordering pushpoll">
-        <button class="order-number button button-gray">1</button>
-        {!! Form::text(null, isset($entry->title) ? $entry->title : null, ['data-type' => 'title', 'class' => 'cd-input', 'placeholder' =>  trans('addpost.option') ]) !!}
-    </div>
-
-</div>
--- resources/views/_widgets/trendlist_sidebar.blade.php
+++
@@ -1,7 +0,0 @@
-@if($lastNews)
-    <ul class="items_lists">
-        @foreach($lastNews as $item)
-            @include('._particles._lists.items_list', ['listtype' => 'big_image titm bolb', 'featuredon' => 'on', 'descof' => 'off','linkcolor' => 'blue'])
-        @endforeach
-    </ul>
-@endif
