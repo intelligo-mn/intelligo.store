0e8529a9425e183c8aac727a5fd6e7d4613e2d2b
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-03 11:47:29 +0000

    index trans

---
+++ resources/lang/mn/index.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | All Language Lines
+    |--------------------------------------------------------------------------
+    */
+    //General
+    'list'          => 'Мэдээний жагсаалт',
+    'new-s'         => 'Мэдээ',
+    'poll'          => 'Санал асуулга',
+    'video'         => 'Видео',
+    'lists'         => 'Жагсаалт',
+    'news'          => 'Мэдээ',
+    'polls'         => 'Санал асуулга',
+    'videos'        => 'Бичлэг',
+    'videonews'     => 'Видео мэдээ',
+    'posts'         => 'Мэдээ',
+
+    'latest'        => 'Сүүлийн :type',
+    'trendings'     => 'Онцлох',
+    'mostrecent'    => 'Хамгийн сүүлийн үеийн :type',
+    'hot'           => 'Халуун :type',
+    'trend'         => 'Их уншсан :type',
+    'todayhit'      => 'Өнөөдрийн онцлох :type',
+    'top'           => 'Шилдэг :type',
+    'new'           => 'Шинэ :type',
+    'total'         => 'Нийт :type',
+
+    'connect'       => 'Холбоо барих',
+    'emptyplace'    => 'Хоосон байна...',
+
+    //header
+    'sections'      => 'Хэсэгүүд',
+    'search'        => 'Хайх...',
+    'create'        => 'Шинэ',
+
+    'myprofile'     => 'Миний мэдээлэл',
+    'draft'         => 'Ноорог мэдээлэлүүд',
+    'trash'         => 'Хогийн сав',
+    'settings'      => 'Тохиргоо',
+    'adminp'        => 'Удирдлагын самбар',
+    'logout'        => 'Гарах',
+    'register'      => 'Бүртгүүлэх',
+    'login'         => 'Нэвтрэх',
+
+
+    // Post page
+    'admintools'    => 'Админ хэрэгсэл',
+    'ownertools'    => 'Эзэмшигчийн хэрэгсэл',
+
+    'edit'          => 'Засах',
+    'waitapprove'   => 'Батлуулахаар хүлээж байна',
+    'approve'       => 'Батлах',
+
+    'createdby'     => 'Нийтэлсэн :user',
+    'postedon'      => 'нэмэгдсэн огноо :time',
+    'updatedon'     => 'засвар орсон огноо :time',
+
+    'conversations' => 'Facebook-ийн яриа',
+    'disqusconversations' => 'Disqus яриа',
+    'maylike'       => 'Холбоотой мэдээлэл',
+
+
+    // User page
+    'usern'         => 'Нэр',
+    'gender'        => 'Хүйс',
+    'location'      => 'Байршил',
+    'about'         => 'ТУХАЙ',
+    'links'         => 'Холбоосууд',
+    'joinedat'      => 'Нэгдсэн :time',
+
+    'account'        => 'Данс',
+    'details'        => 'Дэлгэрэнгүй',
+    'onlycgange'     => 'Өөрчлөлт хийхийг хүсвэл',
+    'fullname'       => 'Бүтэн нэр',
+    'savesettings'   => 'Тохиргоог хадгалах',
+
+
+    'all'           => 'Бүгд',
+
+    'today'         => "Өнөөдрийн",
+    'weekly'        => '7 хоногийн',
+
+    'username'              => "Хэрэглэгчийн нэр",
+    'email'                 => 'И-мэйл',
+    'password'              => 'Нууц үг',
+    'remember'              => 'Намайг сана',
+
+    'terms'                 => 'Үйлчилгээний нөхцөл',
+    'termslink'             => 'Би зөвшөөрч байна :url ',
+
+
+    'Doyouhaveanaccount'    => 'Та бүртгүүлж амжсан уу?',
+    'youdonthaveanaccount'  => 'Та бүртгэлгүй байна?',
+    'registerwithemail'     => 'И-мэйлээрээ бүртгүүлэх',
+    'loginwithemail'        => 'И-мэйлээрээ нэвтрэх',
+
+    'connectfacebok'        => 'Connect with Facebook',
+    'connectgoogle'         => 'Connect with Google',
+    'connecttwitter'        => 'Connect with Twitter',
+
+    'adminnote'             => 'Админ',
+
+    'ccommunity'            => 'Connect With Community',
+    'likeonface'            => 'Like Us On Facebook',
+    'followontwitter'       => 'Follow Us On Twitter',
+    'followongoogle'        => 'Follow Us On Google+',
+    'followoninstagram'     => 'Follow Us On Instagram',
+
+    'sharefacebook'         => 'Share',
+    'sharetweet'            => 'Tweet',
+    'sharegoogle'           => 'Google+',
+    'sharereddit'           => 'Reddit',
+    'sharepinterest'        => 'Pinterest',
+
+
+    'nopermission'          => 'Зөвшөөрөл хэрэгтэй!',
+
+    'successcreated'        => 'Амжилттай бүртгэлээ',
+    'successupdated'        => 'Мэдээ амжилттай шинэчилэгдлээ',
+
+    'minthreeca'            => 'Хамгийн багадаа 2 тэмдэгт шаардлагатай!',
+
+
+
+
+];
