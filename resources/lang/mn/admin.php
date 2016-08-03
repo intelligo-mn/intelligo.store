9ee47efef153a189fea79d070a4cdf779bca4cc3
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-03 11:49:58 +0000

    admin dash trans

---
+++ resources/lang/mn/admin.php
@@ -0,0 +1,395 @@
+<?php
+
+return array(
+
+    'adminpanel' => 'Админ панел',
+    'panel' => 'Самбар',
+
+    'news' => 'Мэдээ',
+    'lists' => 'Жагсаалт',
+    'quizzes' => 'Quiz',
+    'polls' => 'Санал асуулга',
+    'videos' => 'Бичлэг',
+
+    //main
+    'viewsite' => 'Вэбэд зочлох',
+    'Membersince' => 'Гишүүн болсон',
+    'Profile' => 'Хувийн мэдээлэл',
+    'Signout' => 'Гарах',
+
+    //sidebar
+    'Online' => 'Онлайн',
+    'MAINNAVIGATION' => 'ҮНДСЭН ЦЭС',
+    'Inbox' => 'Ирсэн',
+    'Plugins' => 'Нэмэлтүүд',
+    'NEW' => 'ШИНЭ',
+    'Settings' => 'Тохиргоо',
+    'GeneralSettings' => 'Үндсэн тохиргоо',
+    'LayoutSettings' => 'Layout тохиргоо',
+    'SocialMediaSettings' => 'Сошиал тохиргоо',
+    'OtherSettings' => 'Бусад тохиргоо',
+    'Categories' => 'Англилалууд',
+    'LatestPosts' => 'Сүүлд нэмэгдсэн',
+    'FeaturesPosts' => 'Санал болгох',
+    'UnapprovedPosts' => 'Зөвшөөрөгдөөгүй мэдээ',
+
+    'view' => 'Үзэх :type',
+   'Unapproved' => 'Зөвшөөрөгдөөгүй :type',
+   'Trash' => ':type Хогийн сав ',
+
+   'Users' => 'Хэрэглэгчид',
+   'ViewUsers' => 'Хэрэглэгч харах',
+   'BannedUsers' => 'Бандуулцан гарууд',
+   'Staff' => 'Сэтгүүлчид',
+   'Admins' => 'Админууд',
+   'Pages' => 'Хуудас',
+   'ViewPages' => 'Хуудас харах',
+   'AddNewPage' => 'Шинэ хуудас үүсгэх',
+   'Widgets' => 'Нэмэлт хог',
+   'Sitemap' => 'Сайтын бүтэц',
+   'documentation' => 'Documentation буюу бичиг баримт',
+   'UNAPPROVEDPOSTS' => 'ЗӨВШӨӨРӨГДӨӨГҮЙ МЭДЭЭНҮҮД',
+
+
+
+    //dashboard
+
+    'dashboard' => 'Удирдлагын самбар',
+    'controlpanel' => 'Удирдлагын самбар',
+    'newupdate' => 'Шинэ өөрчлөлт!',
+    'waitingapprove' => 'Зөвшөөрөл хүлээж байгаа',
+    'moreinfo' => 'Дэлгэрэнгүй мэдээлэл',
+    'todaysposts' => 'Өнөөдөр нэмэгдсэн мэдээлэл',
+    'todaysuserregistrations' => 'Өнөөдөр бүртгүүлсэн хэрэглэгч',
+    'todaysuserlogins' => 'Өнөөдөр нэвтэрсэн хэрэглэгч',
+    'viewall' => 'Бүгдийг харах',
+    'recentlyadded' => 'Саяхан нэмэгдсэн ',
+    'recentlyaddednews' => 'Шинэ нэмэгдсэн мэдээ',
+    'recentlyaddedlists' => 'Шинэ нэмэгдсэн жагсаалт',
+    'recentlyaddedpolls' => 'Шинэ нэмэгдсэн санал асуулга',
+    'recentlyaddedvideos' => 'Шинэ нэмэгдсэн видео',
+    'recentlyaddedquizzes' => 'Шинэ нэмэгдсэн Quizz',
+    'latestmembers' => 'Сүүлийн хэрэглэгчид',
+    'newmemberstoday' => 'Шинэ хэрэглэгч',
+    'latestunapprovedposts' => 'Сүүлийн зөвшөөрөлгүй мэдээлэл',
+    'Postsonlast30days' => 'Сүүлийн 30 хоногийн мэдэээ',
+
+    'thumb' => 'Thumb',
+    'posts' => 'Мэдээ',
+    'type' => 'Төрөл',
+    'actions' => 'Үйл ажиллагаа',
+
+    'approvepost' => 'Зөвшөөрөл олгох',
+    'editpost' => 'Мэдээ засах',
+    'sendtrash' => 'Хогийн саванд хийх',
+    'deletepermanently' => 'Бүр мөсөн устгах',
+
+    'nothingtoseehere' => 'Юу ч байхгүй байна өө',
+
+    'Last30DaysTotalPostsbyTypes' => 'Сүүлийн сарын бүх төрлийн бичлэгүүд',
+    'Userregistersonlast30days' => 'Сүүлийн сарын бүртгүүлсэн хэрэглэгчид',
+
+    'NumberofNews' => 'Мэдээний тоо',
+    'NumberofLists' => 'Жагсаалтын тоо',
+    'NumberofPolls' => 'Санал асуулгын тоо',
+    'NumberofVideos' => 'Бичлэгийн тоо',
+
+
+    //config
+
+    'OptionalConfigurations' => 'Нэмэлт тохиргоонууд',
+    'SitePostsUrlType' => 'Сайтын холбоосыг оруулна уу?',
+    'SitePostsUrlTypehelp' => 'Та дараах холбоос дээр дарна уу.',
+
+    'Usersregistration' => 'Бүртгэлтэй хэрэглэгчид санал өгөх боломжтой?',
+    'Usersregistrationhelp' => 'Та дараах холбоос дээр дарна уу.',
+
+    'Auto-listedonHomepage' => 'Нүүр хуудсанд харуулах',
+    'Auto-listedonHomepagehelp' => 'Энэ сонголтыг идэвхижүүлсэн бол бүх шинэ мэдээ автоматаар нүүр хуудсанд нэмэгдэнэ',
+
+    'AutoApprovePosts' => 'Автоматаар мэдээ нэмэх',
+    'AutoApprovePostshelp' => 'Энэ нь админы зөвгөөрөлгүй автоматаар нэмэгдэнэ.',
+
+    'Auto-approveeditedposts' => 'Автомат зөвгөөрөл засах',
+    'Auto-approveeditedpostshelp' => 'Автоматаар зөвгөөрөл олгож байгааг засах',
+
+    'Auto-LoadonLists' => 'Auto-Load on Lists',
+    'Auto-LoadonListshelp' => 'You may want to use Auto-Load on the homepage lists and posts page "You may also like" posts.',
+
+    'UserPermissions' => 'User Permissions',
+    'Userscanpost' => 'User can post on site? (Create button only available for admins if it picked as NO)',
+    'Userscandeleteownposts' => 'Users can delete own posts?',
+    'Userscaneditownposts' => 'Users can edit own posts?',
+    'Userscaneditownusernames' => 'Users can edit own usernames?',
+    'Userscaneditownemails' => 'Users can edit own emails?',
+    'Userscanaddownsocialmediaaddresses' => 'Users can add own social media addresses?',
+    'UseRight-to-LeftLanguageSupport' => 'Use Right-to-Left Language Support?',
+
+
+    'SocialMedia' => 'Social Media',
+    'PageUrl' => 'Page Url',
+
+    'EmailSettings' => 'Email Settings',
+    'Siteemail' => 'Site Email',
+
+    'LayoutConfiguration' => 'Layout Configurations',
+    'SiteLayoutType' => 'Site Layout Type:',
+    'NavbarType' => 'Navbar Type:',
+    'SiteBackgroundColor' => 'Site Background Color:',
+    'SiteBackgroundColorOnBoxedMode' => 'Site Background Color On Boxed Mode:',
+    'NavbarBackgroundColor' => 'Navbar Background Color:',
+    'NavbarTop3PixelBorderLineColor' => 'Navbar Top 3 Pixel Border Line Color:',
+    'NavbarLinkColor' => 'Navbar Link Color:',
+    'NavbarLinkHoverColor' => 'Navbar Link Hover Color:',
+    'NavbarCreateButtonBackgroundColor' => 'Navbar Create <U>Button Background</U> Color:',
+    'NavbarCreateButtonFontColor' => 'Navbar Create <U>Button Font</U> Color:',
+    'NavbarCreateButtonHoverBackgroundColor' => 'Navbar Create <U>Button Hover Background</U> Color:',
+    'NavbarCreateButtonHoverFontColor' => 'Navbar Create <U>Button Hover Font</U> Color:',
+
+
+   'MainConfiguration' => 'Main Configuration',
+   'SiteName' => 'Site Name',
+   'SiteLogo' => 'Site Logo',
+   'SiteLogohelp' => 'Upload a site logo here. (Only PNG)',
+
+   'FooterSiteLogo' => 'Footer Site Logo',
+    'SiteFavicon' => 'Site Favicon',
+    'SiteFaviconhelp' => 'Upload a favicon here. (Only PNG)',
+
+    'SiteDefaultMetaTitle' => 'Site Default Meta Title',
+    'SiteDefaultMetaDescription' => 'Site Default Meta Description',
+
+    'TermsofUsePageUrl' => 'Terms of Use Page Url',
+    'TermsofUsePageUrlhelp' => 'For register forms.',
+
+    'SiteLanguageCountryCodes' => 'Site Language&Country Codes',
+    'SiteLanguageCountryCodeshelp' => 'For example: en_US, en_EN, de_DE, tr_TR. For more info please checkout <a href="http://www.w3schools.com/tags/ref_language_codes.asp" target="_blank">Language Codes</a>, <a  target="_blank" href="http://www.w3schools.com/tags/ref_country_codes.asp">Country Codes</a>. ',
+
+    'GoogleFontConfig' => 'Google Font Config',
+    'GoogleFontConfighelp' => 'Change Google Font Here. Find your perfect font on <a href="https://www.google.com/fonts" target="_blank">Google Fonts</a>.',
+
+    'SiteFont' => 'Site Font',
+    'SiteFonthelp' => 'Set your site font family.',
+
+    'LoginConfiguration' => 'Login Configuration',
+    'AdvancedConfiguration' => 'Advanced Configuration',
+    'HeadCode' => 'Head Code',
+    'HeadCodehelp' => 'You may want to add some html/css/js code to head. For example custom css. Google verification code or other meta tags etc.',
+    'Footercode' => 'Footer code',
+    'Footercodehelp' => 'You may want to add some html/css/js code to footer. For Example Google Analytics code etc.',
+
+    'SaveSettings' => 'Save Settings',
+
+    'yes' => 'Yes',
+    'no' => 'No',
+    'on' => 'On',
+    'off' => 'Off',
+    'nouseload' => 'No - Use load more button',
+    'Wide' => 'Wide',
+    'Boxed' => 'Boxed',
+    'Fixed' => 'Fixed',
+    'Relative' => 'Relative',
+
+
+
+    //categories
+    'Maganeyourcategories' => 'Magane your categories',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'addcategory' => 'Add Category',
+
+    'Entercategoryname' => 'Enter category name',
+    'Entercategoryslug' => 'Enter category slug',
+    'Enterpoststitleslug' => 'Enter posts title slug',
+    'Categoryname' => 'Category name',
+    'CategorySlug' => 'Category Slug',
+    'PostUrlSlug' => 'Post Url Slug',
+    'CategoryDescription' => 'Category Description (optional)',
+    'ContentType' => 'Content Type',
+    'Submit' => 'Submit',
+
+    'AddMainCategory' => 'Add Main Category',
+    'AddSubCategory' => 'Add Sub Category',
+    'Order' => 'Order',
+    'disable' => 'Disable?',
+
+
+    //categories
+    'AllPages' => 'All Pages',
+    'PageTitle' => 'Page Title',
+    'CreatePage' => 'Create Page',
+    'PageForm' => 'Page Form',
+    'Title' => 'Title',
+    'TitleSlug' => 'Title Slug',
+    'Descriptiontag' => 'Description (For meta tag)',
+    'text' => 'Text',
+    'Placesometexthere' => 'Please add some text here',
+    'SaveChanges' => 'Save Changes',
+    'Createdat' => 'Created at',
+    'Slug' => 'Slug',
+    'CatName' => 'Category Name',
+    'ChangesSaved' => 'Changes Saved',
+    'Deleted' => 'Deleted',
+
+
+    //widgets
+    'PostPageSidebar' => 'Post Page Sidebar',
+    'CategoriesPageSidebar' => 'Categories Page Sidebar',
+    'HeaderBelow' => 'Header Below',
+    'PostBelow' => 'Post Below',
+    'PostPageBetween2nd3rdentry' => 'Post Page: Between 2nd 3rd entry',
+    'HomePageFirst(List)Column' => 'HomePage First(List) Column - 1. Post Below',
+    'HomePageSecond(News)Column' => 'HomePage Second(News) Column - 1. Post Below',
+    'PostPagesShareBelow' => 'Post Pages Share Below',
+    'Footer' => 'Footer',
+
+    'AddWidget' => 'Add Widget',
+    'Location' => 'Location',
+    'WidgetName' => 'Widget Name',
+    'PasteHtml' => 'Paste Html Content / Adsense Code / Social Media Embeds here',
+    'Display' => 'Display',
+    'Cancel' => 'Cancel',
+    'Disabled' => 'Disabled',
+
+
+    //plugins
+    'AllBuzzyPlugins' => 'All Buzzy Plugins',
+    'NotInstalled' => 'Not Installed',
+    'ActivateCode' => 'Activate Code',
+    'Install' => 'Install',
+    'Activated' => 'Activated',
+    'Deactivate' => 'Deactivate',
+    'BuyNow' => 'Buy Now',
+    'Docs' => 'Docs',
+    'Activateddesc' => 'Activated! But detected some missing files for this plugin.<br> Please upload plugin files you download from CodeCanyon.',
+    'Notavailabeyet' => 'Not availabe yet',
+    'FREE' => 'FREE',
+    'easyCommentPluginSettings' => 'easyComment Plugin Settings',
+    'easyCommentTheme' => 'easyComment Theme',
+    'easyCommentThemeDesc' => 'You can change theme of comment area. <a target="_blank" href="http://easycomment.akbilisim.com/example.html">See Theme Demos Here.</a>',
+    'easyCommentTitle' => 'easyComment Title',
+    'easyCommentTitleDesc' => 'You may want to change Title of comments. Default: Comments',
+    'easyCommentInitiationUrl' => 'easyComment Initiation Url',
+    'DisqusPluginSettings' => 'Disqus Plugin Settings',
+    'DisqusCode' => 'Disqus Code',
+    'BuzzyQuizzesPluginSettings' => 'Buzzy Quizzes Plugin Settings',
+    'QuizzesPluginResultType' => 'Quizzes Plugin Result Type',
+    'BuzzyQuizzesSpecialResultPopup' => 'Buzzy Quizzes Special Result Popup',
+    'OnlyBuzzFeedStyleResult' => 'Only BuzzFeed Style Result',
+    'QuizzesPluginResultTypeDesc' => 'You can change result area of Quizzes.',
+    'BuzzyTranslationManager' => 'Buzzy Translation Manager',
+    'BuzzyContactPluginSettings' => 'Buzzy Contact Plugin Settings',
+    'BuzzyContactName' => 'Buzzy Contact Name',
+    'BuzzyContactNameDesc' => 'This name will appear on mail',
+    'BuzzyContactEmail' => 'Buzzy Contact Email',
+    'BuzzyContactEmailDesc' => 'This email will appear on mail',
+    'EmailSignature' => 'Email Signature',
+    'EmailSignatureDesc' => 'You can type a signature for mail sending',
+    'Sendacopytomyemail' => 'Send a copy to my email',
+    'SendacopytomyemailDesc' => 'Please type an email if you want to get email alert for contact request. System will sent you a copy. Please leave empty if you dont want to',
+    'Usecaptchaoncontactform' => 'Use captcha on contact form',
+    'UsecaptchaoncontactformDesc' => 'You can type a signature for mail sending',
+    'GooglereCaptchaApiKey' => 'Google reCaptcha Api Key',
+    'GooglereCaptchaApiKeyDesc' => 'Get your Google reCaptcha Api Key and Secret http://www.google.com/recaptcha/intro/index.html',
+    'youPERMISSION' => '-YOU DO\'NT HAVE PERMISSION TO SEE THAT-',
+    'BuzzyHomepageBuilder' => 'Buzzy Homepage Builder',
+    'Column' => 'Column',
+    'Section1Title' => 'Section 1 Title',
+    'SelectedTypesorCategories' => 'Selected Types or Categories',
+    'Section2Title' => 'Section 2 Title',
+    'Section3Title' => 'Section 3 Title',
+    'TrendingsOnOff' => 'Trendings On/Off?',
+    'close' => 'Close',
+
+    //posts
+    'Preview' => 'Preview',
+    'User' => 'User',
+    'Status' => 'Status',
+    'FeaturedDate' => 'Featured Date',
+    'Dates' => 'Dates',
+    'Actions' => 'Actions',
+
+    //users
+    'Tcon' => 'Tcon',
+    'Email' => 'Email',
+    'JoinedAt' => 'Joined At',
+    'LastSeen' => 'Last Seen',
+
+
+   //controllers
+
+    'atleastonemail' => 'You must select at least one mail!',
+    'emailsended' => 'Email successfully sent it!',
+    'Savedasdraft' => 'Saved as draft!',
+    'pluginsnotavailable' => 'Plugins not available right now. Please try again later',
+    'wecaninstallplugin' => 'We can not install this plugin. Please try to activate with your access code.',
+    'unabletoupdate' => 'Unable to get update files. Please try again later or try to get manuel download link.',
+    'doneupdate' => 'Done. Your Buzzy System upgraded to Version',
+    'donedownload' => 'Done. Downloading...',
+    'purchaseincorrect' => 'Your purchase code is not correct. Please Contact: contact@akbilisim.com',
+    'accessok' => 'Your Access Code is verified. You can install this plugin. ',
+    'norecord' => 'No record for your purchase code. Please Get New Access Code for this product from http://envato.akbilisim.com/.',
+    'codealreadytaken' => 'This code already used by another domain. If you own this code please go http://envato.akbilisim.com/. And type correct domain name for this code.',
+    'notcorrectpur' => 'Your purchace is not correct. Please Get New Access Code  from http://envato.akbilisim.com/ and try again.',
+    'SuccesfulyCreateted' => 'Succesfuly Createted',
+
+
+   'Newsin30Days' => 'News in 30 Days',
+   'Listsin30Days' => 'Lists in 30 Days',
+   'Quizzesin30Days' => 'Quizzes in 30 Days',
+   'Pollsin30Days' => 'Polls in 30 Days',
+   'Videosin30Days' => 'Videos in 30 Days',
+
+   'Banned' => 'Banned',
+   'Unlocked' => 'Unlocked',
+   'Nowuserisnotadmin' => 'Now user is not admin!',
+   'admin' => 'Admin',
+   'StaffEditor' => 'Staff / Editor',
+   'Member' => 'Member',
+   'UnlockUser' => 'Unlock User',
+   'lockUser' => 'Lock User',
+   'NotAdmin' => 'Not Admin',
+   'MakeAdmin' => 'MakeAdmin',
+   'NotEditorStaff' => 'Not Editor/Staff',
+   'MakeEditorStaff' => 'Make Editor/Staff',
+
+   'Posts' => 'Posts',
+   'AllPosts' => 'All Posts',
+   'Deletedpermanently' => 'Deleted permanently',
+
+   'OnTrash' => 'On Trash',
+   'DraftPost' => 'Draft Post',
+   'AwaitingApproval' => 'Awaiting Approval',
+   'FeaturedPost' => 'FeaturedPost',
+   'Active' => 'Active',
+   'Pickedforhomepage' => 'Picked for homepage',
+   'Approve' => 'Approve',
+   'UndoApprove' => 'Undo Approve',
+   'PickforFeatured' => 'Pick for Featured',
+   'UndoFeatured' => 'Undo Featured',
+   'PickforHomepage' => 'Pick for Homepage',
+   'UndofromHomepage' => 'UndofromHomepage',
+   'EditPost' => 'Edit Post',
+   'SendtoTrash' => 'Send to Trash',
+   'RetrievefromTrash' => 'Retrieve from Trash',
+   'Deletepermanently' => 'Delete permanently',
+
+
+    // dataTable
+    "sEmptyTable"=>     "No data available in table",
+    "sInfo"=>          "Showing _START_ to _END_ of _TOTAL_ entries",
+    "sInfoEmpty"=>     "Showing 0 to 0 of 0 entries",
+    "sInfoFiltered"=>   "(filtered from _MAX_ total entries)",
+    "sLengthMenu"=>    "Show _MENU_ entries",
+    "sLoadingRecords"=> "Loading...",
+    "sProcessing"=>    "Processing...",
+    "sSearch"=>         "Search:",
+    "sZeroRecords"=>    "No matching records found",
+    "sFirst"=> "First",
+    "sLast"=>    "Last",
+    "sNext"=>         "Next",
+    "sPrevious"=>    "Previous",
+    "sSortAscending"=> ": activate to sort column ascending",
+    "sSortDescending"=>    ": activate to sort column descending",
+
+);
