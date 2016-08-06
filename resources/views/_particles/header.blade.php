4a6718db68511c990314b4884709e425fe421e65
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-06 21:48:02 +0000

    updated

--- resources/views/_particles/header.blade.php
+++ resources/views/_particles/header.blade.php
@@ -14,7 +14,8 @@
             </div>
             <div id="colnav" class="toggle-nav pull-l" >
                 <ul class="navmenu">
-
+                    <li class="cats_link" ><a href="http://www.blog.toroo.info" class="biga firsg" data-type=""><i class="fa "></i> Нүүр хуудас <i class="fa fa-caret-right"></i></a></li>
+                    
                     @foreach(\App\Categories::where("main", '1')->where("disabled", '0')->orwhere("main", '2')->where("disabled", '0')->orderBy('order')->limit(9)->get() as $categorys)
                         <li class="cats_link" ><a href="{{ url($categorys->name_slug) }}" class="biga firsg" data-type="{{ $categorys->id }}"><i class="fa fa-{{ $categorys->icon }}"></i> {{ $categorys->name }} <i class="fa fa-caret-right"></i></a></li>
                     @endforeach
