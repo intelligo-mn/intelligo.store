22404366f06bc196f174d2d8be0e235a895617b4
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-14 20:15:04 +0900

    some

--- app/FreeProductOrder.php
+++ app/FreeProductOrder.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Free Prodcuts Order Model
+ * Modu - FreeProductOrder Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
