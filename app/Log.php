faab805e3e7ef5a4ea04412cb5ce119cc3bf98f7
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-14 20:16:29 +0900

    log

--- app/Log.php
+++ app/Log.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Log Model
+ * Modu - Log Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
