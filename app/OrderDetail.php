502b268003498557dcfa9a9e9abe8aa4f34c8b8e
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-14 20:20:32 +0900

    order detail updated

--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Order Detail Model
+ * Modu - Order Detail Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Model;
