d2c10693b1497a9f38581d672881659a7a9c8f05
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-14 20:17:45 +0900

    order updated

--- app/Order.php
+++ app/Order.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Order Model
+ * Modu - Order Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Address;
