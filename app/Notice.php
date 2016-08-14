78d03d569f64bb25dc23e9381f1ca6659277c559
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-14 20:17:00 +0900

    notise updated

--- app/Notice.php
+++ app/Notice.php
@@ -3,9 +3,9 @@
 namespace app;
 
 /*
- * Antvel - Notice Model
+ * Modu - Notice Model
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Eloquent\Collection;
