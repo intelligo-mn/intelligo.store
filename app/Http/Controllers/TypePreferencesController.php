86c7614a0acc4a3e2cbb31145576b24d55e1fd29
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-25 15:29:32 +0900

    updated

--- app/Http/Controllers/TypePreferencesController.php
+++ app/Http/Controllers/TypePreferencesController.php
@@ -3,9 +3,9 @@
 namespace app\Http\Controllers;
 
 /*
- * Antvel - Type Preferences Controller
+ * Modu - TypePreferences Controller
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Http\Controllers\Controller;
