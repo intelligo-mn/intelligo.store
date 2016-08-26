3d0264c49292f104941351d58a6f843d3af43693
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-08-26 15:30:05 +0900

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,9 @@
 namespace app\Http\Controllers;
 
 /*
- * Antvel - Users Controller
+ * Modu - User Controller
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Business;
