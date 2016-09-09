2e085fa160e7be55380050038e6c9038a80e3822
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-09-09 20:48:07 +0900

    company contrller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,9 +3,9 @@
 namespace app\Http\Controllers;
 
 /*
- * Antvel - Main Company Controller
+ * Modu - Company Controller
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Company;
