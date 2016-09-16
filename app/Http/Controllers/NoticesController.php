3c8c634a4d6f8637c7f2520121e8ad2e78126ba7
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-09-16 21:21:06 +0900

    updated

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -3,9 +3,9 @@
 namespace app\Http\Controllers;
 
 /*
- * Antvel - Notice Controller
+ * Modu - Notice Controller
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\ActionType;
