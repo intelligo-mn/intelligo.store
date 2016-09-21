a3c59f006211903d31cd9e0d7315e9bd7bf77d6c
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-09-21 15:35:14 +0900

    routes updated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,8 +8,7 @@
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
-|
-| @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+| @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
 |
 */
 
