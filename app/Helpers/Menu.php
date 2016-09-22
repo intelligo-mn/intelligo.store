b72ac3ad64fae67f221980cc68c4b944d6ee5aa4
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-09-22 15:40:24 +0900

    app menu helper

--- app/Helpers/Menu.php
+++ app/Helpers/Menu.php
@@ -3,9 +3,9 @@
 namespace app\Helpers;
 
 /*
- * Antvel - App Menus Helper
+ * Modu - App menus helper
  *
- * @author  Gustavo Ocanto <gustavoocanto@gmail.com>
+ * @author  Tortuvshin Byambaa <toroo.byamba@gmail.com>
  */
 
 use App\Order;
