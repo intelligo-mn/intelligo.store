3c87ac02819aa38613699bd834d466b4400ff112
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-10-19 03:00:23 +0800

    reset template

---
+++ resources/views/auth/reset.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts/master')
+@section('page_class') user-reset @stop
+{{-- NAVBAR --}}
+@section('navigation')
+	@include('partial.navigation_basic')
+@stop
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">{{ trans('user.reset_password') }}</div>
+				<div class="panel-body">
+					@include('partial.message')
+
+					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<input type="hidden" name="token" value="{{ $token }}">
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">{{ trans('user.email') }}</label>
+							<div class="col-md-6">
+								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">{{ trans('user.password') }}</label>
+							<div class="col-md-6">
+								<input type="password" class="form-control" name="password">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">{{ trans('user.password_confirmation') }}</label>
+							<div class="col-md-6">
+								<input type="password" class="form-control" name="password_confirmation">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-md-6 col-md-offset-4">
+								<button type="submit" class="btn btn-primary">
+									{{ trans('user.reset_password') }}
+								</button>
+								{!! link_to(URL::previous(), trans('globals.cancel'), ['class' => 'btn btn-danger']) !!}
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
