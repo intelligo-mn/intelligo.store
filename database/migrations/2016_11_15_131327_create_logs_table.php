28a5e4a3659bbff825a4e6450094c353cbfb4cca
doljko <doljko927@gmail.com>
2016-11-15 02:45:11 +0800

    created log and notices migrations

---
+++ database/migrations/2016_11_15_131327_create_logs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('action_type_id')->unsigned();
+            $table->integer('source_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('details');
+            $table->timestamps();
+
+            $table->foreign('action_type_id')->references('id')->on('action_types');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('logs');
+    }
+}
