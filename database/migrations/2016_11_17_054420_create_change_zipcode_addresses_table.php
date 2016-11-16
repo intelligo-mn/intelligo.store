ccf3a9800b4087250b20b3f14506cbbcd10021b4
doljko <doljko927@gmail.com>
2016-11-17 02:47:41 +0800

    created change zip code adderess

---
+++ database/migrations/2016_11_17_054420_create_change_zipcode_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChangeZipcodeAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            //
+            $table->string('zipcode', 10)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            //
+            $table->dropColumn('zipcode');
+        });
+    }
+}
