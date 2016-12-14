1e44012bd030e4942e7956d2d46d5efdd1b7c269
doljko <doljko927@gmail.com>
2016-12-15 02:48:14 +0800

    time table

---
+++ database/migrations/2016_12_15_101726_create_time_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //sdf
+        Schema::create('userTime', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sector');
+            $table->string('service');
+            $table->timestamp('service_date');
+            $table->string('userName',25);
+            $table->string('userNumber',8)->nullable();
+            $table->string('addition',225);
+            $table->string('description',225)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('userTime');
+        //
+    }
+}
