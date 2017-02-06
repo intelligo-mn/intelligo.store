b415e363dad17a67d5de1f57b6792202b5e0cb7d
doljko <doljko927@gmail.com>
2017-02-07 02:48:37 +0800

    virtual product

---
+++ database/migrations/2017_02_07_061325_create_virtual_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+class CreateVirtualProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('virtual_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->string('key')->nullable(); 
+            $table->string('url')->nullable(); 
+            $table->integer('amount')->nullable();
+            $table->dateTime('expiration_date')->nullable(); 
+            $table->enum('status', array_keys(trans('globals.order_status')));
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('virtual_products');
+    }
+}
