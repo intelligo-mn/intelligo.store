8464085a2664f1a4745eedaac9dbd7920e052dcf
doljko <doljko927@gmail.com>
2017-02-25 02:18:40 +0800

    config updated

--- config/database.php
+++ config/database.php
@@ -55,7 +55,8 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'ecommerce'),
+            // 'port'     => env('DB_PORT', 3324),
+            'database'  => env('DB_DATABASE', 'bella'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
