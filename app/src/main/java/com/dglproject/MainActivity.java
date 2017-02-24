c008252950af73a69548eeba286188af004aad75
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-02-24 22:53:50 +0800

    bugfix bottom navigation

--- app/src/main/java/com/dglproject/MainActivity.java
+++ app/src/main/java/com/dglproject/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,30 +18,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TabLayout mTabLayout;
+
+    private int[] mTabsIcons = {
+            R.drawable.ic_recents_selector,
+            R.drawable.ic_favorite_selector,
+            R.drawable.ic_place_selector};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        setupViewPager();
     }
 
     // oncreate үйлдэл ажиллахад дуудагдах
     public void init () {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -46,6 +52,76 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void setupViewPager () {
+        CustomViewPager viewPager = (CustomViewPager) findViewById(R.id.view_pager);
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+
+        if (viewPager != null) {
+            viewPager.setPagingEnabled(false);
+            viewPager.setAdapter(pagerAdapter);
+        }
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        if (mTabLayout != null) {
+            mTabLayout.setupWithViewPager(viewPager);
+
+            for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+                TabLayout.Tab tab = mTabLayout.getTabAt(i);
+                if (tab != null)
+                    tab.setCustomView(pagerAdapter.getTabView(i));
+            }
+
+            mTabLayout.getTabAt(0).getCustomView().setSelected(true);
+        }
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public final int PAGE_COUNT = 3;
+
+        private final String[] mTabsTitle = {"Дэлгүүр", "Брэнд апп", "Аялал жуулчлал"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public View getTabView(int position) {
+            // Given you have a custom layout in `res/layout/custom_tab.xml` with a TextView and ImageView
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_tab, null);
+            TextView title = (TextView) view.findViewById(R.id.title);
+            title.setText(mTabsTitle[position]);
+            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+            icon.setImageResource(mTabsIcons[position]);
+            return view;
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch (pos) {
+
+                case 0:
+                    return PageFragment.newInstance(1);
+
+                case 1:
+                    return PageFragment.newInstance(2);
+                case 2:
+                    return PageFragment.newInstance(3);
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTabsTitle[position];
+        }
     }
 
     @Override
