f5d259a243d62a4ea3d848de22c40b8257120bd6
doljko <doljko927@gmail.com>
2017-02-28 02:23:47 +0800

    js updated

--- public/js/rates.js
+++ public/js/rates.js
@@ -1,10 +1,10 @@
 (function(){
 
-    var app = angular.module('AntVel'); //references to AntVel module
+    var app = angular.module('Bella'); //references to Bella module
 
     var rates = angular.module('store-rate', [ ]); //create the home page module
 
-    app.requires.push('store-rate'); //then push a new requirement to AntVel modules
+    app.requires.push('store-rate'); //then push a new requirement to Bella modules
 
     app.filter('dateToISO', function() {
         return function(input) {
