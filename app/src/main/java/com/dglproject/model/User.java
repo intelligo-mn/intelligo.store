c1f6ece0882ecdd3d1bfe0278e295a3c0dd4214f
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-01 16:54:14 +0800

    user model and adapter

---
+++ app/src/main/java/com/dglproject/model/User.java
@@ -0,0 +1,55 @@
+package com.dglproject.model;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/1/2017.
+ */
+
+public class User {
+    private int userId;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+    public User(){
+
+    }
+    public User(int _userId,String _userName,String _userEmail,String _userPassword){
+        userId = _userId;
+        userName = _userName;
+        userEmail = _userEmail;
+        userPassword = _userPassword;
+    }
+
+    public User(String name, String email, String password) {
+        userName = name;
+        userEmail = email;
+        userPassword = password;
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
