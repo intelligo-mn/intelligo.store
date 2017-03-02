9f3ad1aa106bec08260fd1b8947cef1af4958aac
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-03 04:01:36 +0800

    rnm order, created constants, check internet conn

---
+++ app/src/main/java/com/dglproject/Constants.java
@@ -0,0 +1,63 @@
+package com.dglproject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/3/2017.
+ */
+
+public class Constants {
+
+    static String AdminPageURL = "http://www.dgl.toroo.info/";
+    static String CategoryAPI = "http://www.dgl.toroo.info/api/get-all-category-data.php";
+    static String MenuAPI = "http://www.dgl.toroo.info/api/get-menu-data-by-category-id.php";
+    static String TaxCurrencyAPI = "http://www.dgl.toroo.info/api/get-tax-and-currency.php";
+    static String MenuDetailAPI = "http://www.dgl.toroo.info/api/get-menu-detail.php";
+    static String SendDataAPI = "http://www.dgl.toroo.info/api/add-reservation.php";
+
+    static String AccessKey = "12345";
+
+    static String DBPath = "/data/data/com.dglproject/databases/";
+
+    public static boolean isNetworkAvailable(Activity activity) {
+        ConnectivityManager connectivity = (ConnectivityManager) activity
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity == null) {
+            return false;
+        } else {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void CopyStream(InputStream is, OutputStream os)
+    {
+        final int buffer_size=1024;
+        try
+        {
+            byte[] bytes=new byte[buffer_size];
+            for(;;)
+            {
+                int count=is.read(bytes, 0, buffer_size);
+                if(count==-1)
+                    break;
+                os.write(bytes, 0, count);
+            }
+        }
+        catch(Exception ex){}
+    }
+
+}
--- app/src/main/java/com/dglproject/OrderActivity.java
+++ app/src/main/java/com/dglproject/OrderActivity.java
@@ -1,19 +1,16 @@
 package com.dglproject;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
-public class CardActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card);
+        setContentView(R.layout.activity_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
