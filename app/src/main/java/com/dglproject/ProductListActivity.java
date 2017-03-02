eb8f0bcb37e8673a16ef1a16453ac425528cf2be
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-03 04:54:23 +0800

    ангилалд хамаатай барааны жагсаалт харуулах цонх

---
+++ app/src/main/java/com/dglproject/ProductListActivity.java
@@ -0,0 +1,13 @@
+package com.dglproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+    }
+}
