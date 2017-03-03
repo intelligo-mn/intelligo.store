0525892b48d6515f0a767575685949367972f150
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-03 17:50:18 +0800

    created checkout activity

---
+++ app/src/main/java/com/dglproject/CheckoutActivity.java
@@ -0,0 +1,13 @@
+package com.dglproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
