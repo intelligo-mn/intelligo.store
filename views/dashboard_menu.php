6671c952bebec17d0c226d5f4b2a778feb52d597
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-05 01:57:52 +0800

    удирдлагын хэсгийн цэснүүд

---
+++ views/dashboard_menu.php
@@ -0,0 +1,90 @@
+<?php
+	include_once('config/connect.php'); 
+?>
+
+<?php
+
+	//Нийт захиалгын тоо
+	$sql_order = "SELECT COUNT(*) as num FROM order";
+	$total_order = mysqli_query($connect, $sql_order);
+	$total_order = mysqli_fetch_array($total_order);
+	$total_order = $total_order['num'];
+
+	//Нийт ангилал
+	$sql_category = "SELECT COUNT(*) as num FROM category";
+	$total_category = mysqli_query($connect, $sql_category);
+	$total_category = mysqli_fetch_array($total_category);
+	$total_category = $total_category['num'];
+
+
+	$sql_product = "SELECT COUNT(*) as num FROM product";
+	$total_product = mysqli_query($connect, $sql_product);
+	$total_product = mysqli_fetch_array($total_product);
+	$total_product = $total_product['num'];
+
+?>
+<div id="content" class="container col-md-12">
+
+<div class="col-md-12">
+		<h1>Удирдлагын хэсэг</h1>
+		<hr/>
+	</div>
+
+	 	<a href="order.php">
+			<div class="col-sm-6 col-md-2">
+	            <div class="thumbnail">    
+	              <div class="caption">
+	              <center>
+	              <img src="images/ic_order.png" width="100" height="100">
+	                <h3><?php echo $total_order;?></h3>
+	                <p class="detail">Захиалгын жагсаалт</p>  
+	                </center>
+	              </div>
+	            </div>
+	         </div>
+	    </a>
+
+ 		<a href="category.php">
+			<div class="col-sm-6 col-md-2">
+	            <div class="thumbnail">    
+	              <div class="caption">
+	              <center>
+	              <img src="images/ic_category.png" width="100" height="100">
+	                <h3><?php echo $total_category;?></h3>
+	                <p class="detail">Ангилалууд</p>  
+	                </center>
+	              </div>
+	            </div>
+	         </div>
+	    </a>
+
+		<a href="product.php">
+          <div class="col-sm-6 col-md-2">
+            <div class="thumbnail">    
+              <div class="caption">
+              <center>
+              <img src="images/ic_product.png" width="100" height="100">
+                <h3><?php echo $total_product;?></h3>
+                <p class="detail">Бүтээгдэхүүний жагсаалт</p>  
+                </center>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="admin.php">
+          <div class="col-sm-6 col-md-2">
+            <div class="thumbnail"> 
+              <div class="caption">
+              <center>
+              <img src="images/ic_setting.png" width="100" height="100">
+                <h3><br></h3>
+                <p class="detail">Тохиргоо</p>     
+                </center>
+              </div>
+            </div>
+          </div>
+        </a>
+</div>
+
+<?php include_once('config/close_database.php'); ?>
---
+++ views/menubar.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dgl</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+
+                <li class="sidebar-brand"><a href="dashboard.php">DGL</a></li> 
+                <li><a href="order.php">Захиалга</a></li>
+                <li><a href="category.php">Ангилал</a></li>
+                <li><a href="product.php">Бүтээгдэхүүн</a></li>    
+                <li><a href="setting.php">Тохиргоо</a></li>      
+                <li><a href="admin.php">Хэрэглэгч</a></li>
+                <li><a href="logout.php">Гарах</a></li>
+
+            </ul>
+        </div>
