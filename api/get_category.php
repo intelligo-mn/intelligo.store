60d335b7f26c2edcdd027c4364bcc07454898d7a
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-05 02:01:49 +0800

    ангилал авах api

---
+++ api/get_category.php
@@ -0,0 +1,33 @@
+<?php
+	include_once('../config/connect.php'); 
+	include_once('../config/variables.php');
+	
+	if(isset($_GET['accesskey'])) {
+		$access_key_received = $_GET['accesskey'];
+		
+		if($access_key_received == $access_key){
+			// Бүх ангилал авах
+			$sql_query = "SELECT * 
+					FROM category 
+					ORDER BY Category_name ASC ";
+			
+			$result = $connect->query($sql_query) or die ("Error :".mysql_error());
+	 
+			$categories = array();
+			while($category = $result->fetch_assoc()) {
+				$categories[] = array('Category'=>$category);
+			}
+			
+			// json бэлдэх
+			$output = json_encode(array('data' => $categories));
+		}else{
+			die('accesskey is incorrect.');
+		}
+	} else {
+		die('accesskey is required.');
+	}
+ 
+	echo $output;
+
+	include_once('../config/close_database.php'); 
+?>
