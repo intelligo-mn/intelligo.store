82dfe37a3f4d6cf185b908f85c91e0b6ad766bb5
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-05 05:52:19 +0800

    main fragment дээр бүтээгдэхүүн харуулах

---
+++ app/src/main/java/com/dglproject/adapters/AllProductAdapter.java
@@ -0,0 +1,79 @@
+package com.dglproject.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dglproject.DglConstants;
+import com.dglproject.R;
+import com.dglproject.activity.ActivityProductList;
+import com.dglproject.fragments.MainFragment;
+import com.dglproject.fragments.PageFragment;
+import com.dglproject.utils.ImageLoader;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/5/2017.
+ */
+public class AllProductAdapter extends BaseAdapter {
+
+    private Activity activity;
+    public ImageLoader imageLoader;
+
+    public AllProductAdapter(Activity act) {
+        this.activity = act;
+        imageLoader = new ImageLoader(act);
+    }
+
+    public int getCount() {
+
+        return PageFragment.Product_ID.size();
+    }
+
+    public Object getItem(int position) {
+
+        return position;
+    }
+
+    public long getItemId(int position) {
+
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.product_list_item, null);
+            holder = new ViewHolder();
+
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.txtText = (TextView) convertView.findViewById(R.id.txtText);
+        holder.txtSubText = (TextView) convertView.findViewById(R.id.txtSubText);
+        holder.imgThumb = (ImageView) convertView.findViewById(R.id.imgThumb);
+
+        holder.txtText.setText(PageFragment.Product_name.get(position));
+        holder.txtSubText.setText(PageFragment.Product_price.get(position)+" ");
+
+        imageLoader.DisplayImage(DglConstants.AdminPageURL+ PageFragment.Product_image.get(position), holder.imgThumb);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView txtText, txtSubText;
+        ImageView imgThumb;
+    }
+}
+
--- app/src/main/java/com/dglproject/fragments/PageFragment.java
+++ app/src/main/java/com/dglproject/fragments/PageFragment.java
@@ -1,5 +1,8 @@
 package com.dglproject.fragments;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -7,10 +10,33 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.dglproject.DglConstants;
 import com.dglproject.R;
+import com.dglproject.activity.ActivityProductDetail;
+import com.dglproject.adapters.AllProductAdapter;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 /**
  * Created by Tortuvshin Byambaa on 2/24/2017.
@@ -23,6 +49,19 @@ public class PageFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    ListView listMenu;
+    ProgressBar prgLoading;
+    TextView txtAlert;
+
+    AllProductAdapter allProductAdapter;
+
+    public static ArrayList<Long> Product_ID = new ArrayList<Long>();
+    public static ArrayList<String> Product_name = new ArrayList<String>();
+    public static ArrayList<Double> Product_price = new ArrayList<Double>();
+    public static ArrayList<String> Product_image = new ArrayList<String>();
+
+    String ProductAPI;
 
     public static PageFragment newInstance(int pageNo) {
 
@@ -39,72 +78,180 @@ public class PageFragment extends Fragment {
 
     }
 
-    private void setupRecyclerView() {
-
-        String[] myDataset={"Бараа","Бараа","Бараа","Бараа","Бараа",
-                "Бараа","Бараа","Бараа","Бараа","Бараа"};
-
-        int[]myImages = {R.drawable.logo ,R.drawable.logo ,R.drawable.logo, R.drawable.logo ,R.drawable.logo,
-                R.drawable.logo ,R.drawable.logo, R.drawable.logo ,R.drawable.logo ,R.drawable.logo};
-
-        mRecyclerView = (RecyclerView)rootView.findViewById(R.id.my_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new GridLayoutManager(getActivity(), 2);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new PageFragment.MyAdapter(myDataset,myImages);
-        mRecyclerView.setAdapter(mAdapter);
-    }
+//    private void setupRecyclerView() {
+//
+//        String[] myDataset={"Бараа","Бараа","Бараа","Бараа","Бараа",
+//                "Бараа","Бараа","Бараа","Бараа","Бараа"};
+//
+//        int[]myImages = {R.drawable.logo ,R.drawable.logo ,R.drawable.logo, R.drawable.logo ,R.drawable.logo,
+//                R.drawable.logo ,R.drawable.logo, R.drawable.logo ,R.drawable.logo ,R.drawable.logo};
+//
+//        mRecyclerView = (RecyclerView)rootView.findViewById(R.id.my_recycler_view);
+//        mRecyclerView.setHasFixedSize(true);
+//        mLayoutManager = new GridLayoutManager(getActivity(), 2);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mAdapter = new PageFragment.MyAdapter(myDataset,myImages);
+//        mRecyclerView.setAdapter(mAdapter);
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_page, container, false);
-        setupRecyclerView();
+//        setupRecyclerView();
+        prgLoading = (ProgressBar) rootView.findViewById(R.id.prdLoading);
+        listMenu = (ListView) rootView.findViewById(R.id.listProduct);
+        txtAlert = (TextView) rootView.findViewById(R.id.txtFraAlert);
+
+        ProductAPI = DglConstants.ProductApi +"?accesskey="+DglConstants.AccessKey;
+
+
+//        txtTitle.setText(Category_name);
+
+        allProductAdapter = new AllProductAdapter(getActivity());
+
+        listMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long arg3) {
+
+                Intent iDetail = new Intent(getActivity(), ActivityProductDetail.class);
+                iDetail.putExtra("product_id", Product_ID.get(position));
+                startActivity(iDetail);
+            }
+        });
         return rootView;
     }
+//
+//    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+//        private String[] mDataset;
+//        private int[] mImages;
+//
+//        public  class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//            public TextView mTextView;
+//            public ImageView mImageView;
+//            public ViewHolder(View v) {
+//                super(v);
+//                v.setOnClickListener(this);
+//                mTextView = (TextView)v.findViewById(R.id.txt);
+//                mImageView = (ImageView)v.findViewById(R.id.img);
+//            }
+//
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        }
+//
+//        public MyAdapter(String[] myDataset, int[] myImages) {
+//            mDataset = myDataset;
+//            mImages = myImages;
+//        }
+//
+//        @Override
+//        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+//                                                       int viewType) {
+//            View v = LayoutInflater.from(parent.getContext())
+//                    .inflate(R.layout.main_list, parent, false);
+//            ViewHolder vh = new ViewHolder(v);
+//            return vh;
+//        }
+//
+//        public void onBindViewHolder(ViewHolder holder, int position) {
+//            holder.mTextView.setText(mDataset[position]);
+//            holder.mImageView.setImageResource(mImages[position]);
+//        }
+//
+//        public int getItemCount() {
+//            return mDataset.length;
+//        }
+//    }
 
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        private String[] mDataset;
-        private int[] mImages;
+    void clearData(){
+        Product_ID.clear();
+        Product_name.clear();
+        Product_price.clear();
+        Product_image.clear();
+    }
 
-        public  class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-            public TextView mTextView;
-            public ImageView mImageView;
-            public ViewHolder(View v) {
-                super(v);
-                v.setOnClickListener(this);
-                mTextView = (TextView)v.findViewById(R.id.txt);
-                mImageView = (ImageView)v.findViewById(R.id.img);
+    public class getDataTask extends AsyncTask<Void, Void, Void> {
+
+        getDataTask(){
+            if(!prgLoading.isShown()){
+                prgLoading.setVisibility(0);
+                txtAlert.setVisibility(8);
             }
-
-
-            @Override
-            public void onClick(View v) {
-
-            }
-        }
-
-        public MyAdapter(String[] myDataset, int[] myImages) {
-            mDataset = myDataset;
-            mImages = myImages;
         }
 
         @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                       int viewType) {
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.main_list, parent, false);
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
+        protected Void doInBackground(Void... arg0) {
+            parseJSONData();
+            return null;
         }
 
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.mTextView.setText(mDataset[position]);
-            holder.mImageView.setImageResource(mImages[position]);
-        }
+        @Override
+        protected void onPostExecute(Void result) {
+            prgLoading.setVisibility(8);
 
-        public int getItemCount() {
-            return mDataset.length;
+            if(Product_ID.size() > 0){
+                listMenu.setVisibility(0);
+                listMenu.setAdapter(allProductAdapter);
+            }else{
+                txtAlert.setVisibility(0);
+            }
+
         }
+    }
+
+    public void parseJSONData(){
+
+        clearData();
+
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpConnectionParams.setConnectionTimeout(client.getParams(), 15000);
+            HttpConnectionParams.setSoTimeout(client.getParams(), 15000);
+            HttpUriRequest request = new HttpGet(ProductAPI);
+            HttpResponse response = client.execute(request);
+            InputStream atomInputStream = response.getEntity().getContent();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(atomInputStream));
+
+            String line;
+            String str = "";
+            while ((line = in.readLine()) != null){
+                str += line;
+            }
+
+            JSONObject json = new JSONObject(str);
+            JSONArray data = json.getJSONArray("data"); // this is the "items: [ ] part
+
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject object = data.getJSONObject(i);
+
+                JSONObject product = object.getJSONObject("product");
+
+                Product_ID.add(Long.parseLong(product.getString("Product_ID")));
+                Product_name.add(product.getString("Product_name"));
+                Product_price.add(Double.valueOf(product.getDouble("Price")));
+                Product_image.add(product.getString("Product_image"));
+
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onConfigurationChanged(final Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
     }
 }
