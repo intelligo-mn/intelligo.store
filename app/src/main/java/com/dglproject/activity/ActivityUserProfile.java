cf7c93b194db51322f6ab6afe3f976a21c85e341
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-04 23:32:03 +0800

    хэрэглэгчийн профайлын загвар

--- app/src/main/java/com/dglproject/activity/ActivityUserProfile.java
+++ app/src/main/java/com/dglproject/activity/ActivityUserProfile.java
@@ -13,21 +13,5 @@ public class ActivityUserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-        return false;
     }
 }
