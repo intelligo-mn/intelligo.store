4fe7af5ca492637d919c926fdf06ba186080893f
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-07 11:34:28 +0800

    ангилал нэмэх зам алдаа зассан

--- category.php
+++ category.php
@@ -21,19 +21,19 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/custom.css">
+        <link rel="stylesheet" href="public/css/font-awesome.min.css">
+        <link rel="stylesheet" href="public/css/bootstrap.css">
+        <link rel="stylesheet" href="public/css/custom.css">
         <title>DGL</title>
     </head>
     <body>
     	<div id="container">
-    		<?php include('public/menubar.php'); ?>
-    		<?php include('public/category-add.php'); ?>
-			<?php include('public/footer.php'); ?>
+    		<?php include('views/menubar.php'); ?>
+    		<?php include('views/category-add.php'); ?>
+			<?php include('views/footer.php'); ?>
     	</div>
 
-    <script src="css/js/jquery.min.js"></script>
-    <script src="css/js/bootstrap.min.js"></script>	
+    <script src="public/js/jquery.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>	
     </body>
 </html>
