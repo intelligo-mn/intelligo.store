7dd9d91e4424620d134c2ba687970d328883b5b5
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-09 14:07:14 +0800

    view pager adapter

---
+++ app/src/main/java/com/dglproject/adapters/ViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.dglproject.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/9/2017.
+ */
+public class ViewPagerAdapter extends PagerAdapter {
+    private LayoutInflater layoutInflater;
+
+    public ViewPagerAdapter() {
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = layoutInflater.inflate(layouts[position], container, false);
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return layouts.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        return view == obj;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        View view = (View) object;
+        container.removeView(view);
+    }
+}
