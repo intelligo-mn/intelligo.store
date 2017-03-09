b77033526116ecc1416758bf4faaf8d5f791e237
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-09 14:04:09 +0800

    Shared preferences manager class

--- app/src/main/java/com/dglproject/activity/WelcomeActivity.java
+++ app/src/main/java/com/dglproject/activity/WelcomeActivity.java
@@ -3,6 +3,8 @@ package com.dglproject.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dglproject.R;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/dglproject/utils/PrefManager.java
@@ -0,0 +1,35 @@
+package com.dglproject.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Tortuvshin Byambaa on 3/9/2017.
+ */
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    int PRIVATE_MODE = 0;
+
+    private static final String PREF_NAME = "DGLBRAND";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
