2048f42ae7d8b5e41f2955b9d78133bdfd77ff29
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-12 15:12:17 +0800

    бүтээгдэхүүн нэмэх activity layout

---
+++ app/src/main/java/com/dglproject/activity/ActivityProductAdd.java
@@ -0,0 +1,13 @@
+package com.dglproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityProductAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_add);
+    }
+}
