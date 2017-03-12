112abf2a428ffc400d5bbf0157930697177f888c
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-13 07:06:27 +0800

    Сагсанд байгаа бүтээгдэхүүнүүд харуулсан

--- app/src/main/java/com/dglproject/adapters/CartAdapter.java
+++ app/src/main/java/com/dglproject/adapters/CartAdapter.java
@@ -1,66 +1,66 @@
-package com.dglproject.adapters;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
-
-import com.dglproject.activity.ActivityCart;
-import com.dglproject.R;
-
-/**
- * Created by Tortuvshin Byambaa on 3/3/2017.
- */
-public class CartAdapter extends BaseAdapter {
-
-    private LayoutInflater inflater;
-
-    public CartAdapter(Context context) {
-        inflater = LayoutInflater.from(context);
-    }
-
-    public int getCount() {
-
-        return ActivityCart.Menu_ID.size();
-    }
-
-    public Object getItem(int position) {
-
-        return position;
-    }
-
-    public long getItemId(int position) {
-
-        return position;
-    }
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        ViewHolder holder;
-
-        if(convertView == null){
-            convertView = inflater.inflate(R.layout.cart_list_item, null);
-            holder = new ViewHolder();
-            holder.txtMenuName = (TextView) convertView.findViewById(R.id.txtMenuName);
-            holder.txtQuantity = (TextView) convertView.findViewById(R.id.txtQuantity);
-            holder.txtPrice = (TextView) convertView.findViewById(R.id.txtPrice);
-
-            convertView.setTag(holder);
-        }else{
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-
-        holder.txtMenuName.setText(ActivityCart.Menu_name.get(position));
-        holder.txtQuantity.setText(String.valueOf(ActivityCart.Quantity.get(position)));
-        holder.txtPrice.setText(ActivityCart.Sub_total_price.get(position)+" "+ ActivityCart.Currency);
-
-        return convertView;
-    }
-
-    static class ViewHolder {
-        TextView txtMenuName, txtQuantity, txtPrice;
-    }
-}
+//package com.dglproject.adapters;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.TextView;
+//
+//import com.dglproject.activity.ActivityCart;
+//import com.dglproject.R;
+//
+///**
+// * Created by Tortuvshin Byambaa on 3/3/2017.
+// */
+//public class CartAdapter extends BaseAdapter {
+//
+//    private LayoutInflater inflater;
+//
+//    public CartAdapter(Context context) {
+//        inflater = LayoutInflater.from(context);
+//    }
+//
+//    public int getCount() {
+//
+//        return ActivityCart.Menu_ID.size();
+//    }
+//
+//    public Object getItem(int position) {
+//
+//        return position;
+//    }
+//
+//    public long getItemId(int position) {
+//
+//        return position;
+//    }
+//
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//
+//        ViewHolder holder;
+//
+//        if(convertView == null){
+//            convertView = inflater.inflate(R.layout.cart_list_item, null);
+//            holder = new ViewHolder();
+//            holder.txtMenuName = (TextView) convertView.findViewById(R.id.txtMenuName);
+//            holder.txtQuantity = (TextView) convertView.findViewById(R.id.txtQuantity);
+//            holder.txtPrice = (TextView) convertView.findViewById(R.id.txtPrice);
+//
+//            convertView.setTag(holder);
+//        }else{
+//            holder = (ViewHolder) convertView.getTag();
+//        }
+//
+//
+//        holder.txtMenuName.setText(ActivityCart.Menu_name.get(position));
+//        holder.txtQuantity.setText(String.valueOf(ActivityCart.Quantity.get(position)));
+//        holder.txtPrice.setText(ActivityCart.Sub_total_price.get(position)+" "+ ActivityCart.Currency);
+//
+//        return convertView;
+//    }
+//
+//    static class ViewHolder {
+//        TextView txtMenuName, txtQuantity, txtPrice;
+//    }
+//}
---
+++ app/src/main/java/com/dglproject/fragments/CartFragment.java
@@ -0,0 +1,76 @@
+package com.dglproject.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.dglproject.R;
+import com.dglproject.database.CartProductsAdapter;
+import com.dglproject.models.CartProducts;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CartFragment extends android.app.Fragment {
+
+    public final static String  TAG = "Cart";
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_cart, container, false);
+
+        final CartProductsAdapter productAdapter = new CartProductsAdapter(this.getActivity());
+        ArrayList<CartProducts> data = productAdapter.readData();
+
+        ListView listView = (ListView) rootView.findViewById(R.id.fragment_item_ordered_list_view);
+
+        final ArrayList<HashMap<String, String>> dataList;
+
+        dataList = new ArrayList<HashMap<String, String>>();
+
+        for (CartProducts x : data) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("Title", x.getTitle());
+            map.put("TotalOrder", "Бүтээгдэхүүний тоо: " + x.getTotalOrder());
+            map.put("TotalCost", "Нийт үнэ: $" + x.getTotalCost());
+            dataList.add(map);
+        }
+
+        ListAdapter adapter = new SimpleAdapter(
+                this.getActivity(),
+                dataList,
+                R.layout.item_cart,
+                new String[]{"Title", "TotalOrder", "TotalCost"},
+                new int[]{R.id.list_image_text_view, R.id.recepit_total_order_text_view, R.id.recepit_cost_text_view});
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String title = dataList.get(position).get("Title");
+                CartProducts item = productAdapter.findByTitle(title);
+                String costString = "" + item.getCost();
+                String totalCostString = "" + item.totalCost;
+                String totalOrderString = "" + item.getTotalOrder();
+                String ratingString = "" + item.getRatinng();
+                String[] transferData = {item.getTitle(), item.getDescription(), costString, totalCostString, totalOrderString, item.getImage(),ratingString};
+//                Intent intent = new Intent(getActivity(), CartEditActivity.class);
+//                intent.putExtra(TAG, transferData);
+//                startActivity(intent);
+            }
+        });
+        productAdapter.close();
+        return rootView;
+
+    }
+}
