c03106f1ba0106054e519df884b91e69d5e37d68
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-19 10:16:04 +0800

    product service updated

--- api/ProductService.php
+++ api/ProductService.php
@@ -8,44 +8,41 @@
     $price = "";
     $currency = "";
 
-    if(isset($_POST['model'])){
-        $model = $_POST['model'];
+    if(isset($_GET['model'])){
+        $model = $_GET['model'];
     }
     
-    if(isset($_POST['name'])){
+    if(isset($_GET['name'])){
         
-        $name = $_POST['name'];
-        
-    }
-    
-    if(isset($_POST['description'])){
-        
-        $description = $_POST['description'];
-        
-    }
-    if(isset($_POST['price'])){
-        
-        $price = $_POST['price'];
-        
-    }
-    if(isset($_POST['currency'])){
-        
-        $currency = $_POST['currency'];
+        $name = $_GET['name'];
         
     }
     
-    
+    if(isset($_GET['description'])){
+        
+        $description = $_GET['description'];
+        
+    }
+    if(isset($_GET['price'])){
+        
+        $price = $_GET['price'];
+        $doublePrice = (double)$price;
+        
+    }
+    if(isset($_GET['currency'])){
+        
+        $currency = $_GET['currency'];
+        
+    }
     
     $productObject = new ProductController();
-    
     // Бараа нэмэх
     
     if(!empty($model) && !empty($name) && !empty($description) && !empty($price) && !empty($currency)){
         
-        $json_product = $productObject->createProduct($name, $model, $description, $price, $currency);
+        $json_product = $productObject->createProduct($name, $model, $description, $doublePrice, $currency);
         
         echo json_encode($json_product);
         
     }
-    
     ?>
