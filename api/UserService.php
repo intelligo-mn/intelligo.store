8958f4259767ec51b5203a2d74e486d1a851a68d
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-31 02:35:09 +0800

    user service post to get

--- api/UserService.php
+++ api/UserService.php
@@ -8,21 +8,21 @@
     
     $email = "";
     
-    if(isset($_POST['username'])){
+    if(isset($_GET['username'])){
         
-        $username = $_POST['username'];
-        
-    }
-    
-    if(isset($_POST['password'])){
-        
-        $password = $_POST['password'];
+        $username = $_GET['username'];
         
     }
     
-    if(isset($_POST['email'])){
+    if(isset($_GET['password'])){
         
-        $email = $_POST['email'];
+        $password = $_GET['password'];
+        
+    }
+    
+    if(isset($_GET['email'])){
+        
+        $email = $_GET['email'];
         
     }
     
