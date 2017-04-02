44a249814281b432d16cea6f3e226d541faf182e
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-04-02 22:51:50 +0800

    megre head fix

--- ProductService.php
+++ ProductService.php
@@ -1,101 +1,5 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-<<<<<<< HEAD
-
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-require_once 'app/ProductController.php';
-
-$access_key = 12345;
-
-if(isset($_GET['accesskey'])) {
-    $access_key_received = $_GET['accesskey'];
-   
-    if($access_key_received == $access_key){
-
-        $model = "";
-        $name = "";
-        $description = "";
-        $price = "";
-        $currency = "";
-        
-        if(isset($_GET['model'])){
-            $model = $_GET['model'];
-        }
-        
-        if(isset($_GET['name'])){
-            
-            $name = $_GET['name'];
-            
-        }
-        
-        if(isset($_GET['description'])){
-            
-            $description = $_GET['description'];
-            
-        }
-        if(isset($_GET['price'])){
-            
-            $price = $_GET['price'];
-            $doublePrice = (double)$price;
-            
-        }
-        if(isset($_GET['currency'])){
-            
-            $currency = $_GET['currency'];
-            
-        }
-
-        $productObject = new ProductController();
-        
-        // Бараа нэмэх
-        
-        if(!empty($model) && !empty($name) && !empty($description) && !empty($price) && !empty($currency)){
-            
-            $json_product = $productObject->createProduct($name, $model, $description, $doublePrice, $currency);
-            
-            echo json_encode($json_product);
-            
-        }
-
-        $products = $productObject->getProducts();
-
-        echo json_encode($products);
-
-        // Сонгосон брэндийн мэдээлэл харуулах
-
-        if(isset($_GET['brand_id'])) {
-            $brand_ID = $_GET['brand_id'];
-            
-            $brandProduct = $productObject->getProductsBrand($brand_ID);
-        
-            echo json_encode($brandProduct);
-
-        } else {
-            die('brand id are required.');
-        }
-
-        //Сонгосон бүтээгдэхүүний дэлгэрэнгүй харуулах
-
-        if(isset($_GET['product_id'])) {
-            $product_ID = $_GET['product_id'];
-            
-            $product = $productObject->getProductDetail($product_ID);
-
-            echo json_encode($product);
-
-        } else {
-            die('brand id are required.');
-        }
-    }else{
-        die('accesskey is incorrect.');
-    }
-} else {
-    die('accesskey required.');
-}
-
-=======
     require_once 'app/ProductController.php';
     require_once 'config/security.php';
     $access_key = (new DGLSecure())->generateAccessKey();
@@ -137,5 +41,4 @@ if(isset($_GET['accesskey'])) {
     }else {
         echo json_encode(["result" => "invalid request!!!"]);
     }
->>>>>>> 903cc9fd86d5d951363cb78ece0a8c9c0d588aab
 ?>
