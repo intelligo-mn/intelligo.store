3a0de31314893cae71f5db8307552c354052b231
Naranmandakh <ts.naranmandakh@gmail.com>
2017-04-02 19:05:38 +0800

    ProductService request state нэмсэн. бичиглэл цэгцэлсэн.

--- ProductService.php
+++ ProductService.php
@@ -1,99 +1,48 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
     require_once 'app/ProductController.php';
+    $access_key = 12345;
+
+    if(!isset($_GET['accesskey']) || $_GET['accesskey'] != $access_key) 
+        die('accesskey required.')
+    if(!isset($_GET["state"]))
+        die('request state required.')
+
+    $model = "";
+    $name = "";
+    $description = "";
+    $price = "";
+    $currency = "";
     
-    // $access_key = 12345;
+    if(isset($_GET['model']) && isset($_GET['name']) && isset($_GET['description']) && isset($_GET['price']) && isset($_GET['currency'])){
+        $model = $_GET['model'];
+        $name = $_GET['name'];
+        $description = $_GET['description'];
+        $doublePrice = (double)$_GET['price'];
+        $currency = $_GET['currency'];
+    }
 
-    // if(isset($_GET['accesskey'])) {
-    //     $access_key_received = $_GET['accesskey'];
-       
-    //     if($access_key_received == $access_key){
-
-            // $model = "";
-            // $name = "";
-            // $description = "";
-            // $price = "";
-            // $currency = "";
-            
-            // if(isset($_GET['model'])){
-            //     $model = $_GET['model'];
-            // }
-            
-            // if(isset($_GET['name'])){
-                
-            //     $name = $_GET['name'];
-                
-            // }
-            
-            // if(isset($_GET['description'])){
-                
-            //     $description = $_GET['description'];
-                
-            // }
-            // if(isset($_GET['price'])){
-                
-            //     $price = $_GET['price'];
-            //     $doublePrice = (double)$price;
-                
-            // }
-            // if(isset($_GET['currency'])){
-                
-            //     $currency = $_GET['currency'];
-                
-            // }
-
-            $productObject = new ProductController();
-            
-            // Бараа нэмэх
-            
-            // if(!empty($model) && !empty($name) && !empty($description) && !empty($price) && !empty($currency)){
-                
-            //     $json_product = $productObject->createProduct($name, $model, $description, $doublePrice, $currency);
-                
-            //     echo json_encode($json_product);
-                
-            // }
-
-            $products = $productObject->getProducts();
-
-            
-        echo "size: ".sizeof($products);
-        echo json_encode($products);
-
-            // Сонгосон брэндийн мэдээлэл харуулах
-
-            // if(isset($_GET['brand_id'])) {
-            //     $brand_ID = $_GET['brand_id'];
-                
-            //     $brandProduct = $productObject->getProductsBrand($brand_ID);
-            
-            //     echo json_encode($brandProduct);
-
-            // } else {
-            //     die('brand id are required.');
-            // }
-
-            // //Сонгосон бүтээгдэхүүний дэлгэрэнгүй харуулах
-
-            // if(isset($_GET['product_id'])) {
-            //     $product_ID = $_GET['product_id'];
-                
-            //     $product = $productObject->getProductDetail($product_ID);
-
-            //     echo json_encode($product);
-
-            // } else {
-            //     die('brand id are required.');
-            // }
-    //     }else{
-    //         die('accesskey is incorrect.');
-    //     }
-    // } else {
-    //     die('accesskey required.');
-    // }
- 
+    $productObject = new ProductController();
+    
+    //state == c -> Бараа нэмэх 
+    if($_GET["state"] == "c" && !empty($model) && !empty($name) && !empty($description) && !empty($price) && !empty($currency)){
+        
+        $json_product = $productObject->create($name, $model, $description, $doublePrice, $currency);
+        
+        echo json_encode($json_product);
+        
+    }else if($_GET["state"] == "u" && !empty($model) && !empty($name) && !empty($description) && !empty($price) && !empty($currency)){ //state == u -> update
+        //TODO энд upate хийх үйлдэлүүд бичнэ
+    }else if($_GET["state"] == "r"){//state == r -> read
+        // Сонгосон брэндийн мэдээлэл харуулах
+        if(isset($_GET['brand_id']))
+            echo json_encode($productObject->getByBrandId($_GET['brand_id']));
+        if(isset($_GET['product_id'])) {
+            echo json_encode($productObject->getByProductId($_GET['product_id']));
+        else
+            echo json_encode($productObject->getAll());
+    }
 ?>
