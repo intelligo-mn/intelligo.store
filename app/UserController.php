dcde7ef9a5a592660378f3c32e7d26f4057b811b
Naranmandakh <ts.naranmandakh@gmail.com>
2017-04-06 05:22:38 +0800

    usercontorller updated.

--- app/UserController.php
+++ app/UserController.php
@@ -15,60 +15,31 @@ class UserController{
         $this->db = new DbConnect();
     }
     
-    public function isExist($username, $email, $fb_id){
-        
-        $query = "select * from ".$this->db_table." where username = '$username' AND (email = '$email' OR fb_id = '$fb_id')";
-        
-        $result = mysqli_query($this->db->getDb(), $query);
-        
-        if(mysqli_num_rows($result) > 0){
-            
-            mysqli_close($this->db->getDb());
-            
-            return true;
-            
-        }
-        
-        
-        return false;
-        
+    public function isExist($username, $email){
+        $query = "select * from ".$this->db_table." where username = '$username' OR email = '$email'";
+        return mysqli_num_rows(mysqli_query($this->db->getDb(), $query)) > 0;
     }
     public function create($username, $password, $email, $mobile, $fb_id, $avatar_image){
-        
-        
-        $isExisting = $this->isExist($username, $email, $fb_id);
-        
-        $json['success'] = 0;
-            $json['message'] = "Хэрэглэгчийн нэр эсвэл нууц үг буруу байна !";
-        if($isExisting){
-            
-            $json['success'] = 0;
-            $json['message'] = "Алдаа хэрэглэгчийн нэр мэйл хаяг бүртгэлтэй байна";
-        }else{
-
+        if(!isExist($username, $email)){
             $query = "insert into ".$this->db_table." (`username`, `password`, `email`, `firstname`, `lastname`, `mobile`, `gender`, `birthday`, 
               `status`, `email_verification_code`, `folder`, `avatar_image`, `ip_address`, `created_user_id`, `updated_user_id`, 
               `department_id`, `rank_id`, `position_id`, `aimag_id`, `created_at`, `updated_at`, `type`, `person_reg_number`, 
               `person_profession`, `person_biography`, `person_start_year`, `company_name`, `company_register`, `company_description`, 
               `company_founded_year`, `tel`, `fax`, `location`, `timezone`, `hit_counter`, `website`, `level`, `level_started_date`, 
               `level_expire_date`, `fb_id`, `google_id`, `twitter_id`, `linkedin_id`, `instagram_id`, `is_registered_by_social`, 
-              `registered_from_language`, `is_creator`, `is_investor`, `is_idea_owner`, `is_idea_buyer`, `slug`) 
-
-              values ('$username', '$password', '$email', '', '', '', 1, '', 1, NULL, '".date("Y-m")."', ".($avatar_image == "" ? NULL : "'".$avatar_image."'").", '".$_SERVER['REMOTE_ADDR']."', NULL, NULL, NULL, NULL, NULL, NULL, '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."', 'person', '', '', '', NULL, '', '', '', NULL, '', '', '', NULL, 0, '', '0', NULL, NULL, '$fb_id', NULL, NULL, NULL, NULL, ".($fb_id != "" ? 1 : 0).", NULL, NULL, NULL, NULL, NULL, '".($username != "" ? $username : $fb_id)."')";
-           echo "query: $query";
+              `registered_from_language`, `is_creator`, `is_investor`, `is_idea_owner`, `is_idea_buyer`, `slug`)values ('$username', '$password', '$email', '', '', '', 1, '', 1, NULL, '".date("Y-m")."', ".($avatar_image == "" ? NULL : "'".$avatar_image."'").", '".$_SERVER['REMOTE_ADDR']."', NULL, NULL, NULL, NULL, NULL, NULL, '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."', 'person', '', '', '', NULL, '', '', '', NULL, '', '', '', NULL, 0, '', '0', NULL, NULL, '$fb_id', NULL, NULL, NULL, NULL, ".($fb_id != "" ? 1 : 0).", NULL, NULL, NULL, NULL, NULL, '".($username != "" ? $username : $fb_id)."')";
             $inserted = mysqli_query($this->db->getDb(), $query);
-            
+        
             if($inserted == 1){
-                $json['id'] = mysqli_insert_id();
-                $json['success'] = 1;
-                $json['message'] = "Амжилттай бүртгэгдлээ";
-                
+                $ajson['id'] = mysqli_insert_id();
+                $ajson['success'] = 1;
+                $ajson['message'] = "succesfilly registered.";
+             return $ajson;   
             }
-            
-            mysqli_close($this->db->getDb());
-            
         }
-        return $json;
+        $bjson['success'] = 0;
+        $bjson['message'] = "user already registered.";
+        return $bjson;
         
     }
     
