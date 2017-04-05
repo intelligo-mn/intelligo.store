e33fedec6be5a771ce6e1b7f21e67f55d0ef44ca
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-04-05 11:42:04 +0800

    product id, brand id аар бүтээгдэхүүн авахад зураг харуулах

--- app/ProductController.php
+++ app/ProductController.php
@@ -69,6 +69,7 @@ class ProductController{
 
         $products = array();
         while($product = $result->fetch_assoc()) {
+            $product["folder"] = $product["folder"]."/".$this->getPhotoPath($product["default_photo_id"]);
             $products[] = $product;
         }
 
@@ -84,6 +85,7 @@ class ProductController{
 
         $products = array();
         while($product = $result->fetch_assoc()) {
+            $product["folder"] = $product["folder"]."/".$this->getPhotoPath($product["default_photo_id"]);
             $products[] = $product;
         }
 
