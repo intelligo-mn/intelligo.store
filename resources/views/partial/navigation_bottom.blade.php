8e0805d7b36181f02e6278b45b704e762264a2b8
doljko <doljko927@gmail.com>
2017-04-07 02:40:57 +0800

    bottom navigation created

---
+++ resources/views/partial/navigation_bottom.blade.php
@@ -0,0 +1,13 @@
+<div class="header-bottom-content">
+    <div class="col-md-3 col-sm-2 col-xs-12">
+       <h3 class="full-category" id="full-category">Бүх ангилал<i class="fa fa-navicon"></i></h3> 
+    </div>
+    <div class="col-md-9 col-sm-10 col-xs-12">
+        <ul class="line-menu">
+            <li><a class="kk-1" href="{{ route('home') }}">Нүүр</a></li>
+            <li><a class="kk-1" href="{{ route('products') }}">Бүтээгдэхүүний жагсаалт</a></li>
+            <li><a class="kk-1" href="#">Танилцуулга</a></li>
+            <li><a class="kk-1" href="{{ route('tsag') }}">Цаг авах</a></li>
+        </ul>
+    </div>
+</div>
