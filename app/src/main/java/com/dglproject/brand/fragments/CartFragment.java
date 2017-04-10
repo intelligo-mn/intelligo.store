b4c3e58bf903d9423b931d0cf02a29608733ff00
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-04-10 12:50:32 +0800

    сагсанд хийсэн барааг fragment дээр  харуулсан

--- app/src/main/java/com/dglproject/brand/fragments/CartFragment.java
+++ app/src/main/java/com/dglproject/brand/fragments/CartFragment.java
@@ -8,11 +8,13 @@ import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.dglproject.brand.R;
 import com.dglproject.brand.database.CartProductsAdapter;
 import com.dglproject.brand.models.CartProducts;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -20,12 +22,23 @@ public class CartFragment extends Fragment {
 
     public final static String  TAG = "Cart";
 
+    CartProductsAdapter productAdapter;
+    TextView subTotalView;
+    TextView taxView;
+    TextView totalView;
+    double subTotal;
+    double tax;
+    double total;
+    View rootView;
+    ArrayList<CartProducts> data;
+
     public static CartFragment newInstance(int pageNo) {
 
         Bundle args = new Bundle();
         args.putInt(TAG, pageNo);
         CartFragment fragment = new CartFragment();
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -33,10 +46,13 @@ public class CartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_cart, container, false);
+        rootView = inflater.inflate(R.layout.fragment_cart, container, false);
 
-        final CartProductsAdapter productAdapter = new CartProductsAdapter(this.getActivity());
-        ArrayList<CartProducts> data = productAdapter.readData();
+        productAdapter = new CartProductsAdapter(this.getActivity());
+
+        data = productAdapter.readData();
+        displayBill();
+        ArrayList<CartProducts> datas = productAdapter.readData();
 
         ListView listView = (ListView) rootView.findViewById(R.id.fragment_item_ordered_list_view);
 
@@ -44,12 +60,12 @@ public class CartFragment extends Fragment {
 
         dataList = new ArrayList<HashMap<String, String>>();
 
-        for (CartProducts x : data) {
+        for (CartProducts x : datas) {
             HashMap<String, String> map = new HashMap<String, String>();
             map.put("Title", x.getTitle());
             map.put("Image", x.getImage());
-            map.put("TotalOrder", "Тоо: " + x.getTotalOrder());
-            map.put("TotalCost", "Нийт үнэ: " + x.getTotalCost()+" ₮");
+            map.put("TotalOrder", getString(R.string.total)+" :" + x.getTotalOrder());
+            map.put("TotalCost", getString(R.string.total_cost)+" :" + x.getTotalCost()+" ₮");
             dataList.add(map);
         }
 
@@ -79,5 +95,38 @@ public class CartFragment extends Fragment {
         productAdapter.close();
         return rootView;
 
+    }
+
+    public void calculateBill(){
+        subTotal = tax = total = 0;
+        for(CartProducts dat : data){
+            subTotal += dat.getTotalCost();
+        }
+        tax = subTotal*0.1;
+        total = tax + subTotal;
+    }
+
+    public void displayBill(){
+        calculateBill();
+        subTotalView = (TextView) rootView.findViewById(R.id.sub_total_text_view);
+        subTotalView.setText(getString(R.string.total_cost)+" :" + new DecimalFormat("#.##").format(subTotal)+" ₮");
+
+        taxView = (TextView) rootView.findViewById(R.id.tax_text_view);
+        taxView.setText(getString(R.string.tax)+" :" + new DecimalFormat("#.##").format(tax)+" ₮");
+
+        totalView = (TextView) rootView.findViewById(R.id.total_text_view);
+        totalView.setText(getString(R.string.total)+" :" + new DecimalFormat("#.##").format(total)+" ₮");
+    }
+
+    public void startPayment(View view){
+        if (data.size() > 0){
+//            Intent intent = new Intent(this, ActivityOrder.class);
+//            String subTotalString = "" + subTotal;
+//            String taxString = "" + tax;
+//            String totalString = "" + total;
+//            String[] transferData = {subTotalString,taxString,totalString};
+//            intent.putExtra(TAG,transferData);
+//            startActivity(intent);
+        }
     }
 }
