35eef4b77556c48917fbe3e93be2d78121483dd5
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-04-11 05:18:31 +0800

    get all category

--- CategoryService.php
+++ CategoryService.php
@@ -1,11 +1,12 @@
 <?php
     header('Content-Type: text/plain; charset=utf-8');
  
+	require_once 'app/CategoryController.php';
     require_once 'config/security.php';
     if(!isset($_GET['accesskey']) && (new DGLSecure())->generateAccessKey($_GET["accesskey"])) 
         die('accesskey required.');
-    if(!isset($_GET["state"]))
-        die('request state required.');
-
    
+   $categoryObject = new CategoryController();
+
+   echo json_encode($categoryObject->getAll());
 ?>
---
+++ app/CategoryController.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+header('Content-Type: text/plain; charset=utf-8');
+ 
+include_once 'config/db-connect.php';
+
+class CategoryController{
+    
+    private $db;
+    
+    private $db_table = "project_category";
+    
+    public function __construct(){
+        $this->db = new DbConnect();
+    }
+
+    public function getAll () {
+        $sql_query = "SELECT * 
+             FROM ".$this->db_table;
+        
+        $result = mysqli_query($this->db->getDb(), $sql_query) or die ("Error :".mysql_error());
+
+        $categories = array();
+        while($category = $result->fetch_assoc()) {
+            $categories[] = $category;
+        }
+
+        return $categories;
+        
+    }
+
+}
+?>
