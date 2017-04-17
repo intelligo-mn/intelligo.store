3447458f23481ff0d739b99164840434ff198b9a
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-04-17 11:42:19 +0800

    created new activity

---
+++ app/src/main/java/com/dglproject/brand/activity/ActivityAbout.java
@@ -0,0 +1,15 @@
+package com.dglproject.brand.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dglproject.brand.R;
+
+public class ActivityAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
