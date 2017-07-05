97f5d4f7400e9db69f2e16013629364bddfd8ebd
doljko <doljko927@gmail.com>
2017-07-06 03:01:53 +0800

    free product order and participant

---
+++ app/FreeProductOrder.php
@@ -0,0 +1,34 @@
+<?php
+namespace app;
+use App\Eloquent\Model;
+class FreeProductOrder extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'freeproduct_order';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'freeproduct_id',
+        'order_id',
+    ];
+    /**
+     * Obtains information order header.
+     *
+     * @return Collection Order Information
+     */
+    public function Orders()
+    {
+        return $this->hasMany('App\Order');
+    }
+    public function FreeProducts()
+    {
+        return $this->hasMany('App\FreeProduct');
+    }
+}
---
+++ app/FreeProductParticipant.php
@@ -0,0 +1,30 @@
+<?php
+namespace app;
+
+use App\Eloquent\Model;
+
+class FreeProductParticipant extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'freeproduct_participants';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'freeproduct_id',
+        'user_id',
+        'status',
+    ];
+    public function scopeMyParticipations($query, $freeproduct_id)
+    {
+        if (\Auth::user()) {
+            return $query->where('freeproduct_id', $freeproduct_id)->where('user_id', \Auth::user()->id);
+        }
+    }
+}
