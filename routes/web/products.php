e118a11540ba59997f80ba0d39098d5df52480ec
doljko <doljko927@gmail.com>
2017-07-13 03:04:20 +0800

    product route trim

--- routes/web/products.php
+++ routes/web/products.php
@@ -7,15 +7,11 @@ Route::get('search/', 'ProductsController@searchAll');
 Route::get('products/{id}', ['uses' => 'ProductsController@show', 'as' => 'products.show']);
 
 Route::resource('productsoffers', 'ProductOffersController');
-
-
 //free
 Route::get('freeproducts/{id}', ['uses' => 'FreeProductsController@show', 'as' => 'freeproducts.show']);
 
 Route::get('freeproducts/show/all', ['uses' => 'FreeProductsController@index', 'as' => 'freeproducts.search']);
 
 Route::resource('freeproductparticipants', 'FreeProductParticipantsController');
-
-
 //virtual
 Route::resource('virtualproducts', 'VirtualProductsController');
