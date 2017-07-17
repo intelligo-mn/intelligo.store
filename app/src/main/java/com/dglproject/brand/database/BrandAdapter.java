9cf2b5b83f1a052d3aae7007a9d115c8ec3e101e
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-07-17 16:21:23 +0800

    бүх брэндийн мэдээлэл баазаас авах, брэнд устгах, шинэчлэх

--- app/src/main/java/com/dglproject/brand/database/BrandAdapter.java
+++ app/src/main/java/com/dglproject/brand/database/BrandAdapter.java
@@ -4,8 +4,12 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.dglproject.brand.models.Brand;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Tortuvshin Byambaa on 7/17/2017.
@@ -78,5 +82,58 @@ public class BrandAdapter extends DatabaseHelper{
             cursor.moveToFirst();
         }
         return cursor;
+    }
+
+    public List<Brand> getAllBrands() {
+        List<Brand> brands = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_BRAND +" ORDER BY " +BRAND_NAME+" DESC";
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                long id = cursor.getLong(BRAND_ID_INDEX);
+                String name = cursor.getString(BRAND_NAME_INDEX);
+                String image = cursor.getString(BRAND_IMAGE_INDEX);
+                String desc = cursor.getString(BRAND_DESC_INDEX);
+                Brand brand1 = new Brand(id,name,image,desc);
+                brands.add(brand1);
+            } while (cursor.moveToNext());
+        }
+
+        Log.d(TAG, "" + brands);
+        cursor.close();
+        return brands;
+    }
+
+    public int updateBrand(Brand brand) {
+        if (brand == null) {
+            return -1;
+        }
+        SQLiteDatabase db = getWritableDatabase();
+        if (db == null) {
+            return -1;
+        }
+        ContentValues cv = new ContentValues();
+        cv.put(BRAND_ID, brand.getBrandId());
+        cv.put(BRAND_NAME, brand.getBrandName());
+        cv.put(BRAND_IMAGE, brand.getBrandImage());
+        cv.put(BRAND_DESCRIPTION, brand.getBrandDescription());
+
+        int rowCount = db.update(TABLE_BRAND, cv, BRAND_NAME + "=?",
+                new String[]{String.valueOf(brand.getBrandName())});
+        db.close();
+        return rowCount;
+    }
+
+    public void deleteBrand(Brand brand) {
+        if (brand == null) {
+            return;
+        }
+        SQLiteDatabase db = getWritableDatabase();
+        if (db == null) {
+            return;
+        }
+        db.delete(TABLE_BRAND, BRAND_NAME + "=?", new String[]{String.valueOf(brand.getBrandName())});
+        db.close();
     }
 }
