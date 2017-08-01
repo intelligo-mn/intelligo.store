f6fca2c9afb30f581b03c90a38b6a0416a6bb61e
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-08-01 13:08:19 +0800

    spinner adapter, adapter wrapper бичсэн

--- app/src/main/java/com/dglproject/brand/widgets/Spinner/SimpleSpinnerTextFormatter.java
+++ app/src/main/java/com/dglproject/brand/widgets/Spinner/SimpleSpinnerTextFormatter.java
@@ -1,9 +1,14 @@
 package com.dglproject.brand.widgets.Spinner;
 
+import android.text.Spannable;
+import android.text.SpannableString;
 /**
  * Created by turtuvshin on 7/31/17.
  */
 
-public class SimpleSpinnerTextFormatter {
+public class SimpleSpinnerTextFormatter implements SpinnerTextFormatter {
 
+    @Override public Spannable format(String text) {
+        return new SpannableString(text);
+    }
 }
--- app/src/main/java/com/dglproject/brand/widgets/Spinner/SpinnerAdapterWrapper.java
+++ app/src/main/java/com/dglproject/brand/widgets/Spinner/SpinnerAdapterWrapper.java
@@ -1,8 +1,31 @@
 package com.dglproject.brand.widgets.Spinner;
 
+import android.content.Context;
+import android.widget.ListAdapter;
+
 /**
  * Created by turtuvshin on 7/31/17.
  */
 
-public class SpinnerAdapterWrapper {
+public class SpinnerAdapterWrapper extends SpinnerBaseAdapter {
+
+    private final ListAdapter baseAdapter;
+
+    SpinnerAdapterWrapper(Context context, ListAdapter toWrap, int textColor, int backgroundSelector,
+                              SpinnerTextFormatter spinnerTextFormatter) {
+        super(context, textColor, backgroundSelector, spinnerTextFormatter);
+        baseAdapter = toWrap;
+    }
+
+    @Override public int getCount() {
+        return baseAdapter.getCount() - 1;
+    }
+
+    @Override public Object getItem(int position) {
+        return baseAdapter.getItem(position >= selectedIndex ? position + 1 : position);
+    }
+
+    @Override public Object getItemInDataset(int position) {
+        return baseAdapter.getItem(position);
+    }
 }
