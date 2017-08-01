b22e69a6fd7a27d0adf360b6270fdc980e7f1a59
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-08-01 13:04:04 +0800

    spinner base adapter, text formatter бичсэн

--- app/src/main/java/com/dglproject/brand/widgets/Spinner/SimpleSpinnerTextFormatter.java
+++ app/src/main/java/com/dglproject/brand/widgets/Spinner/SimpleSpinnerTextFormatter.java
@@ -5,4 +5,5 @@ package com.dglproject.brand.widgets.Spinner;
  */
 
 public class SimpleSpinnerTextFormatter {
+
 }
--- app/src/main/java/com/dglproject/brand/widgets/Spinner/SpinnerTextFormatter.java
+++ app/src/main/java/com/dglproject/brand/widgets/Spinner/SpinnerTextFormatter.java
@@ -4,5 +4,8 @@ package com.dglproject.brand.widgets.Spinner;
  * Created by turtuvshin on 7/31/17.
  */
 
-public class SpinnerTextFormatter {
+import android.text.Spannable;
+
+public interface SpinnerTextFormatter {
+    Spannable format(String text);
 }
