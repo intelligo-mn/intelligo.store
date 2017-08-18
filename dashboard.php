5a0458326d29d3b13bff54a7f1364ba40f2ac8f8
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-08-18 17:02:53 +0000

    views updated

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 
 	<?php include('views/header.php'); ?>
-	<?php include('views/menubar.php'); ?>
+	<?php include('views/left-sidebar.php'); ?>
 	<?php include('views/dashboard-menu.php'); ?>
 	<?php include('views/footer.php'); ?>
---
+++ views/navigation.php
@@ -0,0 +1,69 @@
+<!-- Search Bar -->
+    <div class="search-bar">
+        <div class="search-icon">
+            <i class="material-icons">search</i>
+        </div>
+        <input type="text" placeholder="ХАЙХ УТГАА БИЧ...">
+        <div class="close-search">
+            <i class="material-icons">close</i>
+        </div>
+    </div>
+    <!-- #END# Search Bar -->
+    <!-- Top Bar -->
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
+                <a href="javascript:void(0);" class="bars"></a>
+                <a class="navbar-brand" href="index.html">TECHSTAR DASHBOARD</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Call Search -->
+                    <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
+                    <!-- #END# Call Search -->
+                    <!-- Notifications -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                            <i class="material-icons">notifications</i>
+                            <span class="label-count">0</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="header">МЭДЭГДЭЛҮҮД</li>
+                            <li class="body">
+                                <ul class="menu">
+                                    
+                                </ul>
+                            </li>
+                            <li class="footer">
+                                <a href="javascript:void(0);">Мэдэгдэл байхгүй байна</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- #END# Notifications -->
+                    <!-- Tasks -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                            <i class="material-icons">flag</i>
+                            <span class="label-count">0</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="header">Ажил</li>
+                            <li class="body">
+                                <ul class="menu tasks">
+                                    
+                                </ul>
+                            </li>
+                            <li class="footer">
+                                <a href="javascript:void(0);">Ажил байхгүй байна</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- #END# Tasks -->
+                    <li class="pull-right"><a href="javascript:void(0);" class="js-right-sidebar" data-close="true"><i class="material-icons">more_vert</i></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- #Top Bar -->
+    
