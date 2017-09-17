2b0c29185affea433ab0f9ef76d87925bd337755
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-09-17 23:38:58 +0800

    хэрэглэгчийн профайл хэрэглэгч нэмэх хэсэг загвар

--- resources/views/_particles/footer.blade.php
+++ resources/views/_particles/footer.blade.php
@@ -1,87 +1,84 @@
-<footer id="footer">
-    <div class="container">
-       
-       <div class="row footer-holder">
-          <nav class="col-sm-4 col-lg-2 footer-nav active">
-             <h3>About Entrada</h3>
-             <ul class="slide">
-                <li><a href="#">The Company</a></li>
-                <li><a href="#">Our Values</a></li>
-                <li><a href="#">Responsiblity</a></li>
-                <li><a href="#">Our Mission</a></li>
-                <li><a href="#">Opportunity</a></li>
-                <li><a href="#">Safety Concerns</a></li>
-             </ul>
-          </nav>
-          <nav class="col-sm-4 col-lg-2 footer-nav">
-             <h3>Destinations</h3>
-             <ul class="slide">
-                <li><a href="#">Nepal</a></li>
-                <li><a href="#">Thailand</a></li>
-                <li><a href="#">Vietnam</a></li>
-                <li><a href="#">Fiji Island</a></li>
-                <li><a href="#">United States</a></li>
-                <li><a href="#">Australia</a></li>
-             </ul>
-          </nav>
-          <nav class="col-sm-4 col-lg-2 footer-nav">
-             <h3>themes</h3>
-             <ul class="slide">
-                <li><a href="#">Hiking and Camping</a></li>
-                <li><a href="#">Trekking Tours</a></li>
-                <li><a href="#">Jungle Safaris</a></li>
-                <li><a href="#">Bungee Jumping</a></li>
-                <li><a href="#">Wildlife &amp; Polar</a></li>
-                <li><a href="#">Peak Climbing</a></li>
-             </ul>
-          </nav>
-          <nav class="col-sm-4 col-lg-2 footer-nav">
-             <h3>reservation</h3>
-             <ul class="slide">
-                <li><a href="#">Booking Conditions</a></li>
-                <li><a href="#">My Bookings</a></li>
-                <li><a href="#">Refund Policy</a></li>
-                <li><a href="#">Includes &amp; Excludes</a></li>
-                <li><a href="#">Your Responsibilities</a></li>
-                <li><a href="#">Order a Brochure</a></li>
-             </ul>
-          </nav>
-          <nav class="col-sm-4 col-lg-2 footer-nav">
-             <h3>ask Entrada</h3>
-             <ul class="slide">
-                <li><a href="#">Why Entrada?</a></li>
-                <li><a href="#">Ask an Expert</a></li>
-                <li><a href="#">Safety Updates</a></li>
-                <li><a href="#">We Help When...</a></li>
-                <li><a href="#">Personal Matters</a></li>
-             </ul>
-          </nav>
-          <nav class="col-sm-4 col-lg-2 footer-nav last">
-             <h3>contact Entrada</h3>
-             <ul class="slide address-block">
-                <li class="wrap-text"><span class="icon-tel"></span> <a href="tel:02072077878">(020) 72077878</a></li>
-                <li class="wrap-text"><span class="icon-fax"></span> <a href="tel:02088828282">(020) 88828282</a></li>
-                <li class="wrap-text"><span class="icon-email"></span> <a href="https://www.waituk.net/cdn-cgi/l/email-protection#dbb2b5bdb49bbeb5afa9babfbaf5b8b4b6"><span class="__cf_email__" data-cfemail="30595e565f70555e44425154511e535f5d">[email&#160;protected]</span></a></li>
-                <li>
-                   <span class="icon-home"></span> 
-                   <address>707 London Road Isleworth, Middx TW7 7QD</address>
-                </li>
-             </ul>
-          </nav>
-       </div>
+
+
+<div class="clear"></div>
+
+
+<footer class="footer">
+    <div class="container" style="border:0 ; box-shadow: none;">
+        <div class="row">
+            <ul>
+                @foreach(\App\Pages::where('footer', '1')->get() as $page)
+                    <li> <a href="{{ action('PagesController@showpage', [$page->slug ]) }}" title="{{ $page->title }}">{{ $page->title }}</a></li>
+                @endforeach
+                    @if(getcong('p-buzzycontact') == 'on')
+                    <li> <a href="{{ action('ContactController@index') }}">{{ trans('buzzycontact.contact') }}</a></li>
+                    @endif
+            </ul>
+            <div class="col-1">
+
+
+                <div class="social-side" >
+
+                    @if(getcong('facebookpage'))<a target="_blank" href="{!!  getcong('facebookpage') !!}"><i class="fa fa-facebook-square"></i></a> @endif
+                    @if(getcong('twitterpage'))<a target="_blank" href="{!!  getcong('twitterpage') !!}"><i class="fa fa-twitter"></i></a>@endif
+                    @if(getcong('googlepage'))<a target="_blank" href="{!!  getcong('googlepage') !!}"><i class="fa fa-google-plus"></i></a>@endif
+                    @if(getcong('instagrampage'))<a target="_blank" href="{!!  getcong('instagrampage') !!}"><i class="fa fa-instagram"></i></a>@endif
+                    <a href="/index.xml"><i class="fa fa-rss"></i></a>
+
+                </div>
+
+            </div>
+
+
+
+
+                <div class="col-2">
+
+
+                </div>
+
+                <div class="col-3">
+
+                </div>
+
+        </div>
     </div>
-    <div class="footer-bottom">
-       <div class="container">
-          <div class="row">
-             <div class="col-lg-6">
-                <strong class="copyright"><i class="fa fa-copyright"></i>
+    <div class="clear"></div>
+</footer>
+<div class="clear"></div>
+<footer class="footer" style="margin:0;background:#F9F9F9;border-top:1px solid #f1f1f1">
+
+    <div class="container" style="border:0 ;box-shadow: none;">
+      <div class="row">
+
+
+            <div class="col-1">
+                <img class="site-logo" src="/assets/img/flogo.png" alt="">
+
+                @if(\Config::get('app.language')!=null)
+                    <div class="language-links hor">
+                        <a class="button button-white" href="javascript:">
+                            <i class="fa fa-globe"></i> <b>{{ \Config::get('app.language.'.$DB_USER_LANG)['name']  }}</b>
+                        </a>
+                        <ul class="sub-nav ">
+                            @foreach(\Config::get('app.language') as $key => $lang)
+                                <li>
+                                    <a href="{{ url('/selectlanguge/'.$key) }}" class="sub-item">{{ $lang['name'] }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="clear"></div>
+            </div>
+
+
+            <div class="col-3" style="padding-top:10px;">
                 {{ trans("updates.copyright") }}
-                </strong>
-             </div>
-             <div class="col-lg-6">
-              
-             </div>
-          </div>
-       </div>
+
+            </div>
+
+        </div>
+<div class="clear"></div>
     </div>
- </footer>
+</footer>
---
+++ resources/views/_particles/mainheader.blade.php
@@ -0,0 +1,98 @@
+<header id="header" class="white-header">
+   <div class="container-fluid">
+      <div class="logo">
+         <a href="index-2.html">
+         <img class="normal" src="travel/img/logos/logo.svg" alt="Entrada">
+         <img class="gray-logo" src="travel/img/logos/logo-gray.svg" alt="Entrada">
+         </a>
+      </div>
+      <nav class="navbar navbar-default">
+         <div class="navbar-header">
+            <button type="button" class="navbar-toggle nav-opener" data-toggle="collapse" data-target="#nav">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+         </div>
+         <div class="collapse navbar-collapse" id="nav">
+            <ul class="nav navbar-nav">
+               <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Home <b class="icon-angle-down"></b></a>
+                  
+               </li>
+               <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Tour Listing <b class="icon-angle-down"></b></a>
+                  
+               </li>
+               <li><a href="tour-detail.html">Tour Detail</a></li>
+               <li class="dropdown has-mega-dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Activities <b class="icon-angle-down"></b></a>
+                </li>
+               <li class="dropdown has-mega-dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <b class="icon-angle-down"></b></a>
+                  
+               </li>
+               <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="icon-angle-down"></b></a>
+                  
+               </li>
+               <li class="dropdown has-mega-dropdown mega-md">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Elements <b class="icon-angle-down"></b></a>
+                  
+               </li>
+               <li class="visible-xs visible-sm">
+                  <a href="login.html">
+                  <span class="icon icon-user"></span>
+                  <span class="text">Login</span>
+                  </a>
+               </li>
+               <li class="hidden-xs hidden-sm v-divider">
+                  <a href="login.html">
+                  <span class="icon icon-user"></span>
+                  </a>
+               </li>
+             
+               <li class="dropdown hidden-xs hidden-sm last-dropdown v-divider">
+                  <a href="#"><span class="text">EN</span> <span class="icon-angle-down"></span></a>
+                  <div class="dropdown-menu dropdown-sm">
+                     <div class="drop-wrap lang-wrap">
+                        <div class="lang-row">
+                           <div class="lang-col">
+                              <a href="#">
+                              <span class="text">English</span>
+                              </a>
+                           </div>
+                        </div>
+                        <div class="lang-row">
+                           <div class="lang-col">
+                              <a href="#">
+                              <span class="text">Монгол</span>
+                              </a>
+                           </div>
+                        </div>
+                        
+                     </div>
+                  </div>
+               </li>
+               <li class="visible-md visible-lg nav-visible v-divider"><a href="#" class="search-opener"><span class="icon icon-search"></span></a></li>
+            </ul>
+         </div>
+      </nav>
+   </div>
+   <form class="search-form" action="#">
+      <fieldset>
+         <a href="#" class="search-opener hidden-md hidden-lg">
+         <span class="icon-search"></span>
+         </a>
+         <div class="search-wrap">
+            <a href="#" class="search-opener close">
+            <span class="icon-cross"></span>
+            </a>
+            <div class="trip-form trip-form-v2 trip-search-main">
+              
+            </div>
+         </div>
+      </fieldset>
+   </form>
+</header>
