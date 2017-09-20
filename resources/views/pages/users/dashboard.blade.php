ff76ecc6d3b324e0dbb089b70a2d738e9790eef7
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-09-20 23:54:32 +0800

    хэрэглэгчийн профайл

--- resources/views/pages/users/dashboard.blade.php
+++ resources/views/pages/users/dashboard.blade.php
@@ -3,9 +3,9 @@
     <div class="profile-section-header">
         <a class="profile-section-item @if(Request::segment(3)=='') active @endif" href="/profile/{{ $userinfo->username_slug }}">{{ trans('index.all') }}</a>
         @if($DB_PLUGIN_NEWS == 'on')<a class="profile-section-item @if(Request::segment(3)=='news') active @endif" href="/profile/{{ $userinfo->username_slug }}/news">{{ trans('index.news') }}</a>@endif
-        @if($DB_PLUGIN_LISTS == 'on')<a class="profile-section-item @if(Request::segment(3)=='lists') active @endif" href="/profile/{{ $userinfo->username_slug }}/lists">{{ trans('index.lists') }}</a>@endif
+        <!-- @if($DB_PLUGIN_LISTS == 'on')<a class="profile-section-item @if(Request::segment(3)=='lists') active @endif" href="/profile/{{ $userinfo->username_slug }}/lists">{{ trans('index.lists') }}</a>@endif
         @if($DB_PLUGIN_QUIZS == 'on')<a class="profile-section-item @if(Request::segment(3)=='quizzes') active @endif" href="/profile/{{ $userinfo->username_slug }}/quizzes">{{ trans('buzzyquiz.quizzes') }}</a>@endif
-        @if($DB_PLUGIN_POLLS == 'on')<a class="profile-section-item @if(Request::segment(3)=='polls') active @endif" href="/profile/{{ $userinfo->username_slug }}/polls">{{ trans('index.polls') }}</a>@endif
+        @if($DB_PLUGIN_POLLS == 'on')<a class="profile-section-item @if(Request::segment(3)=='polls') active @endif" href="/profile/{{ $userinfo->username_slug }}/polls">{{ trans('index.polls') }}</a>@endif -->
         @if($DB_PLUGIN_VIDEOS == 'on')<a class="profile-section-item @if(Request::segment(3)=='videos') active @endif" href="/profile/{{ $userinfo->username_slug }}/videos">{{ trans('index.videos') }}</a>@endif
     </div>
     <div class="recent-activity">
