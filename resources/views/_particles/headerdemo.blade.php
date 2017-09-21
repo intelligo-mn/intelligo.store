c64beb9d0113f7dd9a2e6b292597072785a60780
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-09-21 23:52:52 +0800

    image assets bugfix

--- resources/views/_particles/headerdemo.blade.php
+++ resources/views/_particles/headerdemo.blade.php
@@ -26,44 +26,7 @@
     </div>
     
       <div class="navmenu login-button">
-        @if(Auth::check())
-            <li class="profile-info hor pull-r">
-                <a href="javascript:;" class="user-profile">
-                    <img src="{{ makepreview(Auth::user()->icon, 's', 'members/avatar') }}" width="32" height="32"  alt="{{ Auth::user()->username }}">
-                    <span class="name"><i class="fa fa-caret-down" style="margin-right:0;"></i> </span>
-                </a>
-                <ul class="sub-nav">
-                    <li>
-                        <strong style="display:block;padding:0 15px 10px 10px;font-size:16px;font-weight: 600">{{ Auth::user()->username }}</strong>
-                    </li>
-                    <li>
-                        <a class="sub-item" href="{{ action('UsersController@index', [ Auth::user()->username_slug ]) }}">{{ trans('index.myprofile') }}</a>
-                    </li>
-                    <li>
-                        <a class="sub-item" href="{{ action('UsersController@followfeed', ['id' => Auth::user()->username_slug ]) }}">{{ trans('updates.feedposts') }}</a>
-                    </li>
-                    <li>
-                        <a class="sub-item" href="{{ action('UsersController@draftposts', ['id' => Auth::user()->username_slug ]) }}">{{ trans('index.draft') }}</a>
-                    </li>
-                    <li>
-                        <a class="sub-item" href="{{ action('UsersController@deletedposts', ['id' => Auth::user()->username_slug ]) }}">{{ trans('index.trash') }}</a>
-                    </li>
-                    <li>
-                        <a class="sub-item" href="{{ action('UsersController@updatesettings', ['id' => Auth::user()->username_slug ]) }}">{{ trans('index.settings') }}</a>
-                    </li>
-                    @if(Auth::user()->usertype=='Admin')
-                        <li>
-                            <a class="sub-item" href="/admin">{{ trans('index.adminp') }}</a>
-                        </li>
-                    @endif
-                    <li>
-                        <a class="sub-item" href="{{ action('Auth\AuthController@logout') }}">{{ trans('index.logout') }}</a>
-                    </li>
-
-                </ul>
-                <div class="clear"></div>
-            </li>
-        @endif
+        c
 
     </div>
 </header>
