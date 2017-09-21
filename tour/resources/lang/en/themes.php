e2929f6231bbedd111d09fa606c61504e407e03e
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-09-21 22:59:08 +0800

    rm tour

--- tour/resources/lang/en/themes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-
-    'themes' => 'Themes',
-
-
-);
--- tour/resources/views/errors/swalerror.blade.php
+++
@@ -1,9 +0,0 @@
-@if (Session::has('success.message'))
-    <script>
-        swal({   title: "{{  trans('updates.success') }}"  , text: "{{ Session::get('success.message') }}", type: "success",  html: true });
-    </script>
-    @elseif (Session::has('error.message'))
-    <script>
-        swal({   title: "{{  trans('updates.error') }}", text: "{{ Session::get('error.message') }}", type: "error",  html: true });
-    </script>
-@endif
