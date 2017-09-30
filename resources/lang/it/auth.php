8bc76b01ad83dfcbe30d0b547251d6ab1d417c0f
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-01 01:20:25 +0800

    орос хэлний файлууд

---
+++ resources/lang/it/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Queste credenziali non corrispondono nei nostri record.',
+    'throttle' => 'troppi tentativi di accesso. Riprova tra :seconds seconds.',
+
+    'congratz' => 'Congratulazioni',
+    'joined' => 'Grazie per esserti unito a noi',
+    'logined' => 'Login Sei Entrato con Successo.',
+
+    'cantgetemail' => 'Non possiamo ottenere il vostro indirizzo e-mail! Si prega di registrarsi con il vostro indirizzo e-mail.',
+
+
+];
