21f6f587ec65a0f1037d4fd2864779f769246467
doljko <doljko927@gmail.com>
2017-10-23 00:13:02 +0800

    contact controller where mail cat

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,12 +18,14 @@ class ContactController extends Controller
         parent::__construct();
     }
 
+
     public function index()
     {
         $labels=Categories::byType('maillabel')->lists('name','id' );
 
         return view('_contact.contactpage', compact('labels'));
     }
+
 
     public function create(Request $request)
     {
@@ -44,25 +46,25 @@ class ContactController extends Controller
             return redirect()->back()->withInput($ll);
         }
 
-        if(getcong('BuzzyContactCopyEmail') > ""){
+        if(getenvcong('BuzzyContactCopyEmail') > ""){
             if(!isset($ll['g-recaptcha-response'])){
-                \Session::flash('error.message', trans('contact.yourresponseincorrect'));
+                \Session::flash('error.message', trans('buzzycontact.yourresponseincorrect'));
                 return redirect()->back()->withInput($ll);
             }
 
-            $content = curlit('https://www.google.com/recaptcha/api/siteverify?secret='.getcong('reCaptchaSecret').'&response='.$ll['g-recaptcha-response'].'&remoteip='.$_SERVER['REMOTE_ADDR']);
+            $content = curlit('https://www.google.com/recaptcha/api/siteverify?secret='.getenvcong('reCaptchaSecret').'&response='.$ll['g-recaptcha-response'].'&remoteip='.$_SERVER['REMOTE_ADDR']);
 
             $res= json_decode($content, true);
 
             if($res['success'] == false){
-                \Session::flash('error.message', trans('contact.yourresponseincorrect'));
+                \Session::flash('error.message', trans('buzzycontact.yourresponseincorrect'));
                 return redirect()->back()->withInput($ll);
             }
 
             $this->composesubject=$ll['subject'];
             $this->fromemail=$ll['email'];
-            $this->composeto = getcong('siteemail');
-            $this->sitename = getcong('sitename');
+            $this->composeto = getenvcong('BuzzyContactCopyEmail')>"" ? getenvcong('BuzzyContactCopyEmail') : getenvcong('siteemail');
+            $this->sitename =getenvcong('BuzzyContactName')>"" ? getenvcong('BuzzyContactName') : getenvcong('sitename');
 
 
             $this->mail->send('_contact.emails.mailbox', array('body' => $ll['text']), function($message)
@@ -75,11 +77,15 @@ class ContactController extends Controller
             });
         }
 
+
+        $cat=Categories::byType('mailcat')->where('name_slug', 'inbox')->first();
         $newrecord= new Contacts;
         $newrecord->name=$ll['name'];
         $newrecord->email=$ll['email'];
         $newrecord->subject=$ll['subject'];
         $newrecord->text=$ll['text'];
+        $newrecord->category_id=$cat->id;
+        $newrecord->label_id=$ll['label'];
         $newrecord->read=0;
         $newrecord->save();
 
