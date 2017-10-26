2831abd2c3d8551dafce15604719babb0475676f
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-27 03:19:59 +0800

    created single post left recent post

--- resources/views/coolmon/pages/post.blade.php
+++ resources/views/coolmon/pages/post.blade.php
@@ -94,85 +94,31 @@
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                     <!-- Blog Single Sidebar Start Here -->
                     <div class="sidebar-area">
-                        <div class="like-box-area">
-                            <ul>
-                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> <span class="like-page">like our facebook page <br/>210,956 likes</span> <span class="like"><i class="fa fa-plus" aria-hidden="true"></i></span></a></li>
-                                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> <span class="like-page">Follow us on twitter<br/>2109 followers</span> <span class="like">
-                                <i class="fa fa-plus" aria-hidden="true"></i></span></a></li>
-                                <li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i> <span class="like-page">Subscribe to our rss <br/>210,956 likes</span> <span class="like"><i class="fa fa-plus" aria-hidden="true"></i></span></a></li>
-                            </ul>
-                        </div>
                         <div class="recent-post-area hot-news">
                             <h3 class="title-bg">Recent Post</h3>
                             <ul class="news-post">
+                               @if($lastNews)
+                                @foreach($lastNews as $key=>$item)
                                 <li>
                                     <div class="row">
                                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 content">
                                             <div class="item-post">
                                                 <div class="row">
                                                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 paddimg-right-none">
-                                                        <a href="blog-single.html"><img src="images/popular/1.jpg" alt="" title="News image" /></a>
+                                                        <a href="{{ makeposturl($item) }}"><img src="{{ makepreview($item->thumb, 's', 'posts') }}" alt="" title="{{ $item->title }}" /></a>
                                                     </div>
                                                     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                        <h4><a href="blog-single.html"> US should prepare for<br/> Russian election</a></h4>
-                                                        <span class="date"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> June 28, 2017</span>
+                                                        <h4><a href="{{ makeposturl($item) }}"> {{ $item->title }}</a></h4>
+                                                        <span class="date"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>{{ $item->created_at->diffForHumans() }}</span>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </li>
-                                  <li>
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 content">
-                                            <div class="item-post">
-                                                <div class="row">
-                                                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 paddimg-right-none">
-                                                        <a href="blog-single.html"><img src="images/popular/2.jpg" alt="" title="News image" /></a>
-                                                    </div>
-                                                    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                        <h4><a href="blog-single.html">Pellentesque Odio Nisi <br/>Euismod In Pharet</a></h4>
-                                                        <span class="date"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>June 28, 2017</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                  <li>
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 content">
-                                            <div class="item-post">
-                                                <div class="row">
-                                                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 paddimg-right-none">
-                                                        <a href="blog-single.html"><img src="images/popular/3.jpg" alt="" title="News image" /></a>
-                                                    </div>
-                                                    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                        <h4><a href="blog-single.html"> Erant Aeque Neius No <br/>Numes Electram</a></h4>
-                                                        <span class="date"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> June 28, 2017</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                  <li>
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 content">
-                                            <div class="item-post">
-                                                <div class="row">
-                                                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 paddimg-right-none">
-                                                        <a href="blog-single.html"><img src="images/popular/4.jpg" alt="" title="News image" /></a>
-                                                    </div>
-                                                    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                                        <h4><a href="blog-single.html">Erant Aeque Neius No <br/>Numes Electram</a></h4>
-                                                        <span class="date"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>June 28, 2017</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
+                               
+                                @endforeach
+                              @endif
                             </ul>
                         </div>
                     
