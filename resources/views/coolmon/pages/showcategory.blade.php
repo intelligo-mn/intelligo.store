59c62c39d183161d0e24e374b0ded6fc70b99a3c
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-27 02:42:33 +0800

    category items des

--- resources/views/coolmon/pages/showcategory.blade.php
+++ resources/views/coolmon/pages/showcategory.blade.php
@@ -2,47 +2,61 @@
 @section('head_title', $category->name .' | '.getcong('sitename') )
 @section('head_description', $category->description )
 @section("content")
-<section class="banner banner-inner parallax" data-stellar-background-ratio="0.5" id="gridview-3-col">
-   <div class="banner-text">
-      <div class="center-text">
-         <div class="container">
-            <h1>{{ $category->name }}</h1>
-            <strong class="subtitle">{{ $category->name }}</strong>
-            <nav class="breadcrumbs">
-               <ul>
-                  <li><a href="#">{{ $category->name }}</a></li>
-                  
-                    @foreach(\App\Categories::where('type', $category->id)->orderBy('name')->groupBy('name')->get() as $cat)
-
-                        <li><a data-type="{{ $cat->name_slug }}" href="/{{ $cat->name_slug }}">{{ $cat->name }}</a></li>
-                    @endforeach
-                  
-               </ul>
-            </nav>
-         </div>
-      </div>
-   </div>
-</section>
-<main id="main">
-   <div class="content-block content-sub">
-      <div class="container">
-         
-         <div class="content-holder content-sub-holder">
-               @if($lastNews)
-                    <div class="row db-3-col">
-                      @foreach($lastNews as $key=>$item)
-                         @include('._particles._lists.travel_list', ['listtype' => 'big_image small-h bolb titm','featuredon' => 'on', 'descof' => 'on','linkcolor' => 'default'])
-
-                          @if($key ==0 )
-                              @foreach(\App\Widgets::where('type', 'Homencolsec')->where('display', 'on')->get() as $widget)
-                                  {!! $widget->text !!}
-                              @endforeach
-                          @endif
-                      @endforeach
+  <div class="inner-page-header">
+        <div class="banner">
+            <img src="cooltheme/images/banner/3.jpg" alt="Banner">
+        </div>
+        <div class="banner-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="header-page-locator">
+                            <ul>
+                                <li><a href="index.html">Home <i class="fa fa-compress" aria-hidden="true"></i> </a> Blog Post</li>
+                            </ul>
+                        </div>
+                        <div class="header-page-title">
+                            <h1>Blog</h1>
+                        </div>
+                        <div class="header-page-subtitle">
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                                <br>alteration in some form, by injected humou</p>
+                        </div>
                     </div>
-                @endif
-         </div>
-      </div>
-   </div>
-</main>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Inner Page Header serction end here -->
+ 
+   <!-- Blog Page Start Here -->
+    <div class="blog-page-area">
+        <div class="container">
+            @if($lastNews)
+                  @foreach($lastNews as $key=>$item)
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                      <ul>
+                          <li>
+                              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                  <div class="blog-image">
+                                      <a href="#">
+                                          <i class="fa fa-link" aria-hidden="true"></i>
+                                          <img src="{{ makepreview($item->thumb, 's', 'posts') }}" alt="{{ $item->title }}" srcset="{{ makepreview($item->thumb, 's', 'posts') }}">
+                                      </a>
+                                  </div>
+                              </div>
+                              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                  <span class="date"> <i class="fa fa-calendar-check-o" aria-hidden="true"> </i>{{ $item->created_at->diffForHumans() }}</span>
+                                  <h3><a href="#">{{ str_limit($item->title, 45) }}</a></h3>
+                                  <span class="admin"><a href="#"><i class="fa fa-user-o" aria-hidden="true"></i>  {{ $item->user->username }}</a></span> <span class="like"><a href="#"><i class="fa fa-comment-o" aria-hidden="true"></i>  12 </a></span>
+                                  <p>{{ str_limit($item->body, 100) }}</p>
+                                  <a href="{{ makeposturl($item) }}" class="more">{{ trans('index.showmore') }}<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                              </div>
+                          </li>
+                      </ul>                          
+                  </div>
+              @endforeach
+            @endif
+        </div>
+    </div>
 @endsection
