884a6a28c6cc342c1fd98cd36e3259484864eabd
N0M@D <se12d037@gmail.com>
2017-10-30 04:42:09 +0800

    footer updated

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -7,17 +7,9 @@
         </div>
 
         <div class="col-xs-4 col-sm-4 col-md-4 menu">
-            <h3 class="widget-title heading-title">Холбоо барих</h3>
+            <h3 class="widget-title heading-title">Contact Us</h3>
             <div class="line line-30"></div>
 
-            <ul>
-                <li><i class="fa fa-map-marker"></i> <span class="footertext">  Сүхбаатар дүүрэг, 1-р хороо, Чингисийн өргөн чөлөө-14251, Гэрэгэ тауэр 4-р давхар</span>
-                </li>
-                <li><i class="fa fa-phone"></i> <span class="footertext">  +976 8999 2681</span>
-                </li>
-                <li><i class="fa fa-envelope"> </i><span class="footertext"> Bolormaa_345@yahoo.com</span>
-                </li>
-            </ul>
         </div>
 
         <div class="col-xs-4 col-sm-4 col-md-4 newsletter">
@@ -30,7 +22,7 @@
                     <li>
                         <div class="social-icons">
                             <ul>
-                                <li class="icon-facebook fa-lg"><a href="https://www.facebook.com/pg/Make-up-artist-Bella-451092605001683/videos/?ref=page_internal" target="_blank" title="Become our fan"><i class="fa fa-facebook"></i></a>
+                                <li class="icon-facebook fa-lg"><a href="https://www.facebook.com/" target="_blank" title="Become our fan"><i class="fa fa-facebook"></i></a>
                                 </li>
 
                                 <li class="icon-twitter fa-lg"><a href="http://twitter.com/#" target="_blank" title="Follow us"><i class="fa fa-twitter"></i></a>
@@ -39,7 +31,7 @@
                                 <li class="icon-google fa-lg"><a href="https://plus.google.com/u/0/#" target="_blank" title="Get updates"><i class="fa fa-google-plus"></i></a>
                                 </li>
 
-                                <li class="icon-instagram fa-lg"><a href="https://www.instagram.com/isabellabellasmakeup/" target="_blank" title="Follow us"><i class="fa fa-instagram"></i></a>
+                                <li class="icon-instagram fa-lg"><a href="https://www.instagram.com/" target="_blank" title="Follow us"><i class="fa fa-instagram"></i></a>
                                 </li>                            
                             </ul>
                         </div>
@@ -54,7 +46,7 @@
 
         <div id="copy-right" class="copy-right col-sm-12">
             <div class="col-sm-8">
-                Зохиогчын эрх хуулиар хамгаалагдсан ©. 2016 он. Вэбсайтыг хөгжүүлсэн "Тагтаа Солюшн" ХХК.
+                Copyright ©. 2017 All Right Reserved. Techstar Agency.
             </div>
 
         </div>
