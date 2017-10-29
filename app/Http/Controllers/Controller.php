8efb2b8b059a0407d87a8c66e549b06f75990497
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-30 05:28:08 +0800

    theme lib removed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use YAAP\Theme\Facades\Theme;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
