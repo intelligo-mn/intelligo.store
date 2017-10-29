428d4d97bf0a96b7e3c6b492f1dfd90392044618
doljko <doljko927@gmail.com>
2017-10-30 02:33:56 +0800

    register window created

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,86 +8,26 @@
 @include('partial.message')
 
 @section('content')
+<div class="login-page">
+  <div class="login">
 
-	<div class="content_wrapper_header">
-		<h3>
-			<a href="/" title="{{ trans('globals.go_back_label') }}">
-				{{ trans('user.set_up_new_account') }}
-			</a>
-		</h3>
-	</div>
+    {!! Form::open(['url'=>'/register', 'class'=>'form-horizontal', 'role'=>'form']) !!}
 
-<div class="content_wrapper">
-	<div class="row">
-		<div class="col-md-12">
+    <form class="login-form">
+      <input type="text" placeholder="{!! trans('user.first_name') !!}" name="first_name" />
+      <input type="text" placeholder="{!! trans('user.last_name') !!}" name="last_name"/>
+      <input type="email" placeholder="{!! trans('user.email') !!}" name="email" />
+      <input type="password" placeholder="{!! trans('user.password') !!}" name="password" />
 
-				{!! Form::open(['url'=>'/register', 'class'=>'form-horizontal', 'role'=>'form']) !!}
-
-					<div class="form-group" id="business_div" style="display:none;">
-						{!! Form::label('business_name',trans('user.business_name')) !!}
-						{!! Form::text('business_name', null, ['ng-disabled'=>'disabled','class'=>'form-control']) !!}
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-6 col-lg-6 ">
-							{!! Form::label('first_name',trans('user.first_name')) !!}:
-							<div class="input-group">
-				      			<div class="input-group-addon"><span class="fa fa-align-justify"></span></div>
-								{!! Form::text('first_name', null, ['ng-disabled'=>'disabled','class'=>'form-control']) !!}
-							</div>
-						</div>
-						<div class="col-md-6 col-lg-6 ">
-							{!! Form::label('last_name',trans('user.last_name')) !!}:
-							<div class="input-group">
-				      			<div class="input-group-addon"><span class="fa fa-align-justify"></span></div>
-								{!! Form::text('last_name', null, ['ng-disabled'=>'disabled','class'=>'form-control']) !!}
-							</div>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-12">
-							{!! Form::label('email',trans('user.email')) !!}:
-							<div class="input-group">
-					      		<div class="input-group-addon"><span class="fa fa-envelope"></span></div>
-								{!! Form::email('email', $email, ['ng-disabled'=>'disabled','class'=>'form-control']) !!}
-							</div>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-12">
-							{!! Form::label('password',trans('user.password')) !!}:
-							<div class="input-group">
-						      	<div class="input-group-addon"><span class="fa fa-lock"></span></div>
-								{!! Form::password('password', ['class'=>'form-control']) !!}
-							</div>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-md-12">
-							<label>{{ trans('user.are_you_human') }}</label>
-							{!! Recaptcha::render() !!}
-						</div>
-					</div>
-
-					<div class="form-group">
-						<div class="col-md-12">
-							<hr>
-							<div class="btn-group" role="group">
-								<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-send"></span>&nbsp;{{ trans('user.create_my_account') }}</button>
-								<a href="/login" class="btn btn-info"><span class="fa fa-sign-in"></span>&nbsp;{{  trans('user.sign_in_my_account') }}</a>
-							</div>
-						</div>
-					</div>
-
-				{!! Form::close() !!}
-
-		</div>
-	</div>
+      <!-- {!! Recaptcha::render() !!} -->
+      <button type="submit"> {{ trans('user.create_my_account') }}</button>
+      <button class="btn-back">Буцах</button>
+      <!-- нүүр хуудаслуу буцах товч-->
+	  <p class="message"><a href="/login">{{  trans('user.sign_in_my_account') }}</a></p>
+    </form>
+    {!! Form::close() !!}
+  </div>
 </div>
-
 @endsection
 
 @section('footer')
