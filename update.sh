d5af2c6e8e3b4ffe051cc8738448ef2a4572536f
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-11-01 09:37:21 +0800

    create update sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash;
+echo "Updating...";
+
+echo "Install project dependencies...";
+composer install
+npm install
+npm run dev
+
+echo "Migrate and seed the database...";
+php artisan migrate:refresh
+php artisan db:seed
+
+echo "You are ready to go!";
