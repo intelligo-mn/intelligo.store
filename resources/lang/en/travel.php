80f0271456f14059d67fd97ce48d5229ef08e895
tortuvshin <toroo.byamba@gmail.com>
2017-11-28 12:55:19 +0800

    хэлний файл дээр холбоо барих орчуулгууд

--- resources/lang/en/travel.php
+++ resources/lang/en/travel.php
@@ -1,5 +1,14 @@
 <?php
 
 return array(
+	
+	// Холбоо барих мэдээлэл
+	'email'                   => 'E-mail',
+	'email_text'                   => 'info@traveltomongolia.net
+	contact@traveltomongolia.net',
+	'mobile'                   => 'Tel',
+	'mobile_text'                   => '(+976) 89075555, 97015555',
+	'website'                   => 'Web site',
+	'website_text'                   => 'www.traveltomongolia.net',
 
 );
--- resources/lang/zh/travel.php
+++ resources/lang/zh/travel.php
@@ -1,5 +1,14 @@
 <?php
 
 return array(
+	
+	// Холбоо барих мэдээлэл
+	'email'                   => '电子邮箱',
+	'email_text'                   => 'jackyddon@126.com
+	traveltomongolia@163.com',
+	'mobile'                   => '联系电话',
+	'mobile_text'                   => '(00976) 89075555, 97015555',
+	'website'                   => '网址',
+	'website_text'                   => 'www.traveltomongolia.net',
 
 );
