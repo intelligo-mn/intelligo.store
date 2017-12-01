13b35e77c1b337c42988efd1b50319add6446802
tortuvshin <toroo.byamba@gmail.com>
2017-12-01 18:00:38 +0800

    updated header blade

--- resources/views/uriankhai/_particles/header.blade.php
+++ resources/views/uriankhai/_particles/header.blade.php
@@ -1,69 +1,69 @@
- <header class="header--section header--style-1">
-            <div class="header--topbar bg--color-2">
-                <div class="container">
-                    <div class="float--left float--xs-none text-xs-center">
-                        <ul class="header--topbar-info nav">
-                            <li><i class="fa fm fa-map-marker"></i>New York</li>
-                            <li><i class="fa fm fa-mixcloud"></i>21<sup>0</sup> C</li>
-                            <li><i class="fa fm fa-calendar"></i>Today (Sunday 19 April 2017)</li>
-                        </ul>
-                    </div>
-                    <div class="float--right float--xs-none text-xs-center">
-                        <ul class="header--topbar-action nav">
-                            <li><a href="/login"><i class="fa fm fa-user-o"></i>Login/Register</a></li>
-                        </ul>
-                        <ul class="header--topbar-lang nav">
-                            <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fm fa-language"></i>English<i class="fa flm fa-angle-down"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">English</a></li>
-                                    <li><a href="#">Spanish</a></li>
-                                    <li><a href="#">French</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <ul class="header--topbar-social nav hidden-sm hidden-xxs">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a href="#"><i class="fa fa-rss"></i></a></li>
-                            <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
+<header class="header--section header--style-1">
+    <div class="header--topbar bg--color-2">
+        <div class="container">
+            <div class="float--left float--xs-none text-xs-center">
+                <ul class="header--topbar-info nav">
+                    <li><i class="fa fm fa-map-marker"></i>New York</li>
+                    <li><i class="fa fm fa-mixcloud"></i>21<sup>0</sup> C</li>
+                    <li><i class="fa fm fa-calendar"></i>Today (Sunday 19 April 2017)</li>
+                </ul>
             </div>
-            <div class="header--mainbar">
-                <div class="container">
-                    <div class="header--logo float--left float--sm-none text-sm-center">
-                        <h1 class="h1"> <a href="home-1.html" class="btn-link"> <img src="uriankhai/img/logo.png" alt="USNews Logo"> <span class="hidden">USNews Logo</span> </a> </h1> </div>
-                    <div class="header--ad float--right float--sm-none hidden-xs">
-                        <a href="#"> <img src="uriankhai/img/ads-img/ad-728x90-01.jpg" alt="Advertisement"> </a>
-                    </div>
-                </div>
-            </div>
-            <div class="header--navbar style--1 navbar bd--color-1 bg--color-1" data-trigger="sticky">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#headerNav" aria-expanded="false" aria-controls="headerNav"> <span class="sr-only">Toggle Navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                    </div>
-                    <div id="headerNav" class="navbar-collapse collapse float--left">
-                        <ul class="header--menu-links nav navbar-nav" data-trigger="hoverIntent">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="world-news.html">Worlds News</a></li>
-                            <li><a href="national.html">National</a></li>
-                            <li><a href="financial.html">Financial</a></li>
-                            <li><a href="entertainment.html">Entertainment</a></li>
-                            <li><a href="lifestyle.html">Lifestyle</a></li>
-                            <li><a href="technology.html">Technology</a></li>
-                            <li><a href="travel.html">Travel</a></li>
-                            <li><a href="sports.html">Sports</a></li>
-                            <li><a href="#">Catagory</a></li>
-                            <li><a href="#">Pages</a></li>
+            <div class="float--right float--xs-none text-xs-center">
+                <ul class="header--topbar-action nav">
+                    <li><a href="/login"><i class="fa fm fa-user-o"></i>Login/Register</a></li>
+                </ul>
+                <ul class="header--topbar-lang nav">
+                    <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fm fa-language"></i>English<i class="fa flm fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">English</a></li>
+                            <li><a href="#">Spanish</a></li>
+                            <li><a href="#">French</a></li>
                         </ul>
-                    </div>
-                    <form action="#" class="header--search-form float--right" data-form="validate">
-                        <input type="search" name="search" placeholder="Search..." class="header--search-control form-control" required>
-                        <button type="submit" class="header--search-btn btn"><i class="header--search-icon fa fa-search"></i></button>
-                    </form>
-                </div>
+                    </li>
+                </ul>
+                <ul class="header--topbar-social nav hidden-sm hidden-xxs">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                    <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+                </ul>
             </div>
-        </header>
+        </div>
+    </div>
+    <div class="header--mainbar">
+        <div class="container">
+            <div class="header--logo float--left float--sm-none text-sm-center">
+                <h1 class="h1"> <a href="home-1.html" class="btn-link"> <img src="uriankhai/img/logo.png" alt="USNews Logo"> <span class="hidden">USNews Logo</span> </a> </h1> </div>
+            <div class="header--ad float--right float--sm-none hidden-xs">
+                <a href="#"> <img src="uriankhai/img/ads-img/ad-728x90-01.jpg" alt="Advertisement"> </a>
+            </div>
+        </div>
+    </div>
+    <div class="header--navbar style--1 navbar bd--color-1 bg--color-1" data-trigger="sticky">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#headerNav" aria-expanded="false" aria-controls="headerNav"> <span class="sr-only">Toggle Navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+            </div>
+            <div id="headerNav" class="navbar-collapse collapse float--left">
+                <ul class="header--menu-links nav navbar-nav" data-trigger="hoverIntent">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="world-news.html">Worlds News</a></li>
+                    <li><a href="national.html">National</a></li>
+                    <li><a href="financial.html">Financial</a></li>
+                    <li><a href="entertainment.html">Entertainment</a></li>
+                    <li><a href="lifestyle.html">Lifestyle</a></li>
+                    <li><a href="technology.html">Technology</a></li>
+                    <li><a href="travel.html">Travel</a></li>
+                    <li><a href="sports.html">Sports</a></li>
+                    <li><a href="#">Catagory</a></li>
+                    <li><a href="#">Pages</a></li>
+                </ul>
+            </div>
+            <form action="#" class="header--search-form float--right" data-form="validate">
+                <input type="search" name="search" placeholder="Search..." class="header--search-control form-control" required>
+                <button type="submit" class="header--search-btn btn"><i class="header--search-icon fa fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+</header>
