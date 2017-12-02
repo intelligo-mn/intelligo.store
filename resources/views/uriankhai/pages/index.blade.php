0e6cf7abda99a0868434612ea154f1679630ea86
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-02 23:10:10 +0800

    header top news

--- resources/views/uriankhai/pages/index.blade.php
+++ resources/views/uriankhai/pages/index.blade.php
@@ -46,7 +46,7 @@
                     
                     @foreach($lastFeaturestop->slice(0,1) as $item)
                         
-                        <div class="col-md-6">
+                        <div class="col-md-7">
                             <div class="post--item post--layout-1 post--title-larger">
                                 <div class="post--img">
                                     <a href="{{ makeposturl($item) }}" class="thumb"><img src="{{ makepreview($item->thumb, 'b', 'posts') }}" alt="{{ $item->title }}"></a> <a href="#" class="cat">Politics</a> <a href="#" class="icon"><i class="fa fa-flash"></i></a>
@@ -69,59 +69,46 @@
                         </div>
 
                     @endforeach
-                    <div class="col-md-6">
+                    <div class="col-md-5">
                         <div class="row gutter--15">
-                            <div class="col-xs-6 col-xss-12">
-                                <div class="post--item post--layout-1 post--title-large">
-                                    <div class="post--img">
-                                        <a href="news-single-v1.html" class="thumb"><img src="uriankhai/img/home-img/banner-02.jpg" alt=""></a> <a href="#" class="cat">Travel</a> <a href="#" class="icon"><i class="fa fa-flash"></i></a>
-                                        <div class="post--info">
-                                            <ul class="nav meta">
-                                                <li><a href="#">Corey I. Dean</a></li>
-                                                <li><a href="#">20 April 2017</a></li>
-                                            </ul>
-                                            <div class="title">
-                                                <h2 class="h4"><a href="news-single-v1.html" class="btn-link">Lorem Ipsum is simply dummy text of the printing</a></h2> </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xs-6 hidden-xss">
-                                <div class="post--item post--layout-1 post--title-large">
-                                    <div class="post--img">
-                                        <a href="news-single-v1.html" class="thumb"><img src="uriankhai/img/home-img/banner-03.jpg" alt=""></a> <a href="#" class="cat">Education</a> <a href="#" class="icon"><i class="fa fa-flash"></i></a>
-                                        <div class="post--map">
-                                            <p class="btn-link"><i class="fa fa-map-o"></i>Location in Google Map</p>
-                                            <div class="map--wrapper">
-                                                <div data-map-latitude="23.790546" data-map-longitude="90.375583" data-map-zoom="6" data-map-marker="[[23.790546, 90.375583]]"></div>
+                            
+                            @foreach($lastFeaturestop->slice(1,2) as $item)
+                                <div class="col-xs-6 col-xss-12">
+                                    <div class="post--item post--layout-1 post--title-large">
+                                        <div class="post--img">
+                                            <a href="{{ makeposturl($item) }}" class="thumb"><img src="{{ makepreview($item->thumb, 'b', 'posts') }}" alt=""></a> <a href="#" class="cat">Travel</a> <a href="#" class="icon"><i class="fa fa-flash"></i></a>
+                                            <div class="post--info">
+                                                <ul class="nav meta">
+                                                    <li><a href="#">Corey I. Dean</a></li>
+                                                    <li>
+                                                        <a href="#">{{ $item->created_at->diffForHumans() }}</a>
+                                                    </li>
+                                                </ul>
+                                                <div class="title">
+                                                    <h2 class="h4"><a href="{{ makeposturl($item) }}" class="btn-link">{{ $item->title }}</a></h2> </div>
                                             </div>
                                         </div>
-                                        <div class="post--info">
-                                            <ul class="nav meta">
-                                                <li><a href="#">Leraje</a></li>
-                                                <li><a href="#">20 April 2017</a></li>
-                                            </ul>
-                                            <div class="title">
-                                                <h2 class="h4"><a href="news-single-v1.html" class="btn-link">Lorem Ipsum is simply dummy text of the printing</a></h2> </div>
+                                    </div>
+                                </div>
+                            @endforeach
+
+                            @foreach($lastFeaturestop->slice(2,1) as $item)
+                                <div class="col-sm-12 hidden-sm hidden-xs">
+                                    <div class="post--item post--layout-1 post--title-larger">
+                                        <div class="post--img">
+                                            <a href="{{ makeposturl($item) }}" class="thumb"><img src="{{ makepreview($item->thumb, 'b', 'posts') }}" alt=""></a> <a href="#" class="cat">Politics</a> <a href="#" class="icon"><i class="fa fa-fire"></i></a>
+                                            <div class="post--info">
+                                                <ul class="nav meta">
+                                                    <li><a href="#">Balam</a></li>
+                                                    <li><a href="#">{{ $item->created_at->diffForHumans() }}</a></li>
+                                                </ul>
+                                                <div class="title">
+                                                    <h2 class="h4"><a href="{{ makeposturl($item) }}" class="btn-link">{{ $item->title }}</a></h2> </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-sm-12 hidden-sm hidden-xs">
-                                <div class="post--item post--layout-1 post--title-larger">
-                                    <div class="post--img">
-                                        <a href="news-single-v1.html" class="thumb"><img src="uriankhai/img/home-img/banner-04.jpg" alt=""></a> <a href="#" class="cat">Politics</a> <a href="#" class="icon"><i class="fa fa-fire"></i></a>
-                                        <div class="post--info">
-                                            <ul class="nav meta">
-                                                <li><a href="#">Balam</a></li>
-                                                <li><a href="#">20 April 2017</a></li>
-                                            </ul>
-                                            <div class="title">
-                                                <h2 class="h4"><a href="news-single-v1.html" class="btn-link">On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the sure.</a></h2> </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
