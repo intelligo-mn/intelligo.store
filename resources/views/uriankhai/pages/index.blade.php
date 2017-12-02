4ebd34e935e6db0a3b2d88cf6aac57d5d3ff0cbd
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-02 22:44:32 +0800

    header breaking news

--- resources/views/uriankhai/pages/index.blade.php
+++ resources/views/uriankhai/pages/index.blade.php
@@ -27,16 +27,12 @@
             <h2>News Updates</h2> <span>(Update 12 minutes ago)</span> </div>
         <div class="news-updates--list" data-marquee="true">
             <ul class="nav">
-                <li>
-                    <h3 class="h3"><a href="#">Contrary to popular belief Lorem Ipsum is not simply random text.</a></h3> </li>
-                <li>
-                    <h3 class="h3"><a href="#">Education to popular belief Lorem Ipsum is not simply</a></h3> </li>
-                <li>
-                    <h3 class="h3"><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a></h3> </li>
-                <li>
-                    <h3 class="h3"><a href="#">Corporis repellendus perspiciatis reprehenderit.</a></h3> </li>
-                <li>
-                    <h3 class="h3"><a href="#">Deleniti consequatur laudantium sit aspernatur?</a></h3> </li>
+                @foreach($lastNews as $item)
+                    <li>
+                        <h3 class="h3"><a href="{{ makeposturl($item) }}">{{ $item->title }}</a></h3> 
+                    </li>        
+                @endforeach
+
             </ul>
         </div>
     </div>
