e159939f5e7d379f1500d8d757ddee2a16e4264a
tortuvshin <toroo.byamba@gmail.com>
2017-12-03 10:53:30 +0800

    #uriankhai - admin dashboard index

--- resources/views/uriankhai/_admin/pages/index.blade.php
+++ resources/views/uriankhai/_admin/pages/index.blade.php
@@ -46,29 +46,30 @@
         <!-- small box -->
         <div class="small-box bg-yellow">
             <div class="inner">
-                <h3>{{ $todayusers }}</h3>
-                <p>{{ trans('admin.todaysuserregistrations') }}</p>
+                <h3>{{ $videocount }}</h3>
+                <p>{{ trans('admin.NumberofVideos') }}</p>
             </div>
             <div class="icon">
-                <i class="fa fa-user-plus"></i>
+                <i class="fa fa-youtube-play"></i>
             </div>
-            <a href="/admin/users" class="small-box-footer">{{ trans('admin.moreinfo') }} <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="/admin/all" class="small-box-footer">{{ trans('admin.moreinfo') }} <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div><!-- ./col -->
     <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-red">
             <div class="inner">
-                <h3>{{ $todaylogins }}</h3>
-                <p>{{ trans('admin.todaysuserregistrations') }}</p>
+                <h3>{{ $newscount }}</h3>
+                <p>{{ trans('admin.NumberofNews') }}</p>
             </div>
             <div class="icon">
                 <i class="fa fa-eye"></i>
             </div>
-            <a href="/admin/users" class="small-box-footer">{{ trans('admin.moreinfo') }} <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="/admin/all" class="small-box-footer">{{ trans('admin.moreinfo') }} <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div><!-- ./col -->
 </div><!-- /.row -->
+
 <!-- Main row -->
 <div class="row">
     <!-- Left col -->
@@ -99,38 +100,13 @@
                     @endif
                 </div><!-- /.box-body -->
                 <div class="box-footer text-center">
-                    <a href="/admin/news" class="uppercase">{{ trans('admin.viewall') }}</a>
+                    <a href="/admin/all" class="uppercase">{{ trans('admin.viewall') }}</a>
                 </div><!-- /.box-footer -->
             </div>
         </div>
         @endforeach
 
-    </div>       <!-- USERS LIST -->
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <h3 class="box-title">{{ trans('admin.latestmembers') }}</h3>
-                    <div class="box-tools pull-right">
-                        <span class="label label-danger">{{ $todayusers }} {{ trans('admin.newmemberstoday') }}</span>
-                        <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                        <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                    </div>
-                </div><!-- /.box-header -->
-                <div class="box-body no-padding">
-
-                    <ul class="users-list clearfix">
-                        @foreach($lastusers as $user)
-                            <li style=" width: 20%;">
-                                <img src="{{ makepreview($user->icon, 'b', 'members/avatar') }}" width="110" height="110" style="max-height:110px;min-height:110px" alt="User Image">
-                                <a class="users-list-name" target="_blank" href="/profile/{{ $user->username_slug }}">{{ $user->username }}</a>
-                                <span class="users-list-date">{{ $user->created_at->diffForHumans() }}</span>
-                            </li>
-                        @endforeach
-                    </ul><!-- /.users-list -->
-                </div><!-- /.box-body -->
-                <div class="box-footer text-center">
-                    <a href="/admin/users" class="uppercase">{{ trans('admin.viewall') }}</a>
-                </div><!-- /.box-footer -->
-            </div><!--/.box -->
+    </div>      
     </section><!-- /.Left col -->
 
     <!-- right col (We are only adding the ID to make the widgets sortable)-->
@@ -214,9 +190,6 @@
                 <div class="box-tools pull-right">
                     <ul class="nav nav-tabs pull-right" style="border:0">
                         <li class="active"><a href="#news-chart" style="color:#000;border-radius: 5px;" class="btn btn-box-tool box-line-get" data-type="news" data-toggle="tab">{{ trans('admin.news') }}</a></li>
-                        <li><a href="#lists-chart" style="color:#000;border-radius: 5px;"  class="btn btn-box-tool box-line-get" data-type="lists" data-toggle="tab">{{ trans('admin.lists') }}</a></li>
-                        <li><a href="#quizzes-chart" style="color:#000;border-radius: 5px;"  class="btn btn-box-tool box-line-get" data-type="quizzes" data-toggle="tab">{{ trans('admin.quizzes') }}</a></li>
-                        <li><a href="#polls-chart" style="color:#000;border-radius: 5px;"  class="btn btn-box-tool box-line-get" data-type="polls" data-toggle="tab">{{ trans('admin.polls') }}</a></li>
                         <li><a href="#videos-chart" style="color:#000;border-radius: 5px;"  class="btn btn-box-tool box-line-get" data-type="videos" data-toggle="tab">{{ trans('admin.videos') }}</a></li>
                     </ul>
                 </div>
@@ -230,38 +203,10 @@
                 </div>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
-<style>
-
-    #sales-chart path{
-        stroke: rgba(255, 255, 255, 0.4);
-        stroke-width: 3px;
-    }
-</style>
-
-
 
     </section><!-- right col -->
 </div><!-- /.row (main row) -->
-<div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua"><i class="fa fa-file-text"></i></span>
-                <div class="info-box-content">
-                    <span class="info-box-text">{{ trans('admin.NumberofNews') }}</span>
-                    <span class="info-box-number">{{ $newscount }}</span>
-                </div><!-- /.info-box-content -->
-            </div><!-- /.info-box -->
-        </div><!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-red"><i class="fa fa-youtube-play"></i></span>
-                <div class="info-box-content">
-                    <span class="info-box-text">{{ trans('admin.NumberofVideos') }}</span>
-                    <span class="info-box-number">{{ $videocount }}</span>
-                </div><!-- /.info-box-content -->
-            </div><!-- /.info-box -->
-        </div><!-- /.col -->
-    </div>
+
 </section><!-- /.content -->
 @endsection
 @section('footer')
