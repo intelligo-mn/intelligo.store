e280f66459727923761da431eaf2c43d7a8dab8e
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-05 18:27:08 +0800

    #urainkhai - main header bottom blade created

--- resources/views/uriankhai/pages/index.blade.php
+++ resources/views/uriankhai/pages/index.blade.php
@@ -1,43 +1,8 @@
 @extends("main")
 @section("content")
-<div class="posts--filter-bar style--1 hidden-xs">
-    <div class="container">
-        <ul class="nav">
-            <li>
-                <a href="#"> <i class="fa fa-star-o"></i> <span>Featured News</span> </a>
-            </li>
-            <li>
-                <a href="#"> <i class="fa fa-heart-o"></i> <span>Most Popular</span> </a>
-            </li>
-            <li>
-                <a href="#"> <i class="fa fa-fire"></i> <span>Hot News</span> </a>
-            </li>
-            <li>
-                <a href="#"> <i class="fa fa-flash"></i> <span>Trending News</span> </a>
-            </li>
-            <li>
-                <a href="#"> <i class="fa fa-eye"></i> <span>Most Watched</span> </a>
-            </li>
-        </ul>
-    </div>
-</div>
-<div class="news--ticker">
-    <div class="container">
-        <div class="title">
-            <h2>News Updates</h2> <span>(Update 12 minutes ago)</span> </div>
-        <div class="news-updates--list" data-marquee="true">
-            <ul class="nav">
-                @foreach($lastNews as $item)
-                    <li>
-                        <h3 class="h3"><a href="{{ makeposturl($item) }}">{{ $item->title }}</a></h3> 
-                    </li>
-                            
-                @endforeach
 
-            </ul>
-        </div>
-    </div>
-</div>
+@include('._particles.headerbottom')
+
 <div class="main-content--section pbottom--30">
     <div class="container">
         <div class="main--content">
