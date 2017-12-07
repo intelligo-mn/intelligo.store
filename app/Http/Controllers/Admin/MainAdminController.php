dab0918c6c77bbe7fa247706e2cd3d1799e5fa0e
tortuvshin <toroo.byamba@gmail.com>
2017-12-07 13:44:24 +0800

    admin controllers updated

--- app/Http/Controllers/Admin/MainAdminController.php
+++ app/Http/Controllers/Admin/MainAdminController.php
@@ -15,12 +15,9 @@ use App\Posts;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\AkbilisimController;
 
 class MainAdminController extends Controller
 {
-    protected $verify  = 'http://envato.akbilisim.com/api/BA';
-
     /**
      * Display a listing of the resource.
      *
