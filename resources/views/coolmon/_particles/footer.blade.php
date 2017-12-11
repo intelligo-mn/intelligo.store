13ce177157ca35607061cceab57a1765f1b7baf3
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-12 01:10:24 +0800

    #coolmon - footer top

--- resources/views/coolmon/_particles/footer.blade.php
+++ resources/views/coolmon/_particles/footer.blade.php
@@ -1,5 +1,121 @@
-  <footer>
-       
+<footer>
+   <div class="footer-top-area">
+        <div class="container">
+            <div class="row">
+                <!-- Footer About Section Start Here -->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="single-footer footer-one">
+                        <h3>About</h3>
+                        <div class="footer-logo"><img src="images/footer-logo.png" alt="footer-logo"></div>
+                        <p>Your source for the lifestyle news. This demo is crafted specifically to exhibit the use of the theme as a lifestyle site. Visit our main page for more </p>
+                        <p>We're social, connect with us:</p>
+                        <div class="footer-social-media-area">
+                            <nav>
+                                <ul>
+                                    <!-- Facebook Icon Here -->
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <!-- Google Icon Here -->
+                                    <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                                    <!-- Twitter Icon Here -->
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <!-- Vimeo Icon Here -->
+                                    <li><a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
+                                    <!-- Pinterest Icon Here -->
+                                    <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <!-- Footer About Section End Here -->
+
+                <!-- Footer Popular Post Section Start Here -->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="single-footer footer-two">
+                        <h3>Popular Posts</h3>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-4">
+                                        <a href="blog-single.html"><img src="images/post-images/post-1.jpg" alt="post photo"></a>
+                                    </div>
+                                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-8">
+                                        <p><a href="blog-single.html">US should prepare for Russian election</a></p>
+                                        <span><i class="fa fa-calendar-check-o" aria-hidden="true"> </i> June  28,  2017</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-4">
+                                        <a href="blog-single.html"><img src="images/post-images/post-2.jpg" alt="post photo"></a>
+                                    </div>
+                                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-8">
+                                        <p><a href="blog-single.html">US should prepare for Russian election</a></p>
+                                        <span><i class="fa fa-calendar-check-o" aria-hidden="true"> </i> June  28,  2017</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-4">
+                                        <a href="blog-single.html"><img src="images/post-images/post-3.jpg" alt="post photo"></a>
+                                    </div>
+                                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-8">
+                                        <p><a href="blog-single.html">US should prepare for Russian election</a></p>
+                                        <span><i class="fa fa-calendar-check-o" aria-hidden="true"> </i> June  28,  2017</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <!-- Footer Popular Post Section End Here -->
+
+                <!-- Footer From Flickr Section Start Here -->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                    <div class="single-footer footer-three">
+                        <h3>From Flickr</h3>
+                        <ul>
+                            <li>
+                                <a href="#"><img src="images/flicker/1.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/2.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/3.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/4.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/5.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/6.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/7.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/8.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/9.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/10.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/11.png" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="images/flicker/12.png" alt="flicker photo"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Footer From Flickr Section End Here -->
+            </div>
+        </div>
+    </div>
     <div class="footer-bottom-area">
         <div class="container">
             <div class="row">
