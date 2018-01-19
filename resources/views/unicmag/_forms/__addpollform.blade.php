f8ba34d0fa0b9cbd7e275f1a92d605978f643e30
tortuvshin <toroo.byamba@gmail.com>
2018-01-19 18:34:49 +0800

    remove some themes

--- resources/views/unicmag/_forms/__addpollform.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="entry" data-type="poll" @if(isset($entry->id)) data-entry-id="{{ $entry->id }}" @endif>
-    @include('_forms.__entryactions')
-    <h3><i class="fa fa-check-circle-o"></i> {{ trans('addpost.option') }}</h3>
-    <div class="inpunting ordering pushpoll">
-        <button class="order-number button button-gray">1</button>
-        {!! Form::text(null, isset($entry->title) ? $entry->title : null, ['data-type' => 'title', 'class' => 'cd-input', 'placeholder' =>  trans('addpost.option') ]) !!}
-    </div>
-
-</div>
