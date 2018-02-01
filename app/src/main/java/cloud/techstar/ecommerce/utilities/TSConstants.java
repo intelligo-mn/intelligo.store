7c8fe9b208e278d8ca0e73a697ee3233568d0621
tortuvshin <toroo.byamba@gmail.com>
2018-02-01 10:39:03 +0800

    updated

--- app/src/main/java/cloud/techstar/ecommerce/utilities/TSConstants.java
+++ app/src/main/java/cloud/techstar/ecommerce/utilities/TSConstants.java
@@ -9,11 +9,10 @@ public class TSConstants {
     public static String USER_ID = "user_id";
     public static String USER_NAME = "username";
     public static String USER_EMAIL = "user_email";
-    public static String WebURL = "https://techstar-dashboard-tortuvshin.c9users.io/";
-    public static String ApiURL = "https://techstar-dashboard-tortuvshin.c9users.io/api/";
+    public static String WebURL = "https://www.dglproject.com";
+    public static String ApiURL = "https://www.dglproject.com/api/";
     public static String ProductService = ApiURL+"ProductService.php";
     public static String UserService= ApiURL+"UserService.php";
     public static String BrandService= ApiURL+"BrandService.php";
     public static String CategoryService= ApiURL+"CategoryService.php";
-
 }
