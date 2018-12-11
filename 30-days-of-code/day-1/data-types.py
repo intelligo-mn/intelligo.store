cddda7f1eae84f1d1015b0b487bf327335f65669
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-12-11 13:57:27 +0900

    updated

--- 30-days-of-code/day-1/data-types.py
+++ 30-days-of-code/day-1/data-types.py
@@ -1,3 +1,13 @@
+# Task
+# Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+#
+# Declare  variables: one of type int, one of type double, and one of type String.
+# Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your variables.
+# Use the  operator to perform the following operations:
+# Print the sum of  plus your int variable on a new line.
+# Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+# Concatenate  with the string you read as input and print the result on a new line.
+
 i = 4
 d = 4.0
 s = 'HackerRank '
