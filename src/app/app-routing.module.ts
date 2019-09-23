f9c16aed85874337dd2abf97f36a576ecfbd9bd1
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-09-23 18:42:47 +0800

    initial project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { StoriesComponent } from "./stories/stories.component";
+import { ItemCommentsComponent } from "./item-comments/item-comments.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "news/1", pathMatch: "full" },
+  {
+    path: "news/:page",
+    component: StoriesComponent,
+    data: { storiesType: "news" }
+  },
+  {
+    path: "newest/:page",
+    component: StoriesComponent,
+    data: { storiesType: "newest" }
+  },
+  {
+    path: "show/:page",
+    component: StoriesComponent,
+    data: { storiesType: "show" }
+  },
+  {
+    path: "ask/:page",
+    component: StoriesComponent,
+    data: { storiesType: "ask" }
+  },
+  {
+    path: "jobs/:page",
+    component: StoriesComponent,
+    data: { storiesType: "jobs" }
+  },
+  { path: "item/:id", component: ItemCommentsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/domain.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DomainPipe } from './domain.pipe';
+
+describe('DomainPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DomainPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
