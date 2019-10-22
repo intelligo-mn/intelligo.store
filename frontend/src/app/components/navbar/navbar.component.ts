16a8a08ca02f87a3d56b82730db35c380359ca7c
Doljinsuren Enkhbayar <doljko927@gmail.com>
2019-10-22 23:06:04 +0800

    admin theme oruulsan.

---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { ROUTES } from '../sidebar/sidebar.component';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  public focus;
+  public listTitles: any[];
+  public location: Location;
+  constructor(location: Location,  private element: ElementRef, private router: Router) {
+    this.location = location;
+  }
+
+  ngOnInit() {
+    this.listTitles = ROUTES.filter(listTitle => listTitle);
+  }
+  getTitle(){
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if(titlee.charAt(0) === '#'){
+        titlee = titlee.slice( 1 );
+    }
+
+    for(var item = 0; item < this.listTitles.length; item++){
+        if(this.listTitles[item].path === titlee){
+            return this.listTitles[item].title;
+        }
+    }
+    return 'Dashboard';
+  }
+
+}
---
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
+    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
+    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
+    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
+    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
+    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  public menuItems: any[];
+  public isCollapsed = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true;
+   });
+  }
+}
