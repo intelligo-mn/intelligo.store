83a03e60f07df9ab45e6a2b2e506117be32d09eb
Doljinsuren Enkhbayar <doljko927@gmail.com>
2019-10-22 23:34:55 +0800

    swagger module added.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { CatsController } from '../controllers/cat.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/grocery')],
-  controllers: [AppController , CatsController],
+  imports: [ ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
