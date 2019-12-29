1066211d3f8c505e9097d391d7a85e97d03ae5c8
Turtvshin Byambaa <toroo.byamba@gmail.com>
2019-12-29 23:28:53 +0800

    updated

--- 10-days-of-javascript/day-5/arrows.js
+++ 10-days-of-javascript/day-5/arrows.js
@@ -1,27 +1,3 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
 /*
  * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
  * 
@@ -30,12 +6,4 @@ function readLine() {
  */
 function modifyArray(nums) {
     return nums.map(num => (num % 2 === 0) ? num * 2 : num * 3);
-}
-
-
-function main() {
-    const n = +(readLine());
-    const a = readLine().split(' ').map(Number);
-    
-    console.log(modifyArray(a).toString().split(',').join(' '));
 }
