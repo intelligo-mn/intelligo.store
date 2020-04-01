5cc723137a0620bf173b97ec7059081cbc309226
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-04-01 16:39:43 +0800

    updated

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:grocery/main.dart';
+import 'package:marketgeek/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
