2595bd70184cefa9e72c49e3c9e63673e0e58f54
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-25 22:11:46 +0800

    created base entities

---
+++ backend/src/domain/user.entity.ts
@@ -0,0 +1,44 @@
+import { Authority } from './authority.entity';
+import { Entity, Column, ManyToMany, JoinTable } from 'typeorm';
+import { BaseEntity } from './base/base.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity('user')
+export class User extends BaseEntity {
+  @ApiProperty({ uniqueItems: true, example: 'myuser', description: 'User login' })
+  @Column({ unique: true })
+  login: string;
+  @ApiProperty({ example: 'MyUser', description: 'User first name' })
+  @Column()
+  firstName: string;
+  @ApiProperty({ example: 'MyUser', description: 'User last name' })
+  @Column()
+  lastName: string;
+  @ApiProperty({ example: 'myuser@localhost', description: 'User email' })
+  @Column({ nullable: true })
+  email?: string;
+  @ApiProperty({ example: 'true', description: 'User activation' })
+  @Column()
+  activated: boolean;
+  @ApiProperty({ example: 'en', description: 'User language' })
+  @Column()
+  langKey: string;
+
+  // eslint-disable-next-line
+  @ManyToMany(type => Authority)
+  @JoinTable()
+  @ApiProperty({ isArray: true, enum: ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_ANONYMOUS'], description: 'Array of permissions' })
+  authorities?: any[];
+
+  @ApiProperty({ example: 'myuser', description: 'User password' })
+  @Column()
+  password: string;
+  @Column({ nullable: true })
+  imageUrl?: string;
+  @Column({ nullable: true })
+  activationKey?: string;
+  @Column({ nullable: true })
+  resetKey?: string;
+  @Column({ nullable: true })
+  resetDate?: Date;
+}
