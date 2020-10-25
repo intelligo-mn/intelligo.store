b450ae0d21c73be9228b5fbc7f6b5b64037a9765
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-25 22:20:30 +0800

    created copy resources script

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,19 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { ormconfig } from './orm.config';
-import { ForumModule } from './modules/forum/forum.module';
-import { CategoryModule } from './modules/category/category.module';
-import { DiscussionModule } from './modules/discussion/discussion.module';
-import { CommentModule } from './modules/comment/comment.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
     AuthModule,
-    ForumModule,
-    CategoryModule,
-    DiscussionModule,
-    CommentModule
   ],
   controllers: [
   ],
