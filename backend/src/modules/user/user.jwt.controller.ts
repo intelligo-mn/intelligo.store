86bf8962da3d0cd0d242e39687de3dd4a3627933
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-29 20:18:23 +0800

    changed user controller and service

--- backend/src/modules/user/user.jwt.controller.ts
+++ backend/src/modules/user/user.jwt.controller.ts
@@ -1,20 +1,20 @@
 import { Body, Controller, Logger, Post, Res, Req, UseInterceptors } from '@nestjs/common';
 import { Response, Request } from 'express';
-import { UserLoginDTO } from '../../domain/user-login.dto';
-import { AuthService } from '../auth/auth.service';
-import { LoggingInterceptor } from '../../core/interceptors/logging.interceptor';
-import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+import { UserLoginDTO } from '../../service/dto/user-login.dto';
+import { AuthService } from '../../service/auth.service';
+import { LoggingInterceptor } from '../../client/interceptors/logging.interceptor';
+import { ApiUseTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('api')
 @UseInterceptors(LoggingInterceptor)
-@ApiTags('user-jwt-controller')
+@ApiUseTags('user-jwt-controller')
 export class UserJWTController {
   logger = new Logger('UserJWTController');
 
   constructor(private readonly authService: AuthService) {}
 
   @Post('/authenticate')
-  @ApiOperation({ summary: 'Authorization api retrieving token' })
+  @ApiOperation({ title: 'Authorization api retrieving token' })
   @ApiResponse({
     status: 201,
     description: 'Authorized',
