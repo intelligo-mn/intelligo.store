ec98520a2341e7b98b9c3a6641ddb623d8707f49
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-29 21:10:54 +0800

    fixed bootstrap forms mixin scss

--- frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -8,26 +8,5 @@ export const AdminLayoutRoutes: Routes = [
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [AuthGuard],
-  },
-  {
-    path: 'cash-capital',
-    loadChildren: () => import('../../modules/cash-capital/cash-capital.module').then(m => m.CashCapitalModule),
-
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'inventory',
-    loadChildren: () => import('../../modules/inventory/inventory.module').then(m => m.InventoryModule),
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'main-capital',
-    loadChildren: () => import('../../modules/main-capital/main-capital.module').then(m => m.MainCapitalModule),
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'salary',
-    loadChildren: () => import('../../modules/salary/salary.module').then(m => m.SalaryModule),
-    canActivate: [AuthGuard],
-  },
+  }
 ];
