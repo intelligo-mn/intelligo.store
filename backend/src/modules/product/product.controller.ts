2782615230732ce559d6170931712728da8120e7
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-29 20:19:37 +0800

    created product module with service and controller

---
+++ backend/src/modules/product/product.controller.ts
@@ -0,0 +1,90 @@
+import { Body, Controller, Delete, Get, Logger, Param, Post as PostMethod, Put, UseGuards, Req, UseInterceptors } from '@nestjs/common';
+import { ApiBearerAuth, ApiUseTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+import { Request } from 'express';
+import Product from '../../domain/product.entity';
+import { ProductService } from '../../service/product.service';
+import { PageRequest, Page } from '../../domain/base/pagination.entity';
+import { AuthGuard, Roles, RolesGuard, RoleType } from '../../security';
+import { HeaderUtil } from '../../client/header-util';
+import { LoggingInterceptor } from '../../client/interceptors/logging.interceptor';
+
+@Controller('api/products')
+@UseGuards(AuthGuard, RolesGuard)
+@UseInterceptors(LoggingInterceptor)
+@ApiBearerAuth()
+@ApiUseTags('products')
+export class ProductController {
+  logger = new Logger('ProductController');
+
+  constructor(private readonly productService: ProductService) {}
+
+  @Get('/')
+  @Roles(RoleType.USER)
+  @ApiResponse({
+    status: 200,
+    description: 'List all records',
+    type: Product,
+  })
+  async getAll(@Req() req: Request): Promise<Product[]> {
+    const pageRequest: PageRequest = new PageRequest(req.query.page, req.query.size, req.query.sort);
+    const [results, count] = await this.productService.findAndCount({
+      skip: +pageRequest.page * pageRequest.size,
+      take: +pageRequest.size,
+      order: pageRequest.sort.asOrder(),
+    });
+    HeaderUtil.addPaginationHeaders(req.res, new Page(results, count, pageRequest));
+    return results;
+  }
+
+  @Get('/:id')
+  @Roles(RoleType.USER)
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: Product,
+  })
+  async getOne(@Param('id') id: string): Promise<Product> {
+    return await this.productService.findById(id);
+  }
+
+  @PostMethod('/')
+  @Roles(RoleType.USER)
+  @ApiOperation({ title: 'Create product' })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+    type: Product,
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async post(@Req() req: Request, @Body() product: Product): Promise<Product> {
+    const created = await this.productService.save(product);
+    HeaderUtil.addEntityCreatedHeaders(req.res, 'Product', created.id);
+    return created;
+  }
+
+  @Put('/')
+  @Roles(RoleType.USER)
+  @ApiOperation({ title: 'Update product' })
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully updated.',
+    type: Product,
+  })
+  async put(@Req() req: Request, @Body() product: Product): Promise<Product> {
+    HeaderUtil.addEntityCreatedHeaders(req.res, 'Product', product.id);
+    return await this.productService.update(product);
+  }
+
+  @Delete('/:id')
+  @Roles(RoleType.USER)
+  @ApiOperation({ title: 'Delete product' })
+  @ApiResponse({
+    status: 204,
+    description: 'The record has been successfully deleted.',
+  })
+  async remove(@Req() req: Request, @Param('id') id: string): Promise<Product> {
+    HeaderUtil.addEntityDeletedHeaders(req.res, 'Product', id);
+    const toDelete = await this.productService.findById(id);
+    return await this.productService.delete(toDelete);
+  }
+}
