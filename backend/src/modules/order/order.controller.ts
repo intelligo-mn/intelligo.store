2007dc9e1bdcdc9e89547429e41c5a07820752cf
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-30 09:30:46 +0800

    fixed order module

--- backend/src/modules/order/order.controller.ts
+++ backend/src/modules/order/order.controller.ts
@@ -1,89 +1,113 @@
-import { Body, Controller, Delete, Get, Logger, Param, Post as PostMethod, Put, UseGuards, Req, UseInterceptors } from '@nestjs/common';
-import { ApiBearerAuth, ApiUseTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
-import { Request } from 'express';
-import Order from '../../domain/order.entity';
-import { OrderService } from './order.service';
-import { PageRequest, Page } from '../../domain/base/pagination.entity';
-import { AuthGuard, Roles, RolesGuard, RoleType } from '../../security';
-import { HeaderUtil } from '../../client/header-util';
-import { LoggingInterceptor } from '../../client/interceptors/logging.interceptor';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Post as PostMethod,
+  Put,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger";
+import { Request } from "express";
+import { HeaderUtil } from "src/core/header-util";
+import { LoggingInterceptor } from "src/core/interceptors/logging.interceptor";
+import { Page, PageRequest } from "../../domain/base/pagination.entity";
+import Order from "../../domain/order.entity";
+import { AuthGuard, Roles, RolesGuard, RoleType } from "../../security";
+import { OrderService } from "./order.service";
 
-@Controller('api/orders')
+@Controller("api/orders")
 @UseGuards(AuthGuard, RolesGuard)
 @UseInterceptors(LoggingInterceptor)
 @ApiBearerAuth()
-@ApiUseTags('orders')
+@ApiTags("orders")
 export class OrderController {
-  logger = new Logger('OrderController');
+  logger = new Logger("OrderController");
 
   constructor(private readonly orderService: OrderService) {}
 
-  @Get('/')
+  @Get("/")
   @Roles(RoleType.USER)
   @ApiResponse({
     status: 200,
-    description: 'List all records',
+    description: "List all records",
     type: Order,
   })
   async getAll(@Req() req: Request): Promise<Order[]> {
-    const pageRequest: PageRequest = new PageRequest(req.query.page, req.query.size, req.query.sort);
+    const pageRequest: PageRequest = new PageRequest(
+      req.query.page,
+      req.query.size,
+      req.query.sort
+    );
     const [results, count] = await this.orderService.findAndCount({
       skip: +pageRequest.page * pageRequest.size,
       take: +pageRequest.size,
       order: pageRequest.sort.asOrder(),
     });
-    HeaderUtil.addPaginationHeaders(req.res, new Page(results, count, pageRequest));
+    HeaderUtil.addPaginationHeaders(
+      req.res,
+      new Page(results, count, pageRequest)
+    );
     return results;
   }
 
-  @Get('/:id')
+  @Get("/:id")
   @Roles(RoleType.USER)
   @ApiResponse({
     status: 200,
-    description: 'The found record',
+    description: "The found record",
     type: Order,
   })
-  async getOne(@Param('id') id: string): Promise<Order> {
+  async getOne(@Param("id") id: string): Promise<Order> {
     return await this.orderService.findById(id);
   }
 
-  @PostMethod('/')
+  @PostMethod("/")
   @Roles(RoleType.USER)
-  @ApiOperation({ title: 'Create order' })
+  @ApiOperation({ summary: "Create order" })
   @ApiResponse({
     status: 201,
-    description: 'The record has been successfully created.',
+    description: "The record has been successfully created.",
     type: Order,
   })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiResponse({ status: 403, description: "Forbidden." })
   async post(@Req() req: Request, @Body() order: Order): Promise<Order> {
     const created = await this.orderService.save(order);
-    HeaderUtil.addEntityCreatedHeaders(req.res, 'Order', created.id);
+    HeaderUtil.addEntityCreatedHeaders(req.res, "Order", created.id);
     return created;
   }
 
-  @Put('/')
+  @Put("/")
   @Roles(RoleType.USER)
-  @ApiOperation({ title: 'Update order' })
+  @ApiOperation({ summary: "Update order" })
   @ApiResponse({
     status: 200,
-    description: 'The record has been successfully updated.',
+    description: "The record has been successfully updated.",
     type: Order,
   })
   async put(@Req() req: Request, @Body() order: Order): Promise<Order> {
-    HeaderUtil.addEntityCreatedHeaders(req.res, 'Order', order.id);
+    HeaderUtil.addEntityCreatedHeaders(req.res, "Order", order.id);
     return await this.orderService.update(order);
   }
 
-  @Delete('/:id')
+  @Delete("/:id")
   @Roles(RoleType.USER)
-  @ApiOperation({ title: 'Delete order' })
+  @ApiOperation({ summary: "Delete order" })
   @ApiResponse({
     status: 204,
-    description: 'The record has been successfully deleted.',
+    description: "The record has been successfully deleted.",
   })
-  async remove(@Req() req: Request, @Param('id') id: string): Promise<Order> {
-    HeaderUtil.addEntityDeletedHeaders(req.res, 'Order', id);
+  async remove(@Req() req: Request, @Param("id") id: string): Promise<Order> {
+    HeaderUtil.addEntityDeletedHeaders(req.res, "Order", id);
     const toDelete = await this.orderService.findById(id);
     return await this.orderService.delete(toDelete);
   }
