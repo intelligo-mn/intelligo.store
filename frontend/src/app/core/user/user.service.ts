20408aeb96525c0191df0f7daf650d8328ddf53d
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-30 08:45:46 +0800

    code module with services

---
+++ frontend/src/app/core/user/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { createRequestOption, Pagination } from "app/shared/util/request-util";
+import { IUser } from "./user.model";
+import { environment } from "environments/environment";
+
+@Injectable({ providedIn: "root" })
+export class UserService {
+  public resourceUrl = environment.apiUrl + "api/users";
+
+  constructor(private http: HttpClient) {}
+
+  create(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>(this.resourceUrl, user);
+  }
+
+  update(user: IUser): Observable<IUser> {
+    return this.http.put<IUser>(this.resourceUrl, user);
+  }
+
+  find(login: string): Observable<IUser> {
+    return this.http.get<IUser>(`${this.resourceUrl}/${login}`);
+  }
+
+  query(req?: Pagination): Observable<HttpResponse<IUser[]>> {
+    const options = createRequestOption(req);
+    return this.http.get<IUser[]>(this.resourceUrl, {
+      params: options,
+      observe: "response",
+    });
+  }
+
+  delete(login: string): Observable<{}> {
+    return this.http.delete(`${this.resourceUrl}/${login}`);
+  }
+
+  authorities(): Observable<string[]> {
+    return this.http.get<string[]>(
+      environment.apiUrl + "api/users/authorities"
+    );
+  }
+}
