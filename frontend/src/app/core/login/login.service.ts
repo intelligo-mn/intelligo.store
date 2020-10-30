210e3db5ec91ff24b87426833ee9c81574ac82a0
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-30 09:04:06 +0800

    created login component and service

--- frontend/src/app/core/login/login.service.ts
+++ frontend/src/app/core/login/login.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 
-import { Account } from 'app/core/user/account.model';
-import { AccountService } from 'app/core/auth/account.service';
-import { AuthServerProvider } from 'app/core/auth/auth-jwt.service';
+import { Account } from '../user/account.model';
 import { Login } from './login.model';
+import { AccountService } from '../auth/account.service';
+import { AuthServerProvider } from '../auth/auth-jwt.service';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
