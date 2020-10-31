76fb1bca7cee2738bb92169ca33166499462f286
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-31 13:31:58 +0800

    added modules to app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,24 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './modules/auth/auth.module';
-import { ormconfig } from './orm.config';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./modules/auth/auth.module";
+import { KinderGardenModule } from "./modules/kinder-garden/kinder-garden.module";
+import { OrderModule } from "./modules/order/order.module";
+import { ProductTypeModule } from "./modules/product/product-type.module";
+import { ProductModule } from "./modules/product/product.module";
+import { UnitModule } from "./modules/unit/unit.module";
+import { ormconfig } from "./orm.config";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
     AuthModule,
+    KinderGardenModule,
+    OrderModule,
+    ProductModule,
+    ProductTypeModule,
+    UnitModule,
   ],
-  controllers: [
-  ],
-  providers: [
-  ]
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
