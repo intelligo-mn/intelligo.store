93daa4046b769221a03827a98f3ce495cf27b18e
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-31 14:27:26 +0800

    fix yaml load safe

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,49 +1,53 @@
-import yaml from 'js-yaml';
-import * as fs from 'fs';
-import * as path from 'path';
-import { Logger } from '@nestjs/common';
+import { Logger } from "@nestjs/common";
+import * as fs from "fs";
+import { safeLoad } from "js-yaml";
+import * as path from "path";
 
-const logger = new Logger('Config');
+const logger = new Logger("Config");
 
 export class Config {
-  debugLogging = 'debug';
+  debugLogging = "debug";
 
-  'server.port' = '8090';
-  'intelligo.clientApp.name' = 'childfood';
-  'intelligo.registry.password' = 'admin';
-  'intelligo.security.authentication.jwt.base64-secret' = '';
-  'intelligo.security.authentication.jwt.token-validity-in-seconds' = 86400;
-  'intelligo.security.authentication.jwt.token-validity-in-seconds-for-remember-me' = 2592000;
-  'intelligo.mail.base-url' = 'http://127.0.0.1:${server.port}';
-  'intelligo.mail.from' = 'childfood@localhost';
-  'intelligo.swagger.default-include-pattern' = '/api/.*';
-  'intelligo.swagger.title' = 'childfood API';
-  'intelligo.swagger.description' = 'childfood API documentation';
-  'intelligo.swagger.version' = '0.0.1';
-  'intelligo.swagger.path' = '/api/v2/api-docs';
-  'eureka.client.enabled' = true;
-  'eureka.client.healthcheck.enabled' = true;
-  'eureka.client.fetch-registry' = true;
-  'eureka.client.register-with-eureka' = true;
-  'eureka.client.instance-info-replication-interval-seconds' = 10;
-  'eureka.client.registry-fetch-interval-seconds' = 10;
-  'eureka.instance.appname' = 'childfood';
-  'eureka.instance.instanceId' = 'childfood:${random.value}';
-  'eureka.instance.lease-renewal-interval-in-seconds' = 5;
-  'eureka.instance.lease-expiration-duration-in-seconds' = 10;
-  'eureka.instance.status-page-url-path' = '${management.endpoints.web.base-path}/info';
-  'eureka.instance.health-check-url-path' = '${management.endpoints.web.base-path}/health';
-  'eureka.instance.metadata-map.zone' = 'primary';
+  "server.port" = "8090";
+  "intelligo.clientApp.name" = "childfood";
+  "intelligo.registry.password" = "admin";
+  "intelligo.security.authentication.jwt.base64-secret" = "";
+  "intelligo.security.authentication.jwt.token-validity-in-seconds" = 86400;
+  "intelligo.security.authentication.jwt.token-validity-in-seconds-for-remember-me" = 2592000;
+  "intelligo.mail.base-url" = "http://127.0.0.1:${server.port}";
+  "intelligo.mail.from" = "childfood@localhost";
+  "intelligo.swagger.default-include-pattern" = "/api/.*";
+  "intelligo.swagger.title" = "childfood API";
+  "intelligo.swagger.description" = "childfood API documentation";
+  "intelligo.swagger.version" = "0.0.1";
+  "intelligo.swagger.path" = "/api/v2/api-docs";
+  "eureka.client.enabled" = true;
+  "eureka.client.healthcheck.enabled" = true;
+  "eureka.client.fetch-registry" = true;
+  "eureka.client.register-with-eureka" = true;
+  "eureka.client.instance-info-replication-interval-seconds" = 10;
+  "eureka.client.registry-fetch-interval-seconds" = 10;
+  "eureka.instance.appname" = "childfood";
+  "eureka.instance.instanceId" = "childfood:${random.value}";
+  "eureka.instance.lease-renewal-interval-in-seconds" = 5;
+  "eureka.instance.lease-expiration-duration-in-seconds" = 10;
+  "eureka.instance.status-page-url-path" =
+    "${management.endpoints.web.base-path}/info";
+  "eureka.instance.health-check-url-path" =
+    "${management.endpoints.web.base-path}/health";
+  "eureka.instance.metadata-map.zone" = "primary";
   // 'eureka.instance.metadata-map.profile' = '${profiles.active}';
-  'eureka.instance.metadata-map.git-version' = '${git.commit.id.describe:}';
-  'eureka.instance.metadata-map.git-commit' = '${git.commit.id.abbrev:}';
-  'eureka.instance.metadata-map.git-branch' = '${git.branch:}';
-  'eureka.instance.prefer-ip-address' = true;
-  'eureka.client.service-url.defaultZone' = 'http://admin:${intelligo.registry.password}@localhost:8761/eureka/';
-  'cloud.config.uri' = 'http://admin:${intelligo.registry.password}@localhost:8761/config';
-  'cloud.config.name' = 'childfood';
-  'cloud.config.profile' = 'prod';
-  'loud.config.label' = 'master';
+  "eureka.instance.metadata-map.git-version" = "${git.commit.id.describe:}";
+  "eureka.instance.metadata-map.git-commit" = "${git.commit.id.abbrev:}";
+  "eureka.instance.metadata-map.git-branch" = "${git.branch:}";
+  "eureka.instance.prefer-ip-address" = true;
+  "eureka.client.service-url.defaultZone" =
+    "http://admin:${intelligo.registry.password}@localhost:8761/eureka/";
+  "cloud.config.uri" =
+    "http://admin:${intelligo.registry.password}@localhost:8761/config";
+  "cloud.config.name" = "childfood";
+  "cloud.config.profile" = "prod";
+  "loud.config.label" = "master";
 
   constructor(properties) {
     this.addAll(properties);
@@ -76,27 +80,38 @@ export class Config {
 
   private processTemplate(template, variables): any {
     // console.log(template);
-    if (typeof template === 'string') {
-      return template.replace(new RegExp('\\${[^{]+}', 'g'), name => variables[name.substring(2, name.length - 1)]);
+    if (typeof template === "string") {
+      return template.replace(
+        new RegExp("\\${[^{]+}", "g"),
+        (name) => variables[name.substring(2, name.length - 1)]
+      );
     }
     return template;
   }
 }
 
-const yamlConfigPath = path.join(__dirname, 'config', 'application.yml');
-const envYamlConfigPath = path.join(__dirname, 'config', `application-${process.env.NODE_ENV}.yml`);
+const yamlConfigPath = path.join(__dirname, "config", "application.yml");
+const envYamlConfigPath = path.join(
+  __dirname,
+  "config",
+  `application-${process.env.NODE_ENV}.yml`
+);
 
-const yamlConfig = yaml.safeLoad(fs.readFileSync(yamlConfigPath, 'utf8'));
+const yamlConfig = safeLoad(fs.readFileSync(yamlConfigPath, "utf8"));
 logger.log(`Actual process.env.NODE_ENV value: ${process.env.NODE_ENV}`);
-logger.log('Standard allowed values are: dev, test or prod');
+logger.log("Standard allowed values are: dev, test or prod");
 if (!fs.existsSync(envYamlConfigPath)) {
   logger.error(
-    'does not exist under your config folder an application-{process.env.NODE_ENV}.yml file with your process.env.NODE_ENV value'
+    "does not exist under your config folder an application-{process.env.NODE_ENV}.yml file with your process.env.NODE_ENV value"
   );
 }
-const envYamlConfig = yaml.safeLoad(fs.readFileSync(envYamlConfigPath, 'utf8'));
+const envYamlConfig = safeLoad(fs.readFileSync(envYamlConfigPath, "utf8"));
 
-const config = new Config({ ...objectToArray(yamlConfig), ...objectToArray(envYamlConfig), ipAddress: ipAddress() });
+const config = new Config({
+  ...objectToArray(yamlConfig),
+  ...objectToArray(envYamlConfig),
+  ipAddress: ipAddress(),
+});
 
 export { config };
 
@@ -104,10 +119,10 @@ function objectToArray(source, currentKey?, target?): any {
   target = target || {};
   for (const property in source) {
     if (source.hasOwnProperty(property)) {
-      const newKey = currentKey ? currentKey + '.' + property : property;
+      const newKey = currentKey ? currentKey + "." + property : property;
       const newVal = source[property];
 
-      if (typeof newVal === 'object') {
+      if (typeof newVal === "object") {
         objectToArray(newVal, newKey, target);
       } else {
         target[newKey] = newVal;
@@ -118,12 +133,16 @@ function objectToArray(source, currentKey?, target?): any {
 }
 
 function ipAddress(): any {
-  const interfaces = require('os').networkInterfaces();
+  const interfaces = require("os").networkInterfaces();
   for (const dev in interfaces) {
     if (interfaces.hasOwnProperty(dev)) {
       const iface = interfaces[dev];
       for (const alias of iface) {
-        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        if (
+          alias.family === "IPv4" &&
+          alias.address !== "127.0.0.1" &&
+          !alias.internal
+        ) {
           return alias.address;
         }
       }
