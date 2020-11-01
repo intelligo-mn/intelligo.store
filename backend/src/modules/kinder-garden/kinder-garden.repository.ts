b388f14a849173bb1315cedda6471647e8086828
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-01 08:47:17 +0800

    removed some files

--- backend/src/modules/kinder-garden/kinder-garden.repository.ts
+++
@@ -1,5 +0,0 @@
-import KinderGarden from 'src/domain/kinder-garden.entity';
-import { EntityRepository, Repository } from 'typeorm';
-
-@EntityRepository(KinderGarden)
-export class KinderGardenRepository extends Repository<KinderGarden> {}
--- backend/src/modules/product/product.repository.ts
+++
@@ -1,5 +0,0 @@
-import Product from "src/domain/product.entity";
-import { EntityRepository, Repository } from "typeorm";
-
-@EntityRepository(Product)
-export class ProductRepository extends Repository<Product> {}
