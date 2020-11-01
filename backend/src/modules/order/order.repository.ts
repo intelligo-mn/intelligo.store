ce746ae977a80c770752ccacd8e1a4acd7a3b2da
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-01 08:47:17 +0800

    removed some files

--- backend/src/modules/order/order.repository.ts
+++
@@ -1,5 +0,0 @@
-import Order from "src/domain/order.entity";
-import { EntityRepository, Repository } from "typeorm";
-
-@EntityRepository(Order)
-export class OrderRepository extends Repository<Order> {}
--- backend/src/modules/unit/unit.repository.ts
+++
@@ -1,5 +0,0 @@
-import Unit from "src/domain/unit.entity";
-import { EntityRepository, Repository } from "typeorm";
-
-@EntityRepository(Unit)
-export class UnitRepository extends Repository<Unit> {}
