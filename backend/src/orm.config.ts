3ca896e1ccd92b1d8042d1d5862fc764af7a26d1
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-03 10:52:56 +0800

    created nestjs project

---
+++ backend/src/orm.config.ts
@@ -0,0 +1,83 @@
+const commonConf = {
+  SYNCRONIZE: true,
+  ENTITIES: [__dirname + "/domain/*.entity{.ts,.js}"],
+  MIGRATIONS: [__dirname + "/migrations/**/*{.ts,.js}"],
+  CLI: {
+    migrationsDir: "src/migrations",
+  },
+  MIGRATIONS_RUN: true,
+};
+
+let ormconfig: any = {
+  name: "default",
+  type: "mongodb",
+  database: "food-delivery-app",
+  url: "mongodb://admin:admin9@ds237475.mlab.com:37475/food-delivery-app",
+  logging: false,
+  synchronize: commonConf.SYNCRONIZE,
+  entities: commonConf.ENTITIES,
+  migrations: commonConf.MIGRATIONS,
+  cli: commonConf.CLI,
+  useUnifiedTopology: true,
+  migrationsRun: commonConf.MIGRATIONS_RUN,
+};
+
+if (process.env.NODE_ENV === "dev") {
+  ormconfig = {
+    name: "default",
+    type: "mysql",
+    database: "food-delivery",
+    url: "mysql://root:root@192.168.64.2/food-delivery",
+    logging: false,
+    synchronize: commonConf.SYNCRONIZE,
+    entities: commonConf.ENTITIES,
+    migrations: commonConf.MIGRATIONS,
+    cli: commonConf.CLI,
+    migrationsRun: commonConf.MIGRATIONS_RUN,
+  };
+}
+
+if (process.env.NODE_ENV === "prod") {
+  ormconfig = {
+    name: "default",
+    type: "mysql",
+    database: "food-delivery",
+    url: "mysql://root:root@192.168.64.2/food-delivery",
+    logging: false,
+    synchronize: commonConf.SYNCRONIZE,
+    entities: commonConf.ENTITIES,
+    migrations: commonConf.MIGRATIONS,
+    cli: commonConf.CLI,
+    migrationsRun: commonConf.MIGRATIONS_RUN,
+  };
+}
+
+if (process.env.NODE_ENV === "sqlite") {
+  ormconfig = {
+    name: "default",
+    type: "sqlite",
+    database: "../target/sqlite-dev-db.sql",
+    logging: true,
+    synchronize: true,
+    entities: commonConf.ENTITIES,
+    migrations: commonConf.MIGRATIONS,
+    cli: commonConf.CLI,
+    migrationsRun: commonConf.MIGRATIONS_RUN,
+  };
+}
+
+if (process.env.NODE_ENV === "test") {
+  ormconfig = {
+    name: "default",
+    type: "sqlite",
+    database: ":memory:",
+    logging: true,
+    synchronize: true,
+    entities: commonConf.ENTITIES,
+    migrations: commonConf.MIGRATIONS,
+    cli: commonConf.CLI,
+    migrationsRun: commonConf.MIGRATIONS_RUN,
+  };
+}
+
+export { ormconfig };
