cb014ebab8f8b958991c682fedd8b4174ff1c6f3
ebatgerel <batgerel742@gmail.com>
2020-11-03 22:30:37 +0800

    pages updated

--- ubereats-frontend/src/app/app.module.ts
+++ ubereats-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeRestaurantComponent } from './home-restaurant/home-restaurant.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeRestaurantComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
