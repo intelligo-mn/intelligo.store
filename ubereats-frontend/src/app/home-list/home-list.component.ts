f83debd859174ee3beef6e22d66bf5617a7a9256
batgerel.e <batgerel.e@chinggis.systems>
2020-11-05 13:50:19 +0800

    added some data

--- ubereats-frontend/src/app/home-list/home-list.component.ts
+++ ubereats-frontend/src/app/home-list/home-list.component.ts
@@ -46,6 +46,42 @@ export class HomeListComponent implements OnInit {
         price: '20.00$',
         image: '../../assets/images/food.jpg',
       },
+      {
+        key: 7,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 8,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 9,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 10,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 11,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 12,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
     ];
   }
 
