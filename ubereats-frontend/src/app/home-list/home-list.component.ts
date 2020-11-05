5997afd6b9f5f0088df42c7a0c0d0196effd7222
batgerel.e <batgerel.e@chinggis.systems>
2020-11-05 13:44:09 +0800

    list added

---
+++ ubereats-frontend/src/app/home-list/home-list.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-list',
+  templateUrl: './home-list.component.html',
+  styleUrls: ['./home-list.component.scss'],
+})
+export class HomeListComponent implements OnInit {
+  datas: Object[];
+
+  constructor() {
+    this.datas = [
+      {
+        key: 1,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 2,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 3,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 4,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 5,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+      {
+        key: 6,
+        name: 'Demo food name',
+        price: '20.00$',
+        image: '../../assets/images/food.jpg',
+      },
+    ];
+  }
+
+  ngOnInit(): void {}
+}
