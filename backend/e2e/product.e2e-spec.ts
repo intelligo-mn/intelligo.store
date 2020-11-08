671872163432bb3e246084540b4dbb65799b5838
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-08 10:04:05 +0800

    created e2e tests

---
+++ backend/e2e/product.e2e-spec.ts
@@ -0,0 +1,83 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import request = require('supertest');
+import { AppModule } from '../src/app.module';
+import { INestApplication } from '@nestjs/common';
+import { AuthGuard, RolesGuard } from '../src/core';
+import Product from '../src/domain/product.entity';
+import { ProductService } from '../src/modules/product/product.service';
+
+describe('Product Controller', () => {
+  let app: INestApplication;
+
+  const authGuardMock = { canActivate: (): any => true };
+  const rolesGuardMock = { canActivate: (): any => true };
+  const entityMock: any = {
+    id: 'entityId',
+  };
+
+  const serviceMock = {
+    findById: (): any => entityMock,
+    findAndCount: (): any => [entityMock, 0],
+    save: (): any => entityMock,
+    update: (): any => entityMock,
+    delete: (): any => entityMock,
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(authGuardMock)
+      .overrideGuard(RolesGuard)
+      .useValue(rolesGuardMock)
+      .overrideProvider(ProductService)
+      .useValue(serviceMock)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET all products ', async () => {
+    const getEntities: Product[] = (await request(app.getHttpServer()).get('/api/products').expect(200)).body;
+
+    expect(getEntities).toEqual(entityMock);
+  });
+
+  it('/GET products by id', async () => {
+    const getEntity: Product = (
+      await request(app.getHttpServer())
+        .get('/api/products/' + entityMock.id)
+        .expect(200)
+    ).body;
+
+    expect(getEntity).toEqual(entityMock);
+  });
+
+  it('/POST create products', async () => {
+    const createdEntity: Product = (await request(app.getHttpServer()).post('/api/products').send(entityMock).expect(201)).body;
+
+    expect(createdEntity).toEqual(entityMock);
+  });
+
+  it('/PUT update products', async () => {
+    const updatedEntity: Product = (await request(app.getHttpServer()).put('/api/products').send(entityMock).expect(201)).body;
+
+    expect(updatedEntity).toEqual(entityMock);
+  });
+
+  it('/DELETE products', async () => {
+    const deletedEntity: Product = (
+      await request(app.getHttpServer())
+        .delete('/api/products/' + entityMock.id)
+        .expect(204)
+    ).body;
+
+    expect(deletedEntity).toEqual({});
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
