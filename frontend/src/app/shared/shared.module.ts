925ce45e2711086a9c40f6f8dd73a6746b24d02e
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-08 10:01:38 +0800

    fix throttling history state changes to prevent the browser from hanging #6

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedLibsModule } from './shared-libs.module';
-import { AlertComponent } from './alert/alert.component';
-import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
 
 @NgModule({
   imports: [SharedLibsModule],
-  declarations: [AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
+  declarations: [LoginModalComponent, HasAnyAuthorityDirective],
   entryComponents: [LoginModalComponent],
-  exports: [SharedLibsModule, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
+  exports: [SharedLibsModule, LoginModalComponent, HasAnyAuthorityDirective],
 })
 export class SharedModule {}
