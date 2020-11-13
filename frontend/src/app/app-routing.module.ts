6a1cf33836449ce32d466cd37f029fa770ed133a
ebatgerel <batgerel742@gmail.com>
2020-11-14 00:39:10 +0800

    detail changed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './home-list/detail/detail.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '/', pathMatch: 'full' },
+  { path: 'item/:id', component: DetailComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
