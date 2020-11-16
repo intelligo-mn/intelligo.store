5dbca62c046e6e3a04f78bf9d6b6ce618a21747d
batgerel.e <batgerel.e@chinggis.systems>
2020-11-16 10:43:49 +0800

    Added address modal

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) {}
 
   public isMenuCollapsed = true;
 
   ngOnInit(): void {}
+
+  open(content) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {},
+        (reason) => {}
+      );
+  }
 }
