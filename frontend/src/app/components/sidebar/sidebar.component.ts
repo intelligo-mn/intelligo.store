5da10947f8fe45445d91d520ca2d1c7209bcfb3e
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-26 05:11:19 +0800

    fixed sidebar menu items

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -10,30 +10,22 @@ declare interface RouteInfo {
 }
 export const MAIN_ROUTES: RouteInfo[] = [{ path: '/dashboard', title: 'Удирдлагын хэсэг', icon: 'ni-tv-2 text-primary', class: '' }];
 
-export const CASH_CAPITAL_ROUTES: RouteInfo[] = [
-  { path: '/cash-capital/transaction', title: 'Орлого Зарлага', icon: 'ni-credit-card text-blue', class: '' },
-  { path: '/cash-capital/other', title: 'Бусад гүйлгээ', icon: 'ni-bullet-list-67 text-red', class: '' },
+export const PRODUCT_ROUTES: RouteInfo[] = [
+  { path: '/product', title: 'Бүтээгдэхүүн', icon: 'ni-tv-2 text-primary', class: '' },
+  { path: '/category', title: 'Бүтээгдэхүүний төрөл', icon: 'ni-button-pause text-yellow', class: '' },
+  { path: '/unit', title: 'Хэмжих нэгж', icon: 'ni-bullet-list-67 text-red', class: '' },
 ];
 
-export const INVENTORY_ROUTES: RouteInfo[] = [
-  { path: '/inventory/purchase', title: 'Худалдан авалт', icon: 'ni-tv-2 text-primary', class: '' },
-  { path: '/inventory/sale', title: 'Борлуулалт', icon: 'ni-credit-card text-blue', class: '' },
-  { path: '/inventory/refund', title: 'Буцаалт', icon: 'ni-chart-bar-32 text-orange', class: '' },
-  { path: '/inventory/extenditure', title: 'Зарлага', icon: 'ni-button-pause text-yellow', class: '' },
-  { path: '/inventory/movement', title: 'Дотоод хөдөлгөөн', icon: 'ni-bullet-list-67 text-red', class: '' },
+export const ORDER_ROUTES: RouteInfo[] = [
+  { path: '/order', title: 'Захиалга', icon: 'ni-tv-2 text-primary', class: '' },
+  { path: '/order/create', title: 'Захиалга хийх', icon: 'ni-credit-card text-blue', class: '' },
+  { path: '/package', title: 'Захиалгын багц', icon: 'ni-credit-card text-blue', class: '' },
+  { path: '/package/select', title: 'Захиалгын багц үүсгэх', icon: 'ni-credit-card text-blue', class: '' },
 ];
-
-export const MAIN_CAPITAL_ROUTES: RouteInfo[] = [
-  { path: '/main-capital/purchase', title: 'Худалдан авалт', icon: 'ni-tv-2 text-primary', class: '' },
-  { path: '/main-capital/sale', title: 'Борлуулалт', icon: 'ni-credit-card text-blue', class: '' },
-  { path: '/main-capital/expenditure', title: 'Зарлага', icon: 'ni-button-pause text-yellow', class: '' },
-  { path: '/main-capital/movement', title: 'Дотоод хөдөлгөөн', icon: 'ni-bullet-list-67 text-red', class: '' },
-  { path: '/main-capital/deprecation', title: 'Элэгдэл тооцох', icon: 'ni-chart-bar-32 text-orange', class: '' },
+export const OTHER_ROUTES: RouteInfo[] = [
+  { path: '/organization', title: 'Харилцагч', icon: 'ni-bullet-list-67 text-red', class: '' },
+  { path: '/report', title: 'Тайлан', icon: 'ni-bullet-list-67 text-red', class: '' },
 ];
-export const MAIN_SALARY_ROUTES: RouteInfo[] = [
-  { path: '/salary', title: 'Цалин', icon: 'ni-bullet-list-67 text-red', class: '' },
-];
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -41,10 +33,9 @@ export const MAIN_SALARY_ROUTES: RouteInfo[] = [
 })
 export class SidebarComponent implements OnInit {
   public menuItems: any[];
-  public cashMenuItems: any[];
-  public inventoryMenuItems: any[];
-  public mainCapitalMenuItems: any[];
-  public salaryMenuItems: any[];
+  public productMenuItems: any[];
+  public orderMenuItems: any[];
+  public otherMenuItems: any[];
 
   public isCollapsed = true;
 
@@ -52,10 +43,9 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit() {
     this.menuItems = MAIN_ROUTES.filter(menuItem => menuItem);
-    this.cashMenuItems = CASH_CAPITAL_ROUTES.filter(menuItem => menuItem);
-    this.inventoryMenuItems = INVENTORY_ROUTES.filter(menuItem => menuItem);
-    this.mainCapitalMenuItems = MAIN_CAPITAL_ROUTES.filter(menuItem => menuItem);
-    this.salaryMenuItems = MAIN_SALARY_ROUTES.filter(menuItem => menuItem);
+    this.productMenuItems = PRODUCT_ROUTES.filter(menuItem => menuItem);
+    this.orderMenuItems = ORDER_ROUTES.filter(menuItem => menuItem);
+    this.otherMenuItems = OTHER_ROUTES.filter(menuItem => menuItem);
 
     this.router.events.subscribe(event => {
       this.isCollapsed = false;
