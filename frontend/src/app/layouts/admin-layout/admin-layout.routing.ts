edd0d424b7044c9e189277bf92f7946a45110364
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-25 10:57:30 +0800

    added lazy loaded modules to admin route

--- frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -8,4 +8,64 @@ export const AdminLayoutRoutes: Routes = [
     component: DashboardComponent,
     canActivate: [AuthGuard],
   },
+  {
+    path: 'category',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./../../modules/category/category.module').then(m => m.CategoryModule),
+      },
+    ],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'order',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./../../modules/order/order.module').then(m => m.OrderModule),
+      },
+    ],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'organization',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./../../modules/organization/organization.module').then(m => m.OrganizationModule),
+      },
+    ],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'package',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./../../modules/package/package.module').then(m => m.PackageModule),
+      },
+    ],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'product',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./../../modules/product/product.module').then(m => m.ProductModule),
+      },
+    ],
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'unit',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./../../modules/unit/unit.module').then(m => m.UnitModule),
+      },
+    ],
+    canActivate: [AuthGuard],
+  },
 ];
