7ec7ffe0eebaf1c62e370c904fa0f12a842306ec
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-25 12:57:53 +0800

    захиалга модул

---
+++ frontend/src/app/modules/order/order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss']
+})
+export class OrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
