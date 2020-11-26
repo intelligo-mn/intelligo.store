3f6b192da39a84340848da39af8018f46c921eb0
batgerel.e <batgerel.e@chinggis.systems>
2020-11-26 10:33:38 +0800

    translation added for login pages

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private translate: TranslateService) {}
 
   active = 1;
 
