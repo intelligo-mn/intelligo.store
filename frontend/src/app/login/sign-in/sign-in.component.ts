8b33a13e39599ea1967ea55b0c525878319f0643
batgerel.e <batgerel.e@chinggis.systems>
2020-11-26 10:33:38 +0800

    translation added for login pages

--- frontend/src/app/login/sign-in/sign-in.component.ts
+++ frontend/src/app/login/sign-in/sign-in.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- frontend/src/app/login/sign-up/sign-up.component.ts
+++ frontend/src/app/login/sign-up/sign-up.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(private translate: TranslateService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
