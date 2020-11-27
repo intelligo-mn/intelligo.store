861345fda8c69d3b6c413d042280a508250c34b9
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-27 10:43:09 +0800

    changed some entities

--- backend/src/domain/user.entity.ts
+++ backend/src/domain/user.entity.ts
@@ -1,45 +1,38 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { Column, Entity, JoinTable, ManyToMany } from "typeorm";
+import { EncryptionTransformer } from "typeorm-encrypted";
+import { config } from "../config";
 import { Authority } from "./authority.entity";
 import { BaseEntity } from "./base/base.entity";
 
-@Entity("user")
+@Entity("nhi_user")
 export class User extends BaseEntity {
-  @ApiProperty({
-    uniqueItems: true,
-    example: "myuser",
-    description: "User login",
-  })
   @Column({ unique: true })
   login: string;
-  @ApiProperty({ example: "MyUser", description: "User first name" })
-  @Column()
-  firstName: string;
-  @ApiProperty({ example: "MyUser", description: "User last name" })
-  @Column()
-  lastName: string;
-  @ApiProperty({ example: "myuser@localhost", description: "User email" })
   @Column({ nullable: true })
-  email?: string;
-  @ApiProperty({ example: "true", description: "User activation" })
+  firstName?: string;
+  @Column({ nullable: true })
+  lastName?: string;
   @Column()
-  activated: boolean;
-  @ApiProperty({ example: "en", description: "User language" })
-  @Column()
-  langKey: string;
+  email: string;
+  @Column({ default: false })
+  activated?: boolean;
+  @Column({ default: "en" })
+  langKey?: string;
 
   // eslint-disable-next-line
   @ManyToMany((type) => Authority)
   @JoinTable()
-  @ApiProperty({
-    isArray: true,
-    enum: ["ROLE_USER", "ROLE_ADMIN", "ROLE_ANONYMOUS"],
-    description: "Array of permissions",
-  })
   authorities?: any[];
 
-  @ApiProperty({ example: "myuser", description: "User password" })
-  @Column()
+  @Column({
+    type: "varchar",
+    transformer: new EncryptionTransformer({
+      key: config.get("crypto.key"),
+      algorithm: "aes-256-cbc",
+      ivLength: 16,
+      iv: config.get("crypto.iv"),
+    }),
+  })
   password: string;
   @Column({ nullable: true })
   imageUrl?: string;
