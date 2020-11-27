2aeede7728e954b901d294a5f1e0ec4bbf508b2f
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-27 10:43:09 +0800

    changed some entities

--- backend/src/domain/organization.entity.ts
+++ backend/src/domain/organization.entity.ts
@@ -1,36 +1,33 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Entity, Column, JoinColumn, OneToOne, ManyToOne, OneToMany, ManyToMany, JoinTable } from 'typeorm';
-import { BaseEntity } from './base/base.entity';
-
-import { Contact } from './contact.entity';
-import { Category } from './category.entity';
-import { Customer } from './customer.entity';
-import { OrganizationStatus } from './enumeration/organization-status';
-import { OrganizationType } from './enumeration/organization-type';
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from "typeorm";
+import { BaseEntity } from "./base/base.entity";
+import { Category } from "./category.entity";
+import { Contact } from "./contact.entity";
+import { Customer } from "./customer.entity";
+import { OrganizationStatus } from "./enumeration/organization-status";
+import { OrganizationType } from "./enumeration/organization-type";
 
 /**
  * A Organization.
  */
-@Entity('organization')
+@Entity("organization")
 export class Organization extends BaseEntity {
-  @Column({ name: 'name' })
+  @Column({ name: "name" })
   name: string;
 
-  @Column({ type: 'simple-enum', name: 'status', enum: OrganizationStatus })
+  @Column({ type: "simple-enum", name: "status", enum: OrganizationStatus })
   status: OrganizationStatus;
 
-  @Column({ type: 'simple-enum', name: 'type', enum: OrganizationType })
+  @Column({ type: "simple-enum", name: "type", enum: OrganizationType })
   type: OrganizationType;
 
-  @OneToOne(type => Contact)
+  @OneToOne((type) => Contact)
   @JoinColumn()
   contact: Contact;
 
-  @ManyToOne(type => Category)
+  @ManyToOne((type) => Category)
   distributeType: Category;
 
-  @ManyToOne(type => Customer)
+  @ManyToOne((type) => Customer)
   manager: Customer;
-
-  // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
 }
