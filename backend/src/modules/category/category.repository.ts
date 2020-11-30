2541452a7d8890e23e94b52c099da55344cf8def
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-11-30 09:16:25 +0800

    fixed header unil and removed unused files

--- backend/src/modules/category/category.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import Category from "../../domain/category";
-
-@EntityRepository(Category)
-export class CategoryRepository extends Repository<Category> {}
