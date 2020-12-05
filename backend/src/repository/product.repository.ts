b37609e5ba8fe638caa70ef090da6b295f4fa08c
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2020-12-05 21:34:44 +0800

    fixed repositories

--- backend/src/repository/product.repository.ts
+++ backend/src/repository/product.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
-import Product from '../../domain/product.entity';
+import { Product } from '../domain/product.entity';
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {}
