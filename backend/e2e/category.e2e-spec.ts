32c8efc4eda4affd389fccbb113dc98f928f827a
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-12-05 09:23:10 +0800

    created category module with service

---
+++ backend/e2e/category.e2e-spec.ts
@@ -0,0 +1,98 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import request = require('supertest');
+import { AppModule } from '../src/app.module';
+import { INestApplication } from '@nestjs/common';
+import { AuthGuard } from '../src/security/guards/auth.guard';
+import { RolesGuard } from '../src/security/guards/roles.guard';
+import { CategoryDTO } from '../src/service/dto/category.dto';
+import { CategoryService } from '../src/service/category.service';
+
+describe('Category Controller', () => {
+  let app: INestApplication;
+
+  const authGuardMock = { canActivate: (): any => true };
+  const rolesGuardMock = { canActivate: (): any => true };
+  const entityMock: any = {
+    id: 'entityId'
+  };
+
+  const serviceMock = {
+    findById: (): any => entityMock,
+    findAndCount: (): any => [entityMock, 0],
+    save: (): any => entityMock,
+    update: (): any => entityMock,
+    deleteById: (): any => entityMock
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(authGuardMock)
+      .overrideGuard(RolesGuard)
+      .useValue(rolesGuardMock)
+      .overrideProvider(CategoryService)
+      .useValue(serviceMock)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET all categories ', async () => {
+    const getEntities: CategoryDTO[] = (
+      await request(app.getHttpServer())
+        .get('/api/categories')
+        .expect(200)
+    ).body;
+
+    expect(getEntities).toEqual(entityMock);
+  });
+
+  it('/GET categories by id', async () => {
+    const getEntity: CategoryDTO = (
+      await request(app.getHttpServer())
+        .get('/api/categories/' + entityMock.id)
+        .expect(200)
+    ).body;
+
+    expect(getEntity).toEqual(entityMock);
+  });
+
+  it('/POST create categories', async () => {
+    const createdEntity: CategoryDTO = (
+      await request(app.getHttpServer())
+        .post('/api/categories')
+        .send(entityMock)
+        .expect(201)
+    ).body;
+
+    expect(createdEntity).toEqual(entityMock);
+  });
+
+  it('/PUT update categories', async () => {
+    const updatedEntity: CategoryDTO = (
+      await request(app.getHttpServer())
+        .put('/api/categories')
+        .send(entityMock)
+        .expect(201)
+    ).body;
+
+    expect(updatedEntity).toEqual(entityMock);
+  });
+
+  it('/DELETE categories', async () => {
+    const deletedEntity: CategoryDTO = (
+      await request(app.getHttpServer())
+        .delete('/api/categories/' + entityMock.id)
+        .expect(204)
+    ).body;
+
+    expect(deletedEntity).toEqual({});
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
