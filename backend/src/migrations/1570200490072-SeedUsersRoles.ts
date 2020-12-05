7e646adead6da2e6b886da80850b137300df55bd
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-12-05 09:20:59 +0800

    fixed migrations

--- backend/src/migrations/1570200490072-SeedUsersRoles.ts
+++ backend/src/migrations/1570200490072-SeedUsersRoles.ts
@@ -1,75 +1,80 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
 import { User } from '../domain/user.entity';
 import { Authority } from '../domain/authority.entity';
 
 export class SeedUsersRoles1570200490072 implements MigrationInterface {
-  role1: Authority = { name: 'ROLE_ADMIN' };
+    role1: Authority = { name: 'ROLE_ADMIN' };
 
-  role2: Authority = { name: 'ROLE_USER' };
+    role2: Authority = { name: 'ROLE_USER' };
 
-  user1: User = {
-    login: 'system',
-    password: 'system',
-    firstName: 'System',
-    lastName: 'System',
-    email: 'system@localhost.it',
-    imageUrl: '',
-    activated: true,
-    langKey: 'en',
-    createdBy: 'system',
-    lastModifiedBy: 'system',
-  };
+    user1: User = {
+        login: 'system',
+        password: 'system',
+        firstName: 'System',
+        lastName: 'System',
+        email: 'system@localhost.it',
+        imageUrl: '',
+        activated: true,
+        langKey: 'en',
+        createdBy: 'system',
+        lastModifiedBy: 'system',
+    };
 
-  user2: User = {
-    login: 'anonymoususer',
-    password: 'anonymoususer',
-    firstName: 'Anonymous',
-    lastName: 'User',
-    email: 'anonymoususer@localhost.it',
-    imageUrl: '',
-    activated: true,
-    langKey: 'en',
-    createdBy: 'system',
-    lastModifiedBy: 'system',
-  };
+    user2: User = {
+        login: 'anonymoususer',
+        password: 'anonymoususer',
+        firstName: 'Anonymous',
+        lastName: 'User',
+        email: 'anonymoususer@localhost.it',
+        imageUrl: '',
+        activated: true,
+        langKey: 'en',
+        createdBy: 'system',
+        lastModifiedBy: 'system',
+    };
 
-  user3: User = {
-    login: 'admin',
-    password: 'admin',
-    firstName: 'Administrator',
-    lastName: 'Administrator',
-    email: 'admin@localhost.it',
-    imageUrl: '',
-    activated: true,
-    langKey: 'en',
-    createdBy: 'system',
-    lastModifiedBy: 'system',
-  };
+    user3: User = {
+        login: 'admin',
+        password: 'admin',
+        firstName: 'Administrator',
+        lastName: 'Administrator',
+        email: 'admin@localhost.it',
+        imageUrl: '',
+        activated: true,
+        langKey: 'en',
+        createdBy: 'system',
+        lastModifiedBy: 'system',
+    };
 
-  user4: User = {
-    login: 'user',
-    password: 'user',
-    firstName: 'User',
-    lastName: 'User',
-    email: 'user@localhost.it',
-    imageUrl: '',
-    activated: true,
-    langKey: 'en',
-    createdBy: 'system',
-    lastModifiedBy: 'system',
-  };
+    user4: User = {
+        login: 'user',
+        password: 'user',
+        firstName: 'User',
+        lastName: 'User',
+        email: 'user@localhost.it',
+        imageUrl: '',
+        activated: true,
+        langKey: 'en',
+        createdBy: 'system',
+        lastModifiedBy: 'system',
+    };
 
+    // eslint-disable-next-line
   public async up(queryRunner: QueryRunner): Promise<any> {
-    const conn = queryRunner.connection;
-    await conn.createQueryBuilder().insert().into(Authority).values([this.role1, this.role2]).execute();
+        const authorityRepository = getRepository('nhi_authority');
 
-    await conn.createQueryBuilder().insert().into(User).values([this.user1, this.user2, this.user3, this.user4]).execute();
+        const adminRole = await authorityRepository.save(this.role1);
+        const userRole = await authorityRepository.save(this.role2);
 
-    await conn.createQueryBuilder().relation(User, 'authorities').of([this.user1, this.user3]).add([this.role1, this.role2]);
+        const userRepository = getRepository('nhi_user');
 
-    await conn.createQueryBuilder().relation(User, 'authorities').of(this.user4).add([this.role2]);
-  }
+        this.user1.authorities = [adminRole, userRole];
+        this.user3.authorities = [adminRole, userRole];
+        this.user4.authorities = [userRole];
 
-  // eslint-disable-next-line
+        await userRepository.save([this.user1, this.user2, this.user3, this.user4]);
+    }
+
+    // eslint-disable-next-line
   public async down(queryRunner: QueryRunner): Promise<any> {}
 }
