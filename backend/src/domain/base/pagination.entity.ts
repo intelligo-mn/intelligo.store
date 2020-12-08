b5217e53d3b9561e3ba43d284853b5d4cbea5df4
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-12-08 10:36:47 +0800

    fixed Argument of type 'string | string[] | ParsedQs | ParsedQs[]' is not assignable to parameter of type 'string | number' #6

--- backend/src/domain/base/pagination.entity.ts
+++ backend/src/domain/base/pagination.entity.ts
@@ -26,7 +26,7 @@ export class PageRequest {
     @Type(() => Sort)
     sort: Sort = new Sort('id,ASC');
 
-    constructor(page: number | string, size: number | string, sort: string) {
+    constructor(page: number | string | any, size: number | string | any, sort: string | any) {
         this.page = +page || this.page;
         this.size = +size || this.size;
         this.sort = sort ? new Sort(sort) : this.sort;
