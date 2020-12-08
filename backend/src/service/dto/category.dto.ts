e3a40fedcb8e4ed3f25df26f6d04328e5d73f2d8
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-12-08 10:36:47 +0800

    fixed Argument of type 'string | string[] | ParsedQs | ParsedQs[]' is not assignable to parameter of type 'string | number' #6

--- backend/src/service/dto/category.dto.ts
+++ backend/src/service/dto/category.dto.ts
@@ -1,27 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MinLength, MaxLength, Length, Min, Max, Matches } from 'class-validator';
-import { BaseDTO } from './base.dto';
-
-import { ProductDTO } from './product.dto';
-import { OrganizationDTO } from './organization.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { BaseDTO } from "./base.dto";
+import { OrganizationDTO } from "./organization.dto";
+import { ProductDTO } from "./product.dto";
 
 /**
  * A Category DTO object.
  */
 export class CategoryDTO extends BaseDTO {
   @IsNotEmpty()
-  @ApiProperty({ description: 'name field' })
+  @ApiProperty({ description: "name field" })
   name: string;
 
-  @ApiProperty({ description: 'description field', required: false })
+  @ApiProperty({ description: "description field", required: false })
   description: string;
 
-  @ApiProperty({ type: ProductDTO, isArray: true, description: 'products relationship' })
+  @ApiProperty({
+    type: ProductDTO,
+    isArray: true,
+    description: "products relationship",
+  })
   products: ProductDTO[];
 
-  @ApiProperty({ type: OrganizationDTO, isArray: true, description: 'organizations relationship' })
+  @ApiProperty({
+    type: OrganizationDTO,
+    isArray: true,
+    description: "organizations relationship",
+  })
   organizations: OrganizationDTO[];
-
-  
 }
--- backend/src/service/dto/organization.dto.ts
+++ backend/src/service/dto/organization.dto.ts
@@ -1,38 +1,38 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MinLength, MaxLength, Length, Min, Max, Matches } from 'class-validator';
-import { BaseDTO } from './base.dto';
-
-import { ContactDTO } from './contact.dto';
-import { CategoryDTO } from './category.dto';
-import { CustomerDTO } from './customer.dto';
-import { OrganizationStatus } from '../../domain/enumeration/organization-status';
-import { OrganizationType } from '../../domain/enumeration/organization-type';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { OrganizationStatus } from "../../domain/enumeration/organization-status";
+import { OrganizationType } from "../../domain/enumeration/organization-type";
+import { BaseDTO } from "./base.dto";
+import { CategoryDTO } from "./category.dto";
+import { ContactDTO } from "./contact.dto";
+import { CustomerDTO } from "./customer.dto";
 
 /**
  * A Organization DTO object.
  */
 export class OrganizationDTO extends BaseDTO {
   @IsNotEmpty()
-  @ApiProperty({ description: 'name field' })
+  @ApiProperty({ description: "name field" })
   name: string;
 
   @IsNotEmpty()
-  @ApiProperty({ enum: OrganizationStatus, description: 'status enum field' })
+  @ApiProperty({ enum: OrganizationStatus, description: "status enum field" })
   status: OrganizationStatus;
 
   @IsNotEmpty()
-  @ApiProperty({ enum: OrganizationType, description: 'type enum field' })
+  @ApiProperty({ enum: OrganizationType, description: "type enum field" })
   type: OrganizationType;
 
-  @ApiProperty({ type: ContactDTO, description: 'contact relationship' })
+  @ApiProperty({ type: ContactDTO, description: "contact relationship" })
   contact: ContactDTO;
 
-  @ApiProperty({ type: CategoryDTO, description: 'distributeType relationship' })
+  @ApiProperty({
+    type: CategoryDTO,
+    description: "distributeType relationship",
+  })
   distributeType: CategoryDTO;
 
-  @ApiProperty({ type: CustomerDTO, description: 'manager relationship' })
+  @ApiProperty({ type: CustomerDTO, description: "manager relationship" })
   manager: CustomerDTO;
-
-  
 }
