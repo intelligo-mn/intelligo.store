9257db85dfed2ec1af8bdbf57283f9691b4d3137
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-12-09 11:56:56 +0800

    UnhandledPromiseRejectionWarning: Error: A circular dependency has been detected #10

--- backend/src/service/dto/order.dto.ts
+++ backend/src/service/dto/order.dto.ts
@@ -1,29 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MinLength, MaxLength, Length, Min, Max, Matches } from 'class-validator';
-import { BaseDTO } from './base.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import {
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  Length,
+  Min,
+  Max,
+  Matches,
+} from "class-validator";
+import { BaseDTO } from "./base.dto";
 
-import { OrderItemDTO } from './order-item.dto';
-import { CustomerDTO } from './customer.dto';
-import { OrderStatus } from '../../domain/enumeration/order-status';
+import { OrderItemDTO } from "./order-item.dto";
+import { CustomerDTO } from "./customer.dto";
+import { OrderStatus } from "../../domain/enumeration/order-status";
 
 /**
  * A Order DTO object.
  */
 export class OrderDTO extends BaseDTO {
   @IsNotEmpty()
-  @ApiProperty({ description: 'distributionDate field' })
+  @ApiProperty({ description: "distributionDate field" })
   distributionDate: any;
 
   @IsNotEmpty()
-  @ApiProperty({ enum: OrderStatus, description: 'status enum field' })
+  @ApiProperty({ enum: OrderStatus, description: "status enum field" })
   status: OrderStatus;
 
-  @ApiProperty({ type: OrderItemDTO, description: 'products relationship' })
+  @ApiProperty({ type: OrderItemDTO, description: "products relationship" })
   products: OrderItemDTO;
 
-  @ApiProperty({ type: CustomerDTO, description: 'manager relationship' })
+  @ApiProperty({ type: () => CustomerDTO, description: "manager relationship" })
   manager: CustomerDTO;
-
-  
 }
--- backend/src/service/dto/product.dto.ts
+++ backend/src/service/dto/product.dto.ts
@@ -1,27 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MinLength, MaxLength, Length, Min, Max, Matches } from 'class-validator';
-import { BaseDTO } from './base.dto';
-
-import { CategoryDTO } from './category.dto';
-import { UnitDTO } from './unit.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { BaseDTO } from "./base.dto";
+import { CategoryDTO } from "./category.dto";
+import { UnitDTO } from "./unit.dto";
 
 /**
  * A Product DTO object.
  */
 export class ProductDTO extends BaseDTO {
   @IsNotEmpty()
-  @ApiProperty({ description: 'name field' })
+  @ApiProperty({ description: "name field" })
   name: string;
 
-  @ApiProperty({ description: 'description field', required: false })
+  @ApiProperty({ description: "description field", required: false })
   description: string;
 
-  @ApiProperty({ type: CategoryDTO, description: 'category relationship' })
+  @ApiProperty({
+    type: () => CategoryDTO,
+    description: "category relationship",
+  })
   category: CategoryDTO;
 
-  @ApiProperty({ type: UnitDTO, description: 'unit relationship' })
+  @ApiProperty({ type: UnitDTO, description: "unit relationship" })
   unit: UnitDTO;
-
-  
 }
