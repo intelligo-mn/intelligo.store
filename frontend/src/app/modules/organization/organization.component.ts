4d03828dc098b7ebaef0e88e550e7f9fd8bbffbf
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-12-10 18:35:17 +0800

    changed org module

--- frontend/src/app/modules/organization/organization.component.ts
+++ frontend/src/app/modules/organization/organization.component.ts
@@ -1,15 +1,106 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { JhiEventManager } from 'ng-jhipster';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { IOrganization } from 'src/app/shared/model/organization.model';
+
+import { ITEMS_PER_PAGE } from 'src/app/shared/constants/pagination.constants';
+import { OrganizationService } from './organization.service';
+import { OrganizationDeleteDialogComponent } from './organization-delete-dialog.component';
 
 @Component({
-  selector: 'app-organization',
-  templateUrl: './organization.component.html',
-  styleUrls: ['./organization.component.scss']
+  selector: 'jhi-organization',
+  templateUrl: './organization.component.html'
 })
-export class OrganizationComponent implements OnInit {
+export class OrganizationComponent implements OnInit, OnDestroy {
+  organizations?: IOrganization[];
+  eventSubscriber?: Subscription;
+  totalItems = 0;
+  itemsPerPage = ITEMS_PER_PAGE;
+  page!: number;
+  predicate!: string;
+  ascending!: boolean;
+  ngbPaginationPage = 1;
 
-  constructor() { }
+  constructor(
+    protected organizationService: OrganizationService,
+    protected activatedRoute: ActivatedRoute,
+    protected router: Router,
+    protected eventManager: JhiEventManager,
+    protected modalService: NgbModal
+  ) {}
 
-  ngOnInit(): void {
+  loadPage(page?: number): void {
+    const pageToLoad: number = page || this.page;
+
+    this.organizationService
+      .query({
+        page: pageToLoad - 1,
+        size: this.itemsPerPage,
+        sort: this.sort()
+      })
+      .subscribe(
+        (res: HttpResponse<IOrganization[]>) => this.onSuccess(res.body, res.headers, pageToLoad),
+        () => this.onError()
+      );
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(data => {
+      this.page = data.pagingParams.page;
+      this.ascending = data.pagingParams.ascending;
+      this.predicate = data.pagingParams.predicate;
+      this.ngbPaginationPage = data.pagingParams.page;
+      this.loadPage();
+    });
+    this.registerChangeInOrganizations();
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventSubscriber) {
+      this.eventManager.destroy(this.eventSubscriber);
+    }
+  }
+
+  trackId(index: number, item: IOrganization): number {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    return item.id!;
+  }
+
+  registerChangeInOrganizations(): void {
+    this.eventSubscriber = this.eventManager.subscribe('organizationListModification', () => this.loadPage());
+  }
+
+  delete(organization: IOrganization): void {
+    const modalRef = this.modalService.open(OrganizationDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.organization = organization;
+  }
+
+  sort(): string[] {
+    const result = [this.predicate + ',' + (this.ascending ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  protected onSuccess(data: IOrganization[] | null, headers: HttpHeaders, page: number): void {
+    this.totalItems = Number(headers.get('X-Total-Count'));
+    this.page = page;
+    this.router.navigate(['/organization'], {
+      queryParams: {
+        page: this.page,
+        size: this.itemsPerPage,
+        sort: this.predicate + ',' + (this.ascending ? 'asc' : 'desc')
+      }
+    });
+    this.organizations = data || [];
+  }
+
+  protected onError(): void {
+    this.ngbPaginationPage = this.page;
+  }
 }
---
+++ frontend/src/app/modules/organization/organization.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { SERVER_API_URL } from 'src/app/app.constants';
+import { createRequestOption } from 'src/app/shared/util/request-util';
+import { IOrganization } from 'src/app/shared/model/organization.model';
+
+type EntityResponseType = HttpResponse<IOrganization>;
+type EntityArrayResponseType = HttpResponse<IOrganization[]>;
+
+@Injectable({ providedIn: 'root' })
+export class OrganizationService {
+  public resourceUrl = SERVER_API_URL + 'api/organizations';
+
+  constructor(protected http: HttpClient) {}
+
+  create(organization: IOrganization): Observable<EntityResponseType> {
+    return this.http.post<IOrganization>(this.resourceUrl, organization, { observe: 'response' });
+  }
+
+  update(organization: IOrganization): Observable<EntityResponseType> {
+    return this.http.put<IOrganization>(this.resourceUrl, organization, { observe: 'response' });
+  }
+
+  find(id: number): Observable<EntityResponseType> {
+    return this.http.get<IOrganization>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+
+  query(req?: any): Observable<EntityArrayResponseType> {
+    const options = createRequestOption(req);
+    return this.http.get<IOrganization[]>(this.resourceUrl, { params: options, observe: 'response' });
+  }
+
+  delete(id: number): Observable<HttpResponse<{}>> {
+    return this.http.delete(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+}
