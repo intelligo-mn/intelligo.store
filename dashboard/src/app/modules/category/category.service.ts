b90e695caf9a2fa8c2b14b715bdb62c5a12a5877
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2020-12-14 23:46:54 +0800

    created category module

---
+++ dashboard/src/app/modules/category/category.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { createRequestOption } from 'src/app/shared/util/request-util';
+import { ICategory } from 'src/app/shared/model/category.model';
+
+type EntityResponseType = HttpResponse<ICategory>;
+type EntityArrayResponseType = HttpResponse<ICategory[]>;
+
+@Injectable({ providedIn: 'root' })
+export class CategoryService {
+  public resourceUrl = environment.apiUrl + 'api/categories';
+
+  constructor(protected http: HttpClient) {}
+
+  create(category: ICategory): Observable<EntityResponseType> {
+    return this.http.post<ICategory>(this.resourceUrl, category, { observe: 'response' });
+  }
+
+  update(category: ICategory): Observable<EntityResponseType> {
+    return this.http.put<ICategory>(this.resourceUrl, category, { observe: 'response' });
+  }
+
+  find(id: number): Observable<EntityResponseType> {
+    return this.http.get<ICategory>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+
+  query(req?: any): Observable<EntityArrayResponseType> {
+    const options = createRequestOption(req);
+    return this.http.get<ICategory[]>(this.resourceUrl, { params: options, observe: 'response' });
+  }
+
+  delete(id: number): Observable<HttpResponse<{}>> {
+    return this.http.delete(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+}
