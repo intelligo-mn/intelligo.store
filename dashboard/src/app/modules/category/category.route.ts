96f351d5613ae5a4b8938d1982c6c0980867015d
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2020-12-14 23:46:54 +0800

    created category module

---
+++ dashboard/src/app/modules/category/category.route.ts
@@ -0,0 +1,62 @@
+import { HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router, Routes } from '@angular/router';
+import { EMPTY, Observable, of } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
+import { AuthGuard } from 'src/app/core/auth/auth.guard';
+import { Authority } from 'src/app/shared/constants/authority.constants';
+import { Category, ICategory } from 'src/app/shared/model/category.model';
+import { ResolvePagingParams } from 'src/app/shared/services/resolve-paging-param.service';
+import { CategoryUpdateComponent } from './category-update.component';
+import { CategoryComponent } from './category.component';
+import { CategoryService } from './category.service';
+
+@Injectable({ providedIn: 'root' })
+export class CategoryResolve implements Resolve<ICategory> {
+  constructor(private service: CategoryService, private router: Router) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<ICategory> | Observable<never> {
+    const id = route.params['id'];
+    if (id) {
+      return this.service.find(id).pipe(
+        flatMap((category: HttpResponse<Category>) => {
+          if (category.body) {
+            return of(category.body);
+          } else {
+            this.router.navigate(['404']);
+            return EMPTY;
+          }
+        })
+      );
+    }
+    return of(new Category());
+  }
+}
+
+export const categoryRoute: Routes = [
+  {
+    path: '',
+    component: CategoryComponent,
+    resolve: {
+      pagingParams: ResolvePagingParams,
+    },
+    data: {
+      authorities: [Authority.USER],
+      defaultSort: 'id,asc',
+      pageTitle: 'foodorderApp.category.home.title',
+    },
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'new',
+    component: CategoryUpdateComponent,
+    resolve: {
+      category: CategoryResolve,
+    },
+    data: {
+      authorities: [Authority.USER],
+      pageTitle: 'foodorderApp.category.home.title',
+    },
+    canActivate: [AuthGuard],
+  }
+];
