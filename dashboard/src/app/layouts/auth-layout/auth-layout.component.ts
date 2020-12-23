17123c6940681c88e17a271c4f1cd11a3b87e016
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2020-12-23 23:50:28 +0800

    created auth layout module

---
+++ dashboard/src/app/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-layout',
+  templateUrl: './auth-layout.component.html',
+  styleUrls: ['./auth-layout.component.scss'],
+})
+export class AuthLayoutComponent implements OnInit, OnDestroy {
+  test: Date = new Date();
+  public isCollapsed = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const html = document.getElementsByTagName('html')[0];
+    html.classList.add('auth-layout');
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('bg-default');
+    this.router.events.subscribe(event => {
+      this.isCollapsed = true;
+    });
+  }
+  ngOnDestroy() {
+    const html = document.getElementsByTagName('html')[0];
+    html.classList.remove('auth-layout');
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('bg-default');
+  }
+}
