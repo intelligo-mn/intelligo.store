8dbc39232c0078218701621f67512c4583c366e9
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2020-12-23 23:50:28 +0800

    created auth layout module

---
+++ dashboard/src/app/layouts/auth-layout/auth-layout.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../../modules/login/login.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([{ path: 'login', component: LoginComponent }]),
+    NgbModule,
+  ],
+  declarations: [LoginComponent],
+  exports: [LoginComponent],
+})
+export class AuthLayoutModule {}
