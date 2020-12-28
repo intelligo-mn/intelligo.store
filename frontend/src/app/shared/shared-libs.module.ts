147e74a14c63f114e608be2248dd74050a928d47
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-12-29 04:51:14 +0800

    added ngx datatable

--- frontend/src/app/shared/shared-libs.module.ts
+++ frontend/src/app/shared/shared-libs.module.ts
@@ -2,10 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
-  exports: [FormsModule, CommonModule, NgbModule, InfiniteScrollModule, ReactiveFormsModule, TranslateModule],
+  imports: [
+    NgxDatatableModule.forRoot({
+      messages: {
+        selectedMessage: 'Сонгосон өгөгдөл',
+        emptyMessage: 'Хоосон байна',
+        totalMessage: 'Нийт',
+      },
+    }),
+  ],
+  exports: [FormsModule, CommonModule, NgbModule, InfiniteScrollModule, ReactiveFormsModule, NgxDatatableModule],
 })
 export class SharedLibsModule {}
