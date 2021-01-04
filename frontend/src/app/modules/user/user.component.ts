e0f6938e706fb9abc8530d35e532a2c0f6d0e86f
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2021-01-04 10:13:24 +0800

    changed user list

---
+++ frontend/src/app/modules/user/user.component.ts
@@ -0,0 +1,117 @@
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EventManager } from '@devmn/event-manager';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
+import { AccountService } from 'src/app/core/auth/account.service';
+import { Account } from 'src/app/core/user/account.model';
+import { User } from 'src/app/core/user/user.model';
+import { UserService } from 'src/app/core/user/user.service';
+import { ITEMS_PER_PAGE } from 'src/app/shared/constants/pagination.constants';
+import { UserDeleteDialogComponent } from './user-delete-dialog.component';
+
+@Component({
+  selector: 'user-mgmt',
+  templateUrl: './user.component.html',
+})
+export class UserComponent implements OnInit, OnDestroy {
+  currentAccount: Account | null = null;
+  users: User[] | null = null;
+  userListSubscription?: Subscription;
+  totalItems = 0;
+  itemsPerPage = ITEMS_PER_PAGE;
+  page!: number;
+  predicate!: string;
+  previousPage!: number;
+  ascending!: boolean;
+
+  constructor(
+    private userService: UserService,
+    private accountService: AccountService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private eventManager: EventManager,
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.data
+      .pipe(
+        flatMap(
+          () => this.accountService.identity(),
+          (data, account) => {
+            this.page = data.pagingParams.page;
+            this.previousPage = data.pagingParams.page;
+            this.ascending = data.pagingParams.ascending;
+            this.predicate = data.pagingParams.predicate;
+            this.currentAccount = account;
+            this.loadAll();
+            this.userListSubscription = this.eventManager.subscribe('userListModification', () => this.loadAll());
+          }
+        )
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    if (this.userListSubscription) {
+      this.eventManager.destroy(this.userListSubscription);
+    }
+  }
+
+  setActive(user: User, isActivated: boolean): void {
+    this.userService.update({ ...user, activated: isActivated }).subscribe(() => this.loadAll());
+  }
+
+  trackIdentity(index: number, item: User): any {
+    return item.id;
+  }
+
+  loadPage(page: number): void {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.transition();
+    }
+  }
+
+  transition(): void {
+    this.router.navigate(['./'], {
+      relativeTo: this.activatedRoute.parent,
+      queryParams: {
+        page: this.page,
+        sort: this.predicate + ',' + (this.ascending ? 'asc' : 'desc'),
+      },
+    });
+    this.loadAll();
+  }
+
+  deleteUser(user: User): void {
+    const modalRef = this.modalService.open(UserDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.user = user;
+  }
+
+  private loadAll(): void {
+    this.userService
+      .query({
+        page: this.page - 1,
+        size: this.itemsPerPage,
+        sort: this.sort(),
+      })
+      .subscribe((res: HttpResponse<User[]>) => this.onSuccess(res.body, res.headers));
+  }
+
+  private sort(): string[] {
+    const result = [this.predicate + ',' + (this.ascending ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  private onSuccess(users: User[] | null, headers: HttpHeaders): void {
+    this.totalItems = Number(headers.get('X-Total-Count'));
+    this.users = users;
+  }
+}
