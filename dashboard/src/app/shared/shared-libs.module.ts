54c8dfeedbeaa7d0ec272d68155c409abf491ed0
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2021-01-11 11:48:32 +0800

    created shared module

---
+++ dashboard/src/app/shared/shared-libs.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+@NgModule({
+  exports: [FormsModule, CommonModule, NgbModule, InfiniteScrollModule, ReactiveFormsModule],
+})
+export class SharedLibsModule {}
