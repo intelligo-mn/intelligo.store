29931346229058d668040241a3f61c52875e5edc
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2021-01-14 05:50:52 +0800

    moved tdo to domain

---
+++ backend/src/domain/dto/category.dto.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { BaseDTO } from "./base.dto";
+import { OrganizationDTO } from "./organization.dto";
+import { ProductDTO } from "./product.dto";
+
+/**
+ * A Category DTO object.
+ */
+export class CategoryDTO extends BaseDTO {
+  @IsNotEmpty()
+  @ApiProperty({ description: "name field" })
+  name: string;
+
+  @ApiProperty({ description: "description field", required: false })
+  description: string;
+
+  @ApiProperty({
+    type: ProductDTO,
+    isArray: true,
+    description: "products relationship",
+  })
+  products: ProductDTO[];
+
+  @ApiProperty({
+    type: OrganizationDTO,
+    isArray: true,
+    description: "organizations relationship",
+  })
+  organizations: OrganizationDTO[];
+}
