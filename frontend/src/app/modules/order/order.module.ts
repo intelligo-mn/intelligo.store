2b6512f3dc6e8070765e92d9d536f8efba507140
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2021-01-17 06:06:09 +0800

    created order pack select

--- frontend/src/app/modules/order/order.module.ts
+++ frontend/src/app/modules/order/order.module.ts
@@ -3,14 +3,14 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { OrderComponent } from './order.component';
-import { OrderDetailComponent } from './order-detail.component';
+import { OrderPackSelectComponent } from './order-pack-select.component';
 import { OrderUpdateComponent } from './order-update.component';
 import { OrderDeleteDialogComponent } from './order-delete-dialog.component';
 import { orderRoute } from './order.route';
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild(orderRoute)],
-  declarations: [OrderComponent, OrderDetailComponent, OrderUpdateComponent, OrderDeleteDialogComponent],
+  declarations: [OrderComponent, OrderPackSelectComponent, OrderUpdateComponent, OrderDeleteDialogComponent],
   entryComponents: [OrderDeleteDialogComponent],
 })
 export class ChildfoodOrderModule {}
