1d2e2ce647c199c98d6ec351947e6d392a496efa
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2021-02-03 08:04:28 +0800

    ангилал болон төрөл сонголт өгөгдөл дутуу харагдаж байна #40

--- backend/src/modules/category/category.controller.ts
+++ backend/src/modules/category/category.controller.ts
@@ -1,89 +1,126 @@
-import { Body, Controller, Delete, Get, Logger, Param, Post as PostMethod, Put, UseGuards, Req, UseInterceptors } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
-import { Request } from 'express';
-import { CategoryDTO } from '../../domain/dto/category.dto';
-import { CategoryService } from './category.service';
-import { PageRequest, Page } from '../../domain/base/pagination.entity';
-import { AuthGuard, Roles, RolesGuard, RoleType } from '../../core';
-import { HeaderUtil } from '../../core/header-util';
-import { LoggingInterceptor } from '../../core/interceptors/logging.interceptor';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Post as PostMethod,
+  Put,
+  UseGuards,
+  Req,
+  UseInterceptors,
+} from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiTags,
+  ApiResponse,
+  ApiOperation,
+} from "@nestjs/swagger";
+import { Request } from "express";
+import { CategoryDTO } from "../../domain/dto/category.dto";
+import { CategoryService } from "./category.service";
+import { PageRequest, Page } from "../../domain/base/pagination.entity";
+import { AuthGuard, Roles, RolesGuard, RoleType } from "../../core";
+import { HeaderUtil } from "../../core/header-util";
+import { LoggingInterceptor } from "../../core/interceptors/logging.interceptor";
 
-@Controller('api/categories')
+@Controller("api/categories")
 @UseGuards(AuthGuard, RolesGuard)
 @UseInterceptors(LoggingInterceptor)
 @ApiBearerAuth()
-@ApiTags('Categories')
+@ApiTags("Categories")
 export class CategoryController {
-  logger = new Logger('CategoryController');
+  logger = new Logger("CategoryController");
 
   constructor(private readonly categoryService: CategoryService) {}
 
-  @Get('/')
+  @Get("/")
   @Roles(RoleType.ADMIN)
   @ApiResponse({
     status: 200,
-    description: 'List all records',
-    type: CategoryDTO
+    description: "List all records",
+    type: CategoryDTO,
   })
   async getAll(@Req() req: Request): Promise<CategoryDTO[]> {
-    const pageRequest: PageRequest = new PageRequest(req.query.page, req.query.size, req.query.sort);
-    const [results, count] = await this.categoryService.findAndCount({
-      skip: +pageRequest.page * pageRequest.size,
-      take: +pageRequest.size,
-      order: pageRequest.sort.asOrder()
-    });
-    HeaderUtil.addPaginationHeaders(req.res, new Page(results, count, pageRequest));
+    const pageRequest: PageRequest = new PageRequest(
+      req.query.page,
+      req.query.size,
+      req.query.sort
+    );
+    const [results, count] = await this.categoryService.findAndCount(
+      req?.query?.page
+        ? {
+            skip: +pageRequest.page * pageRequest.size,
+            take: +pageRequest.size,
+            order: pageRequest.sort.asOrder(),
+          }
+        : {}
+    );
+    HeaderUtil.addPaginationHeaders(
+      req.res,
+      new Page(results, count, pageRequest)
+    );
     return results;
   }
 
-  @Get('/:id')
+  @Get("/:id")
   @Roles(RoleType.ADMIN)
   @ApiResponse({
     status: 200,
-    description: 'The found record',
-    type: CategoryDTO
+    description: "The found record",
+    type: CategoryDTO,
   })
-  async getOne(@Param('id') id: string): Promise<CategoryDTO> {
+  async getOne(@Param("id") id: string): Promise<CategoryDTO> {
     return await this.categoryService.findById(id);
   }
 
-  @PostMethod('/')
+  @PostMethod("/")
   @Roles(RoleType.ADMIN)
-  @ApiOperation({ summary: 'Create category' })
+  @ApiOperation({ summary: "Create category" })
   @ApiResponse({
     status: 201,
-    description: 'The record has been successfully created.',
-    type: CategoryDTO
+    description: "The record has been successfully created.",
+    type: CategoryDTO,
   })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
-  async post(@Req() req: Request, @Body() categoryDTO: CategoryDTO): Promise<CategoryDTO> {
+  @ApiResponse({ status: 403, description: "Forbidden." })
+  async post(
+    @Req() req: Request,
+    @Body() categoryDTO: CategoryDTO
+  ): Promise<CategoryDTO> {
     const created = await this.categoryService.save(categoryDTO);
-    HeaderUtil.addEntityCreatedHeaders(req.res, 'Category', created.id);
+    HeaderUtil.addEntityCreatedHeaders(req.res, "Category", created.id);
     return created;
   }
 
-  @Put('/')
+  @Put("/")
   @Roles(RoleType.ADMIN)
-  @ApiOperation({ summary: 'Update category' })
+  @ApiOperation({ summary: "Update category" })
   @ApiResponse({
     status: 200,
-    description: 'The record has been successfully updated.',
-    type: CategoryDTO
+    description: "The record has been successfully updated.",
+    type: CategoryDTO,
   })
-  async put(@Req() req: Request, @Body() categoryDTO: CategoryDTO): Promise<CategoryDTO> {
-    HeaderUtil.addEntityCreatedHeaders(req.res, 'Category', categoryDTO.id);
+  async put(
+    @Req() req: Request,
+    @Body() categoryDTO: CategoryDTO
+  ): Promise<CategoryDTO> {
+    HeaderUtil.addEntityCreatedHeaders(req.res, "Category", categoryDTO.id);
     return await this.categoryService.update(categoryDTO);
   }
 
-  @Delete('/:id')
+  @Delete("/:id")
   @Roles(RoleType.ADMIN)
-  @ApiOperation({ summary: 'Delete category' })
+  @ApiOperation({ summary: "Delete category" })
   @ApiResponse({
     status: 204,
-    description: 'The record has been successfully deleted.'
+    description: "The record has been successfully deleted.",
   })
-  async deleteById(@Req() req: Request, @Param('id') id: string): Promise<void> {
-    HeaderUtil.addEntityDeletedHeaders(req.res, 'Category', id);
+  async deleteById(
+    @Req() req: Request,
+    @Param("id") id: string
+  ): Promise<void> {
+    HeaderUtil.addEntityDeletedHeaders(req.res, "Category", id);
     return await this.categoryService.deleteById(id);
   }
 }
--- backend/src/modules/category/category.service.ts
+++ backend/src/modules/category/category.service.ts
@@ -1,17 +1,20 @@
-import { Injectable, HttpException, HttpStatus, Logger } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { FindManyOptions, FindOneOptions } from 'typeorm';
-import { CategoryDTO } from '../../domain/dto/category.dto';
-import { CategoryMapper } from './category.mapper';
-import { CategoryRepository } from './category.repository';
+import { HttpException, HttpStatus, Injectable, Logger } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindManyOptions, FindOneOptions } from "typeorm";
+import { CategoryDTO } from "../../domain/dto/category.dto";
+import { CategoryMapper } from "./category.mapper";
+import { CategoryRepository } from "./category.repository";
 
 const relationshipNames = [];
 
 @Injectable()
 export class CategoryService {
-  logger = new Logger('CategoryService');
+  logger = new Logger("CategoryService");
 
-  constructor(@InjectRepository(CategoryRepository) private categoryRepository: CategoryRepository) {}
+  constructor(
+    @InjectRepository(CategoryRepository)
+    private categoryRepository: CategoryRepository
+  ) {}
 
   async findById(id: string): Promise<CategoryDTO | undefined> {
     const options = { relations: relationshipNames };
@@ -19,17 +22,23 @@ export class CategoryService {
     return CategoryMapper.fromEntityToDTO(result);
   }
 
-  async findByfields(options: FindOneOptions<CategoryDTO>): Promise<CategoryDTO | undefined> {
+  async findByfields(
+    options: FindOneOptions<CategoryDTO>
+  ): Promise<CategoryDTO | undefined> {
     const result = await this.categoryRepository.findOne(options);
     return CategoryMapper.fromEntityToDTO(result);
   }
 
-  async findAndCount(options: FindManyOptions<CategoryDTO>): Promise<[CategoryDTO[], number]> {
+  async findAndCount(
+    options: FindManyOptions<CategoryDTO>
+  ): Promise<[CategoryDTO[], number]> {
     options.relations = relationshipNames;
     const resultList = await this.categoryRepository.findAndCount(options);
     const categoryDTO: CategoryDTO[] = [];
     if (resultList && resultList[0]) {
-      resultList[0].forEach(category => categoryDTO.push(CategoryMapper.fromEntityToDTO(category)));
+      resultList[0].forEach((category) =>
+        categoryDTO.push(CategoryMapper.fromEntityToDTO(category))
+      );
       resultList[0] = categoryDTO;
     }
     return resultList;
@@ -51,7 +60,10 @@ export class CategoryService {
     await this.categoryRepository.delete(id);
     const entityFind = await this.findById(id);
     if (entityFind) {
-      throw new HttpException('Error, entity not deleted!', HttpStatus.NOT_FOUND);
+      throw new HttpException(
+        "Error, entity not deleted!",
+        HttpStatus.NOT_FOUND
+      );
     }
     return;
   }
