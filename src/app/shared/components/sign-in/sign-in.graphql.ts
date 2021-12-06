2a4aa72ce2642be97a2c1a8344645c79f5de9fa7
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2021-12-06 14:46:04 +0800

    feat(core): Fixed signin

--- src/app/shared/components/sign-in/sign-in.graphql.ts
+++ src/app/shared/components/sign-in/sign-in.graphql.ts
@@ -1,11 +1,16 @@
-import gql from 'graphql-tag';
+import {gql} from 'apollo-angular';
+
+
+import { ERROR_RESULT_FRAGMENT } from '../../../common/graphql/fragments.graphql';
 
 export const SIGN_IN = gql`
     mutation SignIn($emailAddress: String!, $password: String!, $rememberMe: Boolean!) {
         login(username: $emailAddress, password: $password, rememberMe: $rememberMe) {
-            user {
+            ...on CurrentUser {
                 id
             }
+            ...ErrorResult
         }
     }
+    ${ERROR_RESULT_FRAGMENT}
 `;
