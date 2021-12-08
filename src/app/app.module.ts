16e63de3635654910b8a9df8d13992bddc1c795c
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2021-12-08 19:16:59 +0800

    fix: checkout stage indicator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,7 +55,7 @@ export function HttpLoaderFactory(http: HttpClient) {
             registrationStrategy: "registerWithDelay:5000",
         }),
     ],
-    bootstrap: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
     constructor(
