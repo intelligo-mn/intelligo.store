922e535f0ea413a6e31cea84f7943e5a36e306a3
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2021-12-09 09:31:07 +0800

    feat(core): Moved storefront to packages

---
+++ packages/storefront/src/app/shared/components/dropdown/dropdown-trigger.directive.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+    selector: '[vsfDropdownTrigger]',
+})
+export class DropdownTriggerDirective {}
--- storefront/src/app/shared/components/dropdown/dropdown-trigger.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-    selector: '[vsfDropdownTrigger]',
-})
-export class DropdownTriggerDirective {
-}
