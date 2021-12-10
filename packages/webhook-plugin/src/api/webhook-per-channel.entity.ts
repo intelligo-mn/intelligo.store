7643acae3e9f89ca51e2ec6228935cd77af86622
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2021-12-10 14:51:16 +0800

    feat(plugin): Created webhook plugin

---
+++ packages/webhook-plugin/src/api/webhook-per-channel.entity.ts
@@ -0,0 +1,20 @@
+import { DeepPartial, VendureEntity, VendureEvent } from '@vendure/core';
+import { Column, Entity } from 'typeorm';
+
+/**
+ * Here we define a new database entity. Passing this in to the plugin's `entities` array
+ * will instruct TypeORM to create the new database table and make the entity available
+ * to query in your plugin code.
+ */
+@Entity()
+export class WebhookPerChannelEntity extends VendureEntity {
+    constructor(input?: DeepPartial<WebhookPerChannelEntity>) {
+        super(input);
+    }
+
+    @Column({ unique: true })
+    channelId!: string;
+
+    @Column()
+    url!: string;
+}
---
+++ packages/webhook-plugin/src/webhook.plugin.ts
@@ -0,0 +1,47 @@
+import { PluginCommonModule, VendurePlugin } from '@vendure/core';
+import { AdminUiExtension } from '@vendure/ui-devkit/compiler';
+import path from 'path';
+
+import { schema } from './api/schema';
+import { WebhookPerChannelEntity } from './api/webhook-per-channel.entity';
+import { WebhookPluginOptions } from './api/webhook-plugin-options';
+import { WebhookResolver } from './api/webhook.resolver';
+import { WebhookService } from './api/webhook.service';
+
+/**
+ * Calls a configurable webhook when configured events arise.
+ * 1 webhook per channel is configurable
+ */
+@VendurePlugin({
+    imports: [PluginCommonModule],
+    entities: [WebhookPerChannelEntity],
+    providers: [WebhookService],
+    adminApiExtensions: {
+        schema,
+        resolvers: [WebhookResolver],
+    },
+})
+export class WebhookPlugin {
+    static options: WebhookPluginOptions;
+
+    static init(options: WebhookPluginOptions): typeof WebhookPlugin {
+        this.options = options;
+        return WebhookPlugin;
+    }
+    static ui: AdminUiExtension = {
+        extensionPath: path.join(__dirname, 'ui'),
+        ngModules: [
+            {
+                type: 'lazy',
+                route: 'webhook',
+                ngModuleFileName: 'webhook.module.ts',
+                ngModuleName: 'WebhookModule',
+            },
+            {
+                type: 'shared',
+                ngModuleFileName: 'webhook-nav.module.ts',
+                ngModuleName: 'WebhookNavModule',
+            },
+        ],
+    };
+}
