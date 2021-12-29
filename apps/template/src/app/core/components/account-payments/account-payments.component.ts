0b1e809f5a9135d9e4f3f5e4390259c2dd747e68
Doljinsuren Enkhbayar <doljinsuren.e@chinggis.systems>
2021-12-30 01:31:04 +0800

    feat: Created account payments and profile

---
+++ apps/template/src/app/core/components/account-payments/account-payments.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-payments',
+  templateUrl: './account-payments.component.html',
+  styleUrls: ['./account-payments.component.css']
+})
+export class AccountPaymentsComponent implements OnInit {
+  navClass = 'nav-light';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
---
+++ apps/template/src/app/core/components/account-works/account-works.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-works',
+  templateUrl: './account-works.component.html',
+  styleUrls: ['./account-works.component.css']
+})
+export class AccountWorksComponent implements OnInit {
+  navClass = 'nav-light';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
