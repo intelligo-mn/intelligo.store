641912e91b4aa3a0ef22ae00a88d965df04bfa31
Doljinsuren Enkhbayar <doljinsuren.e@chinggis.systems>
2021-12-30 01:28:50 +0800

    feat: Created cover login and re pass

---
+++ apps/template/src/app/auth/auth-cover-re-password/auth-cover-re-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-cover-re-password',
+  templateUrl: './auth-cover-re-password.component.html',
+  styleUrls: ['./auth-cover-re-password.component.css']
+})
+export class AuthCoverRePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
