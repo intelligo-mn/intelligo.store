57ec8ed28e62838cb548b1ac735c19c1fe644c43
Doljinsuren Enkhbayar <doljinsuren.e@chinggis.systems>
2021-12-30 01:31:31 +0800

    feat: Created forums and doc

---
+++ apps/template/src/app/core/components/documentation/documentation.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DocumentationComponent } from './documentation.component';
+
+describe('DocumentationComponent', () => {
+  let component: DocumentationComponent;
+  let fixture: ComponentFixture<DocumentationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DocumentationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DocumentationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/template/src/app/core/components/forums/forums.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forums',
+  templateUrl: './forums.component.html',
+  styleUrls: ['./forums.component.css']
+})
+export class ForumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
