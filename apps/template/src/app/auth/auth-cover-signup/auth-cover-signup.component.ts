f00ae5744f66e166dd375317091ed9d674b12280
Doljinsuren Enkhbayar <doljinsuren.e@chinggis.systems>
2021-12-30 01:29:10 +0800

    feat: Created cover login and re pass

---
+++ apps/template/src/app/auth/auth-cover-signup/auth-cover-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-cover-signup',
+  templateUrl: './auth-cover-signup.component.html',
+  styleUrls: ['./auth-cover-signup.component.css']
+})
+export class AuthCoverSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/template/src/app/auth/auth-login-three/auth-login-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-login-three',
+  templateUrl: './auth-login-three.component.html',
+  styleUrls: ['./auth-login-three.component.css']
+})
+export class AuthLoginThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
