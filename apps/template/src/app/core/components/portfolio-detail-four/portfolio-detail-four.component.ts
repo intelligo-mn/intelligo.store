41fa4d51f1b37f3118317953280bee8aeb91588f
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2022-01-20 11:10:02 +0800

    chore: Added css files

--- apps/template/src/app/core/components/portfolio-detail-four/portfolio-detail-four.component.ts
+++ apps/template/src/app/core/components/portfolio-detail-four/portfolio-detail-four.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-detail-four',
-  templateUrl: './portfolio-detail-four.component.html',
-  styleUrls: ['./portfolio-detail-four.component.css']
+  templateUrl: './portfolio-detail-four.component.html'
 })
 
 /**
