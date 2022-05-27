0f84bc6c09f2944d7ab21dac46077ad94f7c6ea0
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2022-05-28 00:51:51 +0800

    added lang

--- dashboard/next-i18next.config.js
+++ dashboard/next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 module.exports = {
   i18n: {
-    locales: ["en", "de", "es", "ar", "he", "zh"],
-    defaultLocale: "en",
+    locales: ["en", "de", "es", "ar", "he", "zh", "mn"],
+    defaultLocale: "mn",
     // localeDetection: false,
   },
   localePath: path.resolve("./public/locales"),
