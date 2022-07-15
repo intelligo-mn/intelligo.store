22f08107ae3f191e2c8856c44cd904c637e08376
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2022-07-15 23:04:44 +0800

    fix: index page style

--- apps/storefront/pages/index.tsx
+++ apps/storefront/pages/index.tsx
@@ -1,66 +1,66 @@
 import BannerCard from "@components/common/banner-card";
 import Container from "@components/ui/container";
-import BrandBlock from "@containers/brand-block";
-import CategoryGridBlock from "@containers/category-grid-block";
-import FeatureBlock from "@containers/feature-block";
+import BrandGridBlock from "@containers/brand-grid-block";
+import CategoryBlock from "@containers/category-block";
 import { getLayout } from "@components/layout/layout";
-import CollectionBlock from "@containers/collection-block";
+import BannerWithProducts from "@containers/banner-with-products";
+import BannerBlock from "@containers/banner-block";
 import Divider from "@components/ui/divider";
-import ProductsWithFlashSale from "@containers/products-with-flash-sale";
 import DownloadApps from "@components/common/download-apps";
 import Support from "@components/common/support";
-import HeroWithCategory from "@containers/hero-with-category";
-import BannerGridBlock from "@containers/banner-grid-block";
-import BestSellerProductFeed from "@components/product/feeds/best-seller-product-feed";
-import NewArrivalsProductFeed from "@components/product/feeds/new-arrivals-product-feed";
+import Instagram from "@components/common/instagram";
+import ProductsFlashSaleBlock from "@containers/product-flash-sale-block";
+import ProductsFeatured from "@containers/products-featured";
+import BannerSliderBlock from "@containers/banner-slider-block";
+import ExclusiveBlock from "@containers/exclusive-block";
 import Subscription from "@components/common/subscription";
+import NewArrivalsProductFeed from "@components/product/feeds/new-arrivals-product-feed";
 import { ROUTES } from "@lib/routes";
 import {
-  gridBanner,
-  minimalDemoHeroBanner as heroBanner,
-  minimalDemoBanner as banner,
+  masonryBanner,
+  promotionBanner,
+  modernDemoBanner as banner,
+  modernDemoProductBanner as productBanner,
 } from "@data/static/banners";
-import { collectionData } from "@data/static/collection";
 
-export { getStaticProps } from "@framework/ssr/homepage/minimal";
-
-const flashSaleCarouselBreakpoint = {
-  "1281": {
-    slidesPerView: 1,
-    spaceBetween: 28,
-  },
-  "768": {
-    slidesPerView: 2,
-    spaceBetween: 20,
-  },
-  "0": {
-    slidesPerView: 1,
-    spaceBetween: 12,
-  },
-};
+export { getStaticProps } from "@framework/ssr/homepage/modern";
 
 export default function Home() {
   return (
-    <Container>
-      <HeroWithCategory data={heroBanner} />
-      <ProductsWithFlashSale carouselBreakpoint={flashSaleCarouselBreakpoint} />
-      <BannerGridBlock data={gridBanner} />
-      <CategoryGridBlock sectionHeading="text-featured-categories" />
-      <Divider />
-      <BestSellerProductFeed />
-      <BannerCard
-        data={banner}
-        href={`${ROUTES.COLLECTIONS}/${banner.slug}`}
-        className="mb-12 lg:mb-14 xl:mb-16 pb-0.5 lg:pb-1 xl:pb-0"
-      />
-      <NewArrivalsProductFeed />
-      <Divider />
-      <BrandBlock sectionHeading="text-top-brands" />
-      <FeatureBlock />
-      <CollectionBlock data={collectionData} />
-      {/* <Support /> */}
-      {/* <Subscription /> */}
-    </Container>
+    <>
+      <BannerBlock data={masonryBanner} />
+      <Container>
+        <ProductsFlashSaleBlock />
+      </Container>
+      <BannerSliderBlock data={promotionBanner} />
+      <Container>
+        <CategoryBlock sectionHeading="text-shop-by-category" variant="rounded" />
+        <ProductsFeatured sectionHeading="text-featured-products" />
+        <BannerCard
+          data={banner[0]}
+          href={`${ROUTES.COLLECTIONS}/${banner[0].slug}`}
+          className="mb-12 lg:mb-14 xl:mb-16 pb-0.5 lg:pb-1 xl:pb-0"
+        />
+        <BrandGridBlock sectionHeading="text-top-brands" />
+        <BannerCard
+          data={banner[1]}
+          href={`${ROUTES.COLLECTIONS}/${banner[1].slug}`}
+          className="mb-12 lg:mb-14 xl:mb-16 pb-0.5 lg:pb-1 xl:pb-0"
+        />
+        <BannerWithProducts
+          sectionHeading="text-on-selling-products"
+          categorySlug="/search"
+          data={productBanner}
+        />
+        <ExclusiveBlock />
+        <NewArrivalsProductFeed />
+        <DownloadApps />
+        <Support />
+        <Instagram />
+        <Subscription className="bg-opacity-0 px-5 sm:px-16 xl:px-0 py-12 md:py-14 xl:py-16" />
+      </Container>
+      <Divider className="mb-0" />
+    </>
   );
 }
 
