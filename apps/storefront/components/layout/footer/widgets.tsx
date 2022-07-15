634227c48bc2db154737f7b6d2d36f9aad85e324
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2022-07-15 23:00:42 +0800

    fix: widgets component

--- apps/storefront/components/layout/footer/widgets.tsx
+++ apps/storefront/components/layout/footer/widgets.tsx
@@ -1,6 +1,6 @@
 import Container from "@components/ui/container";
 import WidgetLink from "@components/widgets/widget-link";
-// import WidgetSocial from "@components/widgets/widget-social";
+import WidgetSocial from "@components/widgets/widget-social";
 import WidgetContact from "@components/widgets/widget-contact";
 
 interface WidgetsProps {
@@ -14,9 +14,9 @@ interface WidgetsProps {
 const Widgets: React.FC<WidgetsProps> = ({ widgets }) => {
   return (
     <Container>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5 md:gap-9 lg:gap-x-8 xl:gap-5  pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24 lg:mb-0.5 2xl:mb-0 3xl:-mb-1">
-        {/* <WidgetSocial /> */}
-        {/* <WidgetContact /> */}
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5 md:gap-9 lg:gap-x-8 xl:gap-5  pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24 lg:mb-0.5 2xl:mb-0 3xl:-mb-1">
+        <WidgetSocial />
+        <WidgetContact />
         {widgets.map((widget, index) => (
           <WidgetLink data={widget} key={`widget-link-${index}`} />
         ))}
