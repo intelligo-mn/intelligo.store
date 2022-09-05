3c39132c72f64812d40bb4a2d8624276f6541b51
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2022-09-06 00:19:43 +0800

    feat: created config package

---
+++ libs/config/src/faq.settings.ts
@@ -0,0 +1,18 @@
+export const faq = [
+  {
+    titleKey: "faq-one-title",
+    contentKey: "faq-one-content",
+  },
+  {
+    titleKey: "faq-two-title",
+    contentKey: "faq-two-content",
+  },
+  {
+    titleKey: "faq-three-title",
+    contentKey: "faq-three-content",
+  },
+  {
+    titleKey: "faq-four-title",
+    contentKey: "faq-four-content",
+  },
+];
