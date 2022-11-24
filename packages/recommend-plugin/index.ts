3f0022b0403e623cf04eb98071d0dd72535cbaa2
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2022-11-25 00:28:42 +0800

    fix: Build error

--- packages/recommend-plugin/index.ts
+++
@@ -1,9 +0,0 @@
-import { AdminUiExtension } from '@vendure/ui-devkit/compiler';
-
-export { ProductRecommendationsPlugin } from './src/recommend.plugin';
-
-export const ProductRecommendationsInputModule: AdminUiExtension['ngModules'][0] = {
-    type: 'shared',
-    ngModuleFileName: 'recommend-input.module.ts',
-    ngModuleName: 'RecommendInputModule',
-};
